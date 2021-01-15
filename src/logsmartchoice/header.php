4fc180eb5eebde7366036d99b9b46c1cdb67b801
nicker <nickey3000@gmail.com>
2021-01-15 08:19:26 +0700

    update css

--- src/logsmartchoice/header.php
+++ src/logsmartchoice/header.php
@@ -1,6 +1,6 @@
 <header>
     <div class="navbar navbar-expand-lg bsnav bsnav-transparent">
-        <button class="navbar-toggler toggler-spring"><span class="navbar-toggler-icon"></span></button>
+        <!-- <button class="navbar-toggler toggler-spring"><span class="navbar-toggler-icon"></span></button> -->
         <div class="collapse navbar-collapse justify-content-md-end">
             <ul class="navbar-nav navbar-mobile mr-0">
                 <li class="nav-item">
