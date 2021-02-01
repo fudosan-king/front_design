c0e2bc217bd80063a3cb3bab64db726e4eae9624
Khanh Nguyen <khanh@propolife.co.jp>
2021-02-01 09:34:56 +0700

    get new tmp logrenove_realestate

---
+++ src/logrenove_realestate_tmp/footer.php
@@ -0,0 +1,37 @@
+<footer>
+    <div class="footer_top">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-lg-12">
+                    <ul>
+                        <li><a href="#">利用規約</a></li>
+                        <li><a href="#">反社会的勢力排除に関する基本方針</a></li>
+                        <li><a href="#">プライバシーポリシー</a></li>
+                        <li><a href="#">ソーシャルメディアポリシー</a></li>
+                    </ul>
+                    <div class="row">
+                        <div class="col-12 col-lg-6 align-self-center">
+                            <p class="mb-2"><a href="#"><img src="images/SVG/LogKnot.svg" alt="" class="img-fluid" width="178"></a></p>
+                            <p class="mb-0">〒107-0061　東京都港区北⻘⼭3-6-23</p>
+                        </div>
+                        <div class="col-12 col-lg-6 align-self-center">
+                            <a href="#" class="btn btn_contact">お問い合わせ</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="footer_bottom">
+    	<div class="container">
+    		<div class="row">
+                <div class="col-12 col-lg-12">
+                    <p class="copyright">Copyright © <a href="index.php">LogKnot Inc.</a> All Rights Reserved.</p>
+                    <p><a href="#" class="btn btn_accessnow">今すぐ査定</a></p>
+                    <p class="mb-0">※首都圏のマンションのみの取り扱いとなります。</p>
+                </div>
+    		</div>
+    	</div>
+    </div>
+</footer>
