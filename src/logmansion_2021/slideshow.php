672c3e788e0cd8b2b0fb5f4a243f5cb88680f012
Anh Thi <anhthi92@propolife.co.jp>
2021-02-26 10:05:43 +0700

    サイトリニューアル環境準備 | FE

---
+++ src/logmansion_2021/slideshow.php
@@ -0,0 +1,24 @@
+<section class="section-slideshow">
+    <div class="g-slideshow">
+        <div id="slideshow_splide" class="splide">
+	            <div class="splide__track">
+	                <div class="splide__list">
+	                    <div class="splide__slide" >
+							<div class="splide-item" style="background-image: url(./images/2x/slide-2.jpg);" ></div>
+		                </div>
+	                    <div class="splide__slide" >
+	                    	<div class="splide-item" style="background-image: url(./images/2x/slide-2.jpg);"></div>
+		                </div>
+	                    <div class="splide__slide">
+	                    	<div class="splide-item" style="background-image: url(./images/2x/slide-2.jpg);"></div>
+		                </div>
+	                </div>
+	            </div>
+        </div>
+        <div class="slideshow-text">
+        	<h1>ABOUT
+        		<p>LogMansion</p></h1>
+
+        </div>
+    </div>
+</section>
