1d33021fcc14d9c76c6d7851e418da4d605488a1
nicker <nickey3000@gmail.com>
2021-03-02 16:55:30 +0700

    問い合わせ ページの作成　｜ FE

---
+++ src/logmansion_2021/completed.php
@@ -0,0 +1,51 @@
+<?php include 'header.php'; ?>
+<?php include 'navigation.php'; ?>
+
+<!-- Slide Show  -->
+<section class="section-slideshow">
+    <div class="g-slideshow">
+        <div class="g-slideshow_content">
+            <div id="slideshow_splide" class="splide slideshow-splide">
+                    <div class="splide__track">
+                        <div class="splide__list">
+                            <div class="splide__slide" >
+                                <div class="splide-item d-none d-md-block" style="background-image: url(./images/2x/slide-2.jpg);" ></div>
+                                <div class="splide-item d-block d-md-none" style="background-image: url(./images/2x/slide-sp-1.jpg);" ></div>
+                            </div>
+                            <div class="splide__slide" >
+                                <div class="splide-item d-none d-md-block" style="background-image: url(./images/2x/top-page-ct.jpg);"></div>
+                                <div class="splide-item d-block d-md-none" style="background-image: url(./images/2x/slide-sp-1.jpg);" ></div>
+                            </div>
+                            <div class="splide__slide">
+                                <div class="splide-item d-none d-md-block" style="background-image: url(./images/2x/slide-2.jpg);"></div>
+                                <div class="splide-item d-block d-md-none" style="background-image: url(./images/2x/slide-sp-1.jpg);" ></div>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+            <div class="slideshow-text">
+                <h2><img src="images/1x/CONTACT.png" alt="" class="img-fluid"></h2>
+
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Top Page Content  -->
+<section class="section_contact_page">
+    <div class="container">
+    	<div class="row">
+    		<div class="col-12 col-lg-12">
+    			<div class="box_completed">
+    				<h2>送信完了</h2>
+    				<p>お問い合わせありがとうございます。送信内容を受け付けました。<br>
+    				追って担当者よりご連絡させていただきます。<br>
+    				連絡がない場合はメールが届いていない可能性がありますので、お手数ですが再度お問合わせください。</p>
+    				<p><small>※当社指定日を挟んだ場合や、お見積り内容により、回答が数日かかる場合があります。予めご了承ください。</small></p>
+    				<p><a href="index.php" class="btn btn-mansion bg-gradient-green-turquoise btn_back">トップに戻る</a></p>
+    			</div>
+    		</div>
+    	</div>
+    </div>
+</section>
+<?php include 'footer.php'; ?>
