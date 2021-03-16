464e27a5b9d7cd03e64587d9aeb78290f39baf3f
nicker <nickey3000@gmail.com>
2021-03-16 11:36:23 +0700

    landing page lp

---
+++ src/logrenove_lp/header.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="UTF-8">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="images/icon/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="images/icon/favicons/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="images/icon/favicons/favicon-16x16.png" sizes="16x16">
+    <link rel="mask-icon" href="images/icon/favicons/safari-pinned-tab.svg" color="#5bbad5">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/bootstrap-v4.6.0.min.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+
+    <title>天然無垢材・自然素材のリノベーションマンション｜ログマンション</title>
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+
+</head>
+    <body>
---
+++ src/logrenove_lp/index.php
@@ -0,0 +1,87 @@
+<?php include 'header.php'; ?>
+
+<div class="nav_top fixed-top">
+    <a href="#" class="btn flex-fill">
+        <span class="time">簡単！１分で</span>
+        <span>事例集プレゼント</span>
+    </a>
+    <a href="#" class="btn flex-fill">
+        <img src="images/LINE_SOCIAL_Square 1.png" alt="" class="img-fluid" width="40">１分でマイホーム診断
+    </a>
+</div>
+
+<main>
+    <section class="section_intro">
+        <p><img src="images/LogRenove_logo_color.svg" alt="" class="img-fluid" width="326"></p>
+        <h1>失敗しない中古マンション<br>
+        選び方セミナー</h1>
+        <div class="box_img_top"></div>
+    </section>
+
+    
+    <section class="section_more pb-0">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-lg-12">
+                    <h2>購入時よりも想定以上に<br>
+                    価格が下がってしまった！</h2>
+                    <p>不動産の価値は景気や市況などに左右されるので、売却する際に価格が非常に上がったり、下がったりいうことは起こり得ます。<br>
+                    とはいえ、一般的には長く時間が経つにつれて建物の価値は下がることは理解しておかないといけません。<br>
+                    以下は首都圏の物件がどれ位の築年数で売却されているのかの平均なのでまず、ここを理解しておくことは大切です。</p>
+                </div>
+            </div>
+        </div>
+        <div class="box_chart mb-0">
+            <img src="images/chat01.png" alt="" class="img-fluid">
+        </div>
+    </section>
+
+    <section class="section_how">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-lg-12 text-center">
+                    <p class="mb-5">上記から築25年までは建物の売却単価は下がっていますが、それ以降は下がりにくくなっています。<br>
+                    これ以降の物件は比較的建物価値の下落リスクは低い、とも捉えることができます。</p>
+                    <h2>古いマンションは大丈夫なの？</h2>
+                    <p>建物自体がきちんと管理されていれば100年は持つ、とも言われていますが、実際、古いマンションはどれ位あるのでしょうか？</p>
+                </div>
+            </div>
+        </div>
+        <div class="box_chart">
+            <img src="images/chat02.png" alt="" class="img-fluid">
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-lg-12 text-center">
+                    <h2 class="mt-lg-5 mt-2 mb-0">このような価格や広さなどだけではない<br>
+                    見分け方を知ることは<br>
+                    失敗するリスクを大幅に減らせます。</h2>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="section_know">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-lg-7 m-auto">
+                    <div class="box_seminar">
+                        <h2>今回のオンラインセミナーは<br>こんな方にお勧めします</h2>
+                        <ul>
+                            <li>
+                                <p><span>#</span> ここに記載できていない注意点や具体的な見分け方のポイントを知りたい</p>
+                            </li>
+                            <li>
+                                <p><span>#</span> ご希望に応じてこれらを踏まえた実際の物件情報を知りたい</p>
+                            </li>
+                        </ul>
+                        <a href="#" class="btn">失敗しない中古マンションセミナー</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</main>
+
+<?php include 'footer.php'; ?>
