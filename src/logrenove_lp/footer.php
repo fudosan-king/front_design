bd804c4cd2b9a1b5f6e8dad317ed181b868b25bc
nicker <nickey3000@gmail.com>
2021-03-17 07:54:21 +0700

    update links socail & optimize photos

--- src/logrenove_lp/footer.php
+++ src/logrenove_lp/footer.php
@@ -4,10 +4,10 @@
 					<div class="col-12 col-lg-12">
 						<a href="#"><img src="images/LogRenove_logo.svg" alt="" class="img-fluid" width="180"></a>
 						<ul>
-							<li><a href="#"><img src="images/fb.svg" alt="" class="img-fluid" width="9"></a></li>
-							<li><a href="#"><img src="images/tw.svg" alt="" class="img-fluid" width="16"></a></li>
-							<li><a href="#"><img src="images/ins.svg" alt="" class="img-fluid" width="14"></a></li>
-							<li><a href="#"><img src="images/line.svg" alt="" class="img-fluid" width="14"></a></li>
+							<li><a href="https://www.facebook.com/logrenove.jp"><img src="images/fb.svg" alt="" class="img-fluid" width="9"></a></li>
+							<li><a href="https://twitter.com/logrenove"><img src="images/tw.svg" alt="" class="img-fluid" width="16"></a></li>
+							<li><a href="https://www.instagram.com/logrenove/"><img src="images/ins.svg" alt="" class="img-fluid" width="14"></a></li>
+							<li><a href="https://page.line.me/hci7480k"><img src="images/line.svg" alt="" class="img-fluid" width="14"></a></li>
 						</ul>
 						<p class="freecall mb-0"><img src="images/freecall.svg" alt="" class="img-fluid mr-1" width="25"><a href="tel:0120-000-0000"> 0120-000-0000</a></p>
 						<p class="mb-0"><small>受付時間：10時〜19時（火・水定休）</small></p>
