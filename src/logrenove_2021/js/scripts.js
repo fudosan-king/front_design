b9018653d36704dc60177dfa984da48f23488870
nicker <nickey3000@gmail.com>
2021-03-31 15:25:56 +0700

    update script

--- src/logrenove_2021/js/scripts.js
+++ src/logrenove_2021/js/scripts.js
@@ -4,7 +4,7 @@ $(function($) {
 		    event.preventDefault();
 		    var $section = $($(this).attr('href')); 
 		    $('html, body').animate({
-		      scrollTop: $section.offset().top - 50
+		      scrollTop: $section.offset().top
 		    }, 500);
 	  	}
 	  	$('[data-scroll]').on('click', scrollToSection);
