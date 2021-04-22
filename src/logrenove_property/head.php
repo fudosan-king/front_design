1da4b1b6b368c9c717c778646e4bb3610220f078
nicker <nickey3000@gmail.com>
2021-04-22 09:15:57 +0700

    Make list page of property - Logrenove

---
+++ src/logrenove_property/head.php
@@ -0,0 +1,89 @@
+<head>
+    <meta name="keywords" content="リノベーション, リフォーム, ログリノベ, LogRenove">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="UTF-8">
+    
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="apple-touch-icon" sizes="152x152" href="https://www.logrenove.jp/wp-content/themes/logrenove2020/assets/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="https://www.logrenove.jp/wp-content/themes/logrenove2020/assets/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="https://www.logrenove.jp/wp-content/themes/logrenove2020/assets/favicon/favicon-16x16.png">
+    <link rel="manifest" href="https://www.logrenove.jp/wp-content/themes/logrenove2020/assets/favicon/site.webmanifest">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" type="text/css">
+    <link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/flickity/2.2.0/flickity.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flickity/2.2.0/flickity.min.css">
+    <noscript>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flickity/2.2.0/flickity.min.css"></noscript>
+    <link rel="preload" href="https://www.logrenove.jp/wp-content/themes/logrenove2020/assets/css/fontawesome-pro/webfonts/fa-brands-400.woff2" as="font" type="font/woff2" crossorigin>
+    <link rel="preload" href="https://www.logrenove.jp/wp-content/themes/logrenove2020/assets/css/fontawesome-pro/webfonts/fa-light-300.woff2" as="font" type="font/woff2" crossorigin>
+    <noscript>
+        <link rel="stylesheet" href="https://www.logrenove.jp/wp-content/themes/logrenove2020/assets/css/fontawesome-pro/css/all.min.css"></noscript>
+    <link rel="preload" href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'">
+    <noscript>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap"></noscript>
+    <script src="https://sdk.form.run/js/v2/formrun.js"></script>
+    <script src='https://www.google.com/recaptcha/api.js'></script>
+    
+    <script>
+    (function(html) { html.className = html.className.replace(/\bno-js\b/, 'js') })(document.documentElement);
+    </script>
+    <!-- This site is optimized with the Yoast SEO plugin v15.4 - https://yoast.com/wordpress/plugins/seo/ -->
+    <link media="all" href="https://www.logrenove.jp/wp-content/cache/autoptimize/css/autoptimize_9f296b46f4faba53cde73b8c763f11f3.css" rel="stylesheet" />
+
+    <link media="all" href="https://www.logrenove.jp/wp-content/cache/autoptimize/css/autoptimize_36e927813e8b7b7401c1fda19ec8b660.css" rel="stylesheet" />
+
+    <link rel="stylesheet" href="css/styles.css">
+
+    <title>リノベーションのイベント・セミナー情報 | オンライン開催実施中 | LogRenove(ログリノベ)</title>
+    <meta name="description" content="LogRenoveのリノベーションイベント・セミナー情報です。リノベーションや住宅購入、資金計画、住宅ローンなど、リノベーションする際のお悩みを解決できるイベントやセミナー、講座を無料開催しています。ご自宅で参加するオンラインセミナーも実施しております。バーチャルプランナーのセミナーまで充実！" />
+    <meta name="robots" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
+    <link rel="canonical" href="https://www.logrenove.jp/events/" />
+    <meta property="og:locale" content="ja_JP" />
+    <meta property="og:type" content="article" />
+    <meta property="og:title" content="リノベーションのイベント・セミナー情報 | オンライン開催実施中 | LogRenove(ログリノベ)" />
+    <meta property="og:description" content="LogRenoveのリノベーションイベント・セミナー情報です。リノベーションや住宅購入、資金計画、住宅ローンなど、リノベーションする際のお悩みを解決できるイベントやセミナー、講座を無料開催しています。ご自宅で参加するオンラインセミナーも実施しております。バーチャルプランナーのセミナーまで充実！" />
+    <meta property="og:url" content="https://www.logrenove.jp/events/" />
+    <meta property="og:site_name" content="LogRenove(ログリノベ)" />
+    <meta property="article:publisher" content="https://www.facebook.com/logrenove.jp/" />
+    <meta property="article:modified_time" content="2020-12-24T11:22:37+00:00" />
+    <meta property="og:image" content="https://www.logrenove.jp/wp-content/uploads/2020/11/Frame-1-9.jpg" />
+    <meta property="og:image:width" content="1024" />
+    <meta property="og:image:height" content="538" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:creator" content="@logrenove" />
+    <meta name="twitter:site" content="@logrenove" />
+    <meta name="twitter:label1" content="Written by">
+    <meta name="twitter:data1" content="LogRenove編集部">
+    
+    <!-- / Yoast SEO plugin. -->
+    <link rel='dns-prefetch' href='//cdnjs.cloudflare.com' />
+    <link rel='dns-prefetch' href='//s.w.org' />
+    <link href='https://cdn.shortpixel.ai' rel='preconnect' />
+    <link rel="alternate" type="application/rss+xml" title="LogRenove(ログリノベ) &raquo; フィード" href="https://www.logrenove.jp/feed/" />
+    
+    
+    <!--[if lt IE 9]>
+<link rel='stylesheet' id='twentyseventeen-ie8-css'  href='https://www.logrenove.jp/wp-content/themes/twentyseventeen/assets/css/ie8.css?ver=20161202' media='all' />
+<![endif]-->
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js' id='jquery-js'></script>
+    <!--[if lt IE 9]>
+<script src='https://www.logrenove.jp/wp-content/themes/twentyseventeen/assets/js/html5.js?ver=20161020' id='html5-js'></script>
+<![endif]-->
+    <link rel="https://api.w.org/" href="https://www.logrenove.jp/wp-json/" />
+    <link rel="alternate" type="application/json" href="https://www.logrenove.jp/wp-json/wp/v2/pages/12697" />
+    <link rel="EditURI" type="application/rsd+xml" title="RSD" href="https://www.logrenove.jp/xmlrpc.php?rsd" />
+    <link rel="wlwmanifest" type="application/wlwmanifest+xml" href="https://www.logrenove.jp/wp-includes/wlwmanifest.xml" />
+    <meta name="generator" content="WordPress 5.5.1" />
+    <link rel='shortlink' href='https://www.logrenove.jp/?p=12697' />
+    <link rel="alternate" type="application/json+oembed" href="https://www.logrenove.jp/wp-json/oembed/1.0/embed?url=https%3A%2F%2Fwww.logrenove.jp%2Fevents%2F" />
+    <link rel="alternate" type="text/xml+oembed" href="https://www.logrenove.jp/wp-json/oembed/1.0/embed?url=https%3A%2F%2Fwww.logrenove.jp%2Fevents%2F&#038;format=xml" />
+    
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MH83P2Q');</script>
+    <!-- End Google Tag Manager -->
+</head>
