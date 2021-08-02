0f67e8f56b8aa955d183c20caf9c8312daf16083
nicker <nickey3000@gmail.com>
2021-08-02 10:39:44 +0700

    all page

--- src/logrenove_lpv2/head.php
+++ src/logrenove_lpv2/head.php
@@ -1,7 +1,7 @@
 <meta charset="UTF-8">
 <!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0">
-<title>Order Renove</title>
+<title>Log Renove</title>
 <meta name="description" content="">
 <meta name="keywords" content="">
 
