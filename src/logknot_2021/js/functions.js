9ab1becd75acefc21b25265bf3030380fadbf43a
nicker <nickey3000@gmail.com>
2021-09-09 16:23:51 +0700

    dd

--- src/logknot_2021/js/functions.js
+++ src/logknot_2021/js/functions.js
@@ -30,3 +30,16 @@ $(function($) {
 	});
 
 
+	$(function () {
+	  $(document).ready(function () {
+	    return $(window).scroll(function () {
+	      return $(window).scrollTop() > 200 ? $("#back-to-top").addClass("show") : $("#back-to-top").removeClass("show")
+	    }), $("#back-to-top").click(function () {
+	      return $("html,body").animate({
+	        scrollTop: "0"
+	      })
+	    })
+	  })
+	});
+
+
