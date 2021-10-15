198f6f4f60fbaff5cbfa10dc1d2bb7622ff6673c
nicker <nickey3000@gmail.com>
2021-10-15 13:59:15 +0700

    lll

--- src/orderrenove/thanks_nonelogin.php
+++ src/orderrenove/thanks_nonelogin.php
@@ -22,7 +22,7 @@
                                 <p>ご入力いただいたメールアドレスに、お問い合わせ完了メールをお送りいたしました。<br>
                                 後ほど、（株）LogSuiteの担当者よりご連絡申し上げます。</p>
                                 <p>以下の「会員登録する」のボタンより、お問い合わせ時の入力内容にて会員登録が可能です。登録後は、お気に入り機能や新着メルマガなどの会員様向けのサービスをご利用いただけます。</p>
-                                <p class="d-flex justify-content-center">
+                                <p class="d-flex justify-content-center w_btngoto">
                                     <a class="btn btngoto mr-2 mr-lg-3" href="#">会員登録する <img src="images/svg/i_right_white.svg" alt="" class="img-fluid" width="10"></a>
                                     <a class="btn btngoto" href="#">トップに戻る <img src="images/svg/i_right_white.svg" alt="" class="img-fluid" width="10"></a>
                                 </p>
