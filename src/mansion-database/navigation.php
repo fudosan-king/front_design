98ebdcd235a2fadd194bf6e44f3f6c03fc943772
Anh Thi <anhthi92@propolife.co.jp>
2021-12-01 16:37:08 +0700

    MSDB｜デザインをFE作成

---
+++ src/mansion-database/navigation.php
@@ -0,0 +1,26 @@
+<header>
+	<div class="nav_menu">
+		<nav class="navbar navbar-expand-lg">
+	        <a class="navbar-brand" href="#">
+	        	<img class="logo-white" src="./images/common/logo-orderrenove-white.svg" alt="" title="" />
+	        	<img class="logo-black" src="./images/common/logo-orderrenove-black.svg" alt="" title="" />
+	        </a>
+	        <!-- button class="navbar-toggler js-menu" type="button" >
+	            <span class="nav-toggler-line"></span>
+	            <span class="nav-toggler-line"></span>
+	            <span class="nav-toggler-line"></span>
+	        </button> -->
+	        <div class="collapse navbar-collapse nav_menu-list">
+	            <ul class="navbar-nav">
+	                <li class="nav-item user">
+	                    <a class="nav-link" href="#"><i class="c-icon i-user"></i><span class="d-none d-md-block">ログイン</span></a>
+	                </li>
+	                <li class="nav-item search">
+	                    <a class="nav-link" href="#"><i class="c-icon i-search"></i></a>
+	                </li>
+	            </ul>
+	        </div>
+	    </nav>
+	</div>
+</header>
+
