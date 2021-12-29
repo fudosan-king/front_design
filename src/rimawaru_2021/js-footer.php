7d828f56a461e29c3163f84f318c019cf81216c5
Anh Thi <anhthi92@propolife.co.jp>
2021-12-29 10:53:20 +0700

    Rimawaru｜Frontend作業

--- src/rimawaru_2021/js-footer.php
+++ src/rimawaru_2021/js-footer.php
@@ -1,7 +1,6 @@
 <script src="js/jquery.min.js"></script>
 <script src="js/bootstrap.bundle.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
-<script src="js/bsnav.min.js"></script>
 <script src="js/bootstrap-datepicker.js"></script>
 <script src="js/bootstrap-datepicker.ja.min.js"></script>
 
