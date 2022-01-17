e78d1ab5886c8077e7f9ddbe5638ee09bd17d703
nicker <nickey3000@gmail.com>
2022-01-17 09:59:03 +0700

    sss

---
+++ src/syoujyudou_syouhinken_a/head.php
@@ -0,0 +1,28 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0">
+<title>LogKnot株式会社 - アタリマエをリノベーション</title>
+<meta name="description" content="">
+<meta name="keywords" content="">
+
+<link rel="apple-touch-icon" sizes="152x152" href="favicon_package_v0.16/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="favicon_package_v0.16/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="favicon_package_v0.16/favicon-16x16.png">
+<link rel="manifest" href="favicon_package_v0.16/site.webmanifest">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#ffffff">
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<link rel="stylesheet" href="css/animate.min.css" type="text/css">
+<link rel="stylesheet" href="css/styles.css" type="text/css">
+
+<script>
+  (function(d) {
+    var config = {
+      kitId: 'lfp3wkb',
+      scriptTimeout: 3000,
+      async: true
+    },
+    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+  })(document);
+</script>
---
+++ src/syoujyudou_syouhinken_a/index.php
@@ -0,0 +1,187 @@
+<!doctype html>
+<html lang="ja">
+    <?php include('head.php') ?>
+    <body>
+        <?php include('header.php') ?>
+
+        <main>
+            <section class="section_printing">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12 col-lg-12">
+                            <h1><span>偽造防止印刷・印字は弊社にお任せください！</span></h1>
+                            <div class="row mb-5 list_check">
+                                <div class="col-12 col-lg-6">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value="" id="ck01" checked>
+                                        <label class="form-check-label" for="ck01">
+                                            プレミアム商品券 
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-lg-6">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value="" id="ck02" checked>
+                                        <label class="form-check-label" for="ck02">
+                                            地域振興券
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-lg-6">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value="" id="ck03" checked>
+                                        <label class="form-check-label" for="ck03">
+                                            ゴミ処理券 
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-lg-6">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value="" id="ck04" checked>
+                                        <label class="form-check-label" for="ck04">
+                                            クーポン券
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-lg-6">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value="" id="ck05" checked>
+                                        <label class="form-check-label" for="ck05">
+                                            グルメチケット 
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-lg-6">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value="" id="ck06" checked>
+                                        <label class="form-check-label" for="ck06">
+                                            百貨店ギフトカード 
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <ul>
+                                <li>
+                                    <span><img src="images/svg/check.svg" alt="" class="img-fluid" width="224"></span>
+                                    <h2>全国各地に</h2>
+                                    <h1>実績多数！</h1>
+                                </li>
+                                <li>
+                                    <span><img src="images/svg/check.svg" alt="" class="img-fluid" width="224"></span>
+                                    <h2>印刷・印字製本まで</h2>
+                                    <h1>トータルサポート</h1>
+                                </li>
+                                <li>
+                                    <span><img src="images/svg/check.svg" alt="" class="img-fluid" width="224"></span>
+                                    <h2>創業80年の</h2>
+                                    <h1>歴史と信頼</h1>
+                                </li>
+                            </ul>
+
+                            <p class="text-center mt-5">
+                                <a href="#"><img src="images/first_estimate.png" alt="" class="img-fluid"></a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="section_achievement">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12 col-lg-12">
+                            <h2><span>実績タイトル</span></h2>
+                            <p class="mb-5">〇〇〇〇◎〇〇〇〇●〇〇〇〇◎〇〇〇〇●〇〇〇〇◎〇〇〇〇●〇〇〇〇◎〇〇〇〇●〇〇〇〇◎〇〇〇〇●〇〇〇〇◎〇〇〇〇●〇〇〇〇◎〇〇〇〇●〇〇〇〇◎〇〇〇〇●〇〇〇〇◎〇〇〇〇●〇〇〇〇◎〇〇〇〇◆</p>
+                            <p class="text-center"><img src="images/svg/chart.svg" alt="" class="img-fluid" width="745"></p>
+                            <ul>
+                                <li>〇〇〇〇〇〇〇〇</li>
+                                <li>〇〇〇〇〇〇〇〇</li>
+                                <li>〇〇〇〇〇〇〇〇</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="section_giftcertificates">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12 col-lg-12">
+                            <h2><span>商品券に偽造防止加工って必要?</span></h2>
+                            <p>　近年のパソコン、スキャナー、プリンター機能の高度化に伴い古くから用いられてきた偽造防止技術では容易に偽造されてしまうリスクが高くなっています。実際に、地域復興クーポン券の印刷状態によって本物か偽物か判別が難しくトラブルに発展した事例も見受けられました。偽造自体をさせないことも重要ですが、万一偽造品が出回ってしまった際にお客様が本物と偽物を見分けられることも危機管理として求められます。</p>
+                            <ul class="question">
+                                <li>
+                                    <div class="box_circle">
+                                        <span>偽物</span>
+                                    </div>
+                                </li>
+                                <li>
+                                    <img src="images/svg/confuse.svg" alt="" class="img-fluid" width="164" alt="">
+                                </li>
+                                <li>
+                                    <div class="box_circle">
+                                        <span>本物</span>
+                                    </div>
+                                </li>
+                            </ul>
+                            <p>　印刷会社のパイオニアとして創業80周年を迎える弊社では、商品券や偽造防止技術に関して長年の経験から培った独自のノウハウがあります。「商品券を作るのに何から始めたらよいか分からない」「偽造されないためにはどんな技術が必要なのか知りたい」といったお客様にも、ご要望に合わせたご提案が可能です。</p>
+                            <p class="text-center my-5">
+                                <a href="#"><img src="images/about_anti-counterfeit.png" alt="" class="img-fluid w-100" width="800"></a>
+                            </p>
+
+                            <h3><span>納品までの流れ</span> <span>校了から納品までは、約1か月を想定しています！</span></h3>
+                            <ul class="flow">
+                                <li>
+                                    <div class="row no-gutters">
+                                        <div class="col-12 col-lg-3 align-self-center">
+                                            <img src="images/svg/design.svg" alt="" class="img-fluid mb-3 mb-lg-0" width="208">
+                                        </div>
+                                        <div class="col-12 col-lg-9 align-self-center">
+                                            <img src="images/svg/design_content.svg" alt="" class="img-fluid ml-2 w-100" width="593">
+                                        </div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="row no-gutters">
+                                        <div class="col-12 col-lg-3 align-self-center">
+                                            <img src="images/svg/anti_counterfeiting.svg" alt="" class="img-fluid mb-3 mb-lg-0" width="200">
+                                        </div>
+                                        <div class="col-12 col-lg-9 align-self-center">
+                                            <h4><span>まずは、お打ち合わせし、<small>（対面・オンラインどちらも対応可）</small><br>
+                                            偽造防止印刷・加工についてご説明・ご提案いたします！</span></h4>
+                                            <img src="images/svg/anti_counterfeiting_content.svg" alt="" class="img-fluid ml-2 w-100" width="593">
+                                        </div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="row no-gutters">
+                                        <div class="col-12 col-lg-3 align-self-center">
+                                            <img src="images/svg/gift_certificate.svg" alt="" class="img-fluid mb-3 mb-lg-0" width="242">
+                                        </div>
+                                        <div class="col-12 col-lg-9 align-self-center">
+                                            <h4><span>他自治体様や企業様の実績をご紹介しながら<br>
+                                            偽造防止印刷・加工についてご説明・ご提案いたします！</span></h4>
+                                            <img src="images/svg/gift_certificate_content.svg" alt="" class="img-fluid ml-2 w-100" width="593">
+                                            <p class="design_fee mt-2"><small>※弊社でデザインから製作する場合はデザイン費を頂戴いたします。</small></p>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                            <p class="text-center">
+                                <a href="#"><img src="images/inquiries.png" alt="" class="img-fluid w-100"></a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
+
+        </main>
+
+        <?php include('footer.php') ?>
+
+        <?php include('js-footer.php') ?>
+    </body>
+</html>
