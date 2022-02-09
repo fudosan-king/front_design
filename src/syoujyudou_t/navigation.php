288742a6cb5d4e59a3f29d5c4f718aa4991e59db
nicker <nickey3000@gmail.com>
2022-02-09 14:58:23 +0700

    昇寿堂様_LP＋フォーム制作 v6

--- src/syoujyudou_t/navigation.php
+++ src/syoujyudou_t/navigation.php
@@ -1,7 +1,7 @@
 <header>
     <div class="nav_menu">
         <nav class="navbar navbar-expand-lg">
-            <a class="navbar-brand" href="#">
+            <a class="navbar-brand" href="index.php">
                 <img src="images/common/logo-blue.png" alt="" class="img-fluid logo-white">
             </a>
             <button class="navbar-toggler js-menu" type="button" >
@@ -13,7 +13,7 @@
             <div class="nav_menu-list">
             	<ul class="navbar-nav nav-pc">
             		<li class="nav-item">
-            			 <a class="nav-link btn-link" href="#">お問い合わせ・お申し込み</a>
+            			 <a class="nav-link btn-link" href="inquiry.php">お問い合わせ・お申し込み</a>
             		</li>
             	</ul>
                 <ul class="navbar-nav nav-sp">
