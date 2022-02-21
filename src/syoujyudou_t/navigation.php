e4993ca64ddffa594377472e49c1447d4a12150f
Anh Thi <anhthi92@propolife.co.jp>
2022-02-21 16:04:13 +0700

    昇寿堂様_LP＋フォーム制作

--- src/syoujyudou_t/navigation.php
+++ src/syoujyudou_t/navigation.php
@@ -16,43 +16,7 @@
             			 <a class="nav-link btn-link" href="inquiry.php">お問い合わせ・お申し込み</a>
             		</li>
             	</ul>
-                <ul class="navbar-nav nav-sp">
-                    <li class="nav-item">
-                        <a class="sub-link nav-link" href="#">
-                        マイページ</a>
-                        <ul>
-                            <li><a href="#">マイページ</a></li>
-                            <li><a href="#">お気に入り</a></li>
-                            <li><a href="#">メルマガ希望条件</a></li>
-                            <li><a href="#">お知らせ</a></li>
-                            <li><a href="#">ログイン／会員登録</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">OrderRenoveについて<i class="c-icon i-arrow-gray"></i></a>
-                    </li> 
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">物件一覧<i class="c-icon i-arrow-gray"></i></a>
-                    </li> 
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">リノベプラン一覧<i class="c-icon i-arrow-gray"></i></a>
-                    </li> 
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">コンシェルジュ相談<i class="c-icon i-arrow-gray"></i></a>
-                    </li> 
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">マネー診断シミュレーター<i class="c-icon i-arrow-gray"></i></a>
-                    </li> 
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">売却サポート<i class="c-icon i-arrow-gray"></i></a>
-                    </li> 
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">LogRenove ライフスタイルメディア<i class="c-icon i-arrow-gray"></i></a>
-                    </li> 
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">ご利用ガイド<i class="c-icon i-arrow-gray"></i></a>
-                    </li>
-                </ul>
+                <ul class="navbar-nav nav-sp"></ul>
             </div>
         </nav>
     </div>
