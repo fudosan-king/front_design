f1a16aff06bc92478df1670b38475fdd6f676983
nicker <nickey3000@gmail.com>
2022-03-25 15:02:36 +0700

    10843

---
+++ src/timerep/head.php
@@ -0,0 +1,21 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>TimeRep</title>
+<meta name="description" content="">
+<meta name="keywords" content="">
+
+<link rel="apple-touch-icon" sizes="152x152" href="favicon_package_v0.16/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="favicon_package_v0.16/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="favicon_package_v0.16/favicon-16x16.png">
+<link rel="manifest" href="favicon_package_v0.16/site.webmanifest">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#ffffff">
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<link rel="stylesheet" href="css/animate.min.css" type="text/css">
+<link rel='stylesheet' href='https://unpkg.com/css-pro-layout@1.1.0/dist/css/css-pro-layout.css'>
+<link rel="stylesheet" href="css/styles.css" type="text/css">
---
+++ src/timerep/operator.php
@@ -0,0 +1,325 @@
+<!doctype html>
+<html lang="en">
+    <?php include('head.php'); ?>
+    <body>
+
+<div class="layout has-sidebar fixed-sidebar fixed-header">
+    <aside id="sidebar" class="sidebar break-point-lg has-bg-image">
+        <div class="sidebar-layout">
+            <div class="sidebar-header">
+                <span>全6拠点</span>
+            </div>
+            <div class="sidebar-content">
+                <div id="col_base" class="col_base">
+                    <ul>
+                        <li>
+                            <span class="base_img">
+                                <img src="images/img_base.jpg" alt="" class="img-fluid">
+                                <a href="#"><img src="images/svg/i_call_white.svg" alt="" class="img-fluid" width="16"> コールする</a>
+                            </span>
+                            <p>customer_A</p>
+                            <p>（カスタマーA）</p>
+                        </li>
+                        <li>
+                            <span class="base_img">
+                                <img src="images/img_base.jpg" alt="" class="img-fluid">
+                                <a href="#"><img src="images/svg/i_call_white.svg" alt="" class="img-fluid" width="16"> コールする</a>
+                            </span>
+                            <p>customer_B</p>
+                            <p>（カスタマーB）</p>
+                        </li>
+                        <li>
+                            <span class="base_img">
+                                <img src="images/img_base.jpg" alt="" class="img-fluid">
+                                <a href="#"><img src="images/svg/i_call_white.svg" alt="" class="img-fluid" width="16"> コールする</a>
+                            </span>
+                            <p>customer_C</p>
+                            <p>（カスタマーC）</p>
+                        </li>
+                        <li>
+                            <span class="base_img">
+                                <img src="images/svg/i_slash.svg" alt="" class="img-fluid w-25 h-auto" width="42">
+                                <a href="#"><img src="images/svg/i_call_white.svg" alt="" class="img-fluid" width="16"> コールする</a>
+                            </span>
+                            <p>customer_A</p>
+                            <p>（カスタマーA）</p>
+                        </li>
+                        <li class="talking">
+                            <span class="base_img">
+                                <!-- <img src="images/img_base.jpg" alt="" class="img-fluid"> -->
+                                <span class="text">通話中</span>
+                                <a href="#"><img src="images/svg/i_call_white.svg" alt="" class="img-fluid" width="16"> コールする</a>
+                            </span>
+                            <p>customer_A</p>
+                            <p>（カスタマーA）</p>
+                        </li>
+                        <li>
+                            <span class="base_img">
+                                <img src="images/img_base.jpg" alt="" class="img-fluid">
+                                <a href="#"><img src="images/svg/i_call_white.svg" alt="" class="img-fluid" width="16"> コールする</a>
+                            </span>
+                            <p>customer_B</p>
+                            <p>（カスタマーB）</p>
+                        </li>
+                        <li>
+                            <span class="base_img">
+                                <img src="images/img_base.jpg" alt="" class="img-fluid">
+                                <a href="#"><img src="images/svg/i_call_white.svg" alt="" class="img-fluid" width="16"> コールする</a>
+                            </span>
+                            <p>customer_C</p>
+                            <p>（カスタマーC）</p>
+                        </li>
+
+                        <li>
+                            <span class="base_img">
+                                <img src="images/img_base.jpg" alt="" class="img-fluid">
+                                <a href="#"><img src="images/svg/i_call_white.svg" alt="" class="img-fluid" width="16"> コールする</a>
+                            </span>
+                            <p>customer_A</p>
+                            <p>（カスタマーA）</p>
+                        </li>
+                        <li>
+                            <span class="base_img">
+                                <img src="images/img_base.jpg" alt="" class="img-fluid">
+                                <a href="#"><img src="images/svg/i_call_white.svg" alt="" class="img-fluid" width="16"> コールする</a>
+                            </span>
+                            <p>customer_A</p>
+                            <p>（カスタマーA）</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </aside>
+
+    <div id="overlay" class="overlay"></div>
+
+    <div class="layout">
+        <header class="header">
+            <a id="btn-collapse" class="btn_open" href="#"></a>
+            <div class="customer_info">
+                <p>カスタマー: <span>customer_D（カスタマー_D）</span></p>
+                <p>グループ： <span>group_design＿test（デザインについて）</span></p>
+            </div>
+            <a href="#" class="btn btn_transfercall ms-auto"><img src="images/svg/i_transfer.svg" alt="" class="img-fluid" width="34"> コールを転送する</a>
+        </header>
+
+    <main class="content">
+        <section class="section_main">
+            <div class="box_main_left">
+                <div class="box_cam_content">
+                    <img src="images/img_main.jpg" alt="" class="img-fluid">
+                </div>
+                
+                <span class="label_maincamera">メインカメラ</span>
+                <a href="#">
+                    <div class="sub_cam">
+                        <img src="images/sub_cam.jpg" alt="" class="img-fluid">
+                        <span class="label_subcamera">カメラ2</span>
+                    </div>
+                </a>
+            </div>
+
+            <div class="box_main_right">
+                <div class="box_right_top">
+                    <img src="images/avatar/img_men2.jpg" alt="" class="img-fluid w-100">
+                    <a href="#box_emotion" class="btn_emotion" data-bs-toggle="collapse">
+                        <img src="images/svg/i_emotion.svg" alt="" class="img-fluid" width="36">
+                    </a>
+                    <ul id="box_emotion" class="box_emotion collapse multi-collapse">
+                        <li>
+                            <a href="#">
+                                <img src="images/svg/i_nod.svg" alt="" class="img-fluid" width="50">
+                                <span>うなずき</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="images/svg/i_laughter.svg" alt="" class="img-fluid" width="40">
+                                <span>笑い</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="images/svg/i_sympathy.svg" alt="" class="img-fluid" width="40">
+                                <span>共感</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="images/svg/i_cry.svg" alt="" class="img-fluid" width="50">
+                                <span>共感</span>
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#">
+                                <img src="images/svg/i_wavinghands.svg" alt="" class="img-fluid" width="59">
+                                <span>手をふる</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="images/svg/i_shakeyourhead.svg" alt="" class="img-fluid" width="61">
+                                <span>首を振る</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="images/svg/i_bow.svg" alt="" class="img-fluid" width="40">
+                                <span>おじぎ</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="images/svg/i_perfect.svg" alt="" class="img-fluid" width="50">
+                                <span>ばっちり!</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div id="box_chat" class="box_notice collapse multi-collapse">
+                <div class="card mb-3">
+                    <div class="card-header">お知らせ <a href="#box_chat" class="btn_close"  data-bs-toggle="collapse"><img src="images/svg/i_close.svg" alt="" class="img-fluid" width="15"></a></div>
+                    <div class="card-body text-secondary pe-1">
+                        <div class="box_mes">
+                            <div class="msg msg_from">
+                                <div class="recepient">
+                                    <span class="icon"><img class="img-fluid" src="images/svg/i_people.svg"></span>
+                                </div>
+                                <div class="content_ms">
+                                    <span>テキストテキステキストトテキステキストテキステキストトテキステキストトテキステテキステキストトテキステキストトテキステ</span>
+                                    <span class="time">13:00</span>
+                                </div>
+                            </div>
+                            <div class="msg msg_to">
+                                <div class="content_ms">
+                                    <span>Do not write me anymore.</span>
+                                    <span class="time">13:32</span>
+                                </div>
+                                <div class="sender">
+                                <span class="icon"><img class="img-fluid" src="images/svg/i_people.svg"></span>
+                                </div>
+                            </div>
+                            <div class="msg msg_from">
+                                <div class="recepient">
+                                    <span class="icon"><img class="img-fluid" src="images/svg/i_people.svg"></span>
+                                </div>
+                                <div class="content_ms">
+                                    <span>テキストテキステキストトテキステキストテキステキストトテキステキストトテキステテキステキストトテキステキストトテキステ</span>
+                                    <span class="time">13:00</span>
+                                </div>
+                            </div>
+                            <div class="msg msg_to">
+                                <div class="content_ms">
+                                    <span>Do not write me anymore.</span>
+                                    <span class="time">13:32</span>
+                                </div>
+                                <div class="sender">
+                                <span class="icon"><img class="img-fluid" src="images/svg/i_people.svg"></span>
+                                </div>
+                            </div>
+                            <div class="msg msg_from">
+                                <div class="recepient">
+                                    <span class="icon"><img class="img-fluid" src="images/svg/i_people.svg"></span>
+                                </div>
+                                <div class="content_ms">
+                                    <span>テキストテキステキストトテキステキストテキステキストトテキステキストトテキステテキステキストトテキステキストトテキステ</span>
+                                    <span class="time">13:00</span>
+                                </div>
+                            </div>
+                            <div class="msg msg_to">
+                                <div class="content_ms">
+                                    <span>Do not write me anymore.</span>
+                                    <span class="time">13:32</span>
+                                </div>
+                                <div class="sender">
+                                <span class="icon"><img class="img-fluid" src="images/svg/i_people.svg"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="box_input">
+                            <input type="text" class="form-control" placeholder="入力してください">
+                            <button type="button" class="btn btn_send"></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div id="box_notice" class="box_notice collapse multi-collapse">
+                <div class="card mb-3">
+                    <div class="card-header">メモ <a href="#box_notice" class="btn_close"  data-bs-toggle="collapse"><img src="images/svg/i_close.svg" alt="" class="img-fluid" width="15"></a></div>
+                    <div class="card-body text-secondary">
+                        <form action="" class="frm_notice">
+                            <div class="mb-3">
+                                <div class="row">
+                                    <div class="col-12 col-lg-6">
+                                        <select name="" class="form-select">
+                                            <option selected>履歴5</option>
+                                            <option value="">履歴4</option>
+                                            <option value="">...</option>
+                                        </select>
+                                    </div>
+                                    <div class="col-12 col-lg-6">
+                                        <a href="#" class="btn btn_newmemo">
+                                            <img src="images/svg/i_add.svg" alt="" class="img-fluid" width="26">
+                                            メモを新規分割
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="mb-3">
+                                <label for="">フリーメモ</label>
+                                <textarea name="" class="form-control" cols="30" rows="6" placeholder="ここにテキストを入力"></textarea>
+                            </div>
+                            <div class="mb-3">
+                                <label for="">性別</label>
+                                <select name="" class="form-select">
+                                    <option selected>選択してください</option>
+                                    <option value="">...</option>
+                                </select>
+                            </div>
+                            <div class="mb-3">
+                                <label for="">年代</label>
+                                <select name="" class="form-select">
+                                    <option selected>選択してください</option>
+                                    <option value="">...</option>
+                                </select>
+                            </div>
+                            <div class="mb-3">
+                                <label for="">問い合わせ内容</label>
+                                <select name="" class="form-select">
+                                    <option selected>選択してください</option>
+                                    <option value="">...</option>
+                                </select>
+                            </div>
+                            <div class="">
+                                <label for="">問題は解決しましたか？</label>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" checked>
+                                    <label class="form-check-label" for="flexRadioDefault1">はい</label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2">
+                                    <label class="form-check-label" for="flexRadioDefault2">いいえ</label>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+    </main>
+
+    <div class="overlay"></div>
+  </div>
+</div>
+
+        <?php include('footer.php'); ?>
+
+        <?php include('js-footer.php') ?>
+    </body>
+</html>
