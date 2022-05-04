7b97d2e2610ce56bd038706462495752bf028170
nicker <nickey3000@gmail.com>
2022-05-04 13:44:22 +0700

    gggg

--- src/timerep/setting_services.php
+++ src/timerep/setting_services.php
@@ -19,6 +19,7 @@
                     </select>
                 </form>
                 <a href="index.php" class="btn btn_logout">ログアウト</a>
+                <a id="btn-collapse" class="btn_open btn_open_sm" href="#"></a>
             </div>
         </header>
 
@@ -122,8 +123,8 @@
                     <div class="template">
                         <div class="setting_services">
                             <h2>接客記録の設定</h2>
-                            <div class="setting_services_content">
-                                <div class="setting_services_item">
+                            <!-- <div class="setting_services_content">
+                                <div class="setting_services_item" draggable="true">
                                     <form class="frm_setting_services_item" action="">
                                         <div class="setting_services_header">
                                             <img src="images/svg/i_line.svg" alt="" class="img-fluid" width="25">
@@ -131,12 +132,8 @@
                                         </div>
                                         <div class="setting_services_title">
                                             <h4>フリーメモ</h4>
-                                            <label>お知らせ機能として利用する
-                                                <input type="checkbox" class="ios-switch green  bigswitch" checked>
-                                                <div><div></div></div>
-                                            </label>
-                                            <label>
-                                                <input type="checkbox" class="ios-switch bigswitch" checked>
+                                            <label>お知らせ機能として利用する 
+                                                <input type="checkbox" class="ios-switch blue  bigswitch" checked>
                                                 <div><div></div></div>
                                             </label>
                                         </div>
@@ -144,11 +141,193 @@
                                         <ul class="setting_services_footer">
                                             <li><a href="#"><img src="images/svg/i_edit.svg" alt="" class="img-fluid" width="27"> </a></li>
                                             <li><a href="#"><img src="images/svg/i_trash.svg" alt="" class="img-fluid" width="24"></a></li>
-                                            <li><a href="#"><img src="images/svg/i_storage.svg" alt="" class="img-fluid" width="36"></a></li>
+                                            <li><a href="#"><img src="images/svg/i_save.svg" alt="" class="img-fluid" width="32"></a></li>
                                         </ul>
                                     </form>
                                 </div>
-                            </div>
+
+                                <div class="setting_services_item" draggable="true">
+                                    <form class="frm_setting_services_item" action="">
+                                        <div class="setting_services_header">
+                                            <img src="images/svg/i_line.svg" alt="" class="img-fluid" width="25">
+                                            <h3>プルダウン</h3>
+                                        </div>
+                                        <div class="setting_services_title">
+                                            <h4>性別</h4>
+                                        </div>
+                                        <select>
+                                            <option value="男性">男性</option>
+                                            <option value="女性">女性</option>
+                                            <option value="その他">その他</option>
+                                        </select>
+                                        <ul class="setting_services_footer">
+                                            <li><a href="#"><img src="images/svg/i_edit.svg" alt="" class="img-fluid" width="27"> </a></li>
+                                            <li><a href="#"><img src="images/svg/i_trash.svg" alt="" class="img-fluid" width="24"></a></li>
+                                            <li><a href="#"><img src="images/svg/i_save2.svg" alt="" class="img-fluid" width="30"></a></li>
+                                        </ul>
+                                    </form>
+                                </div>
+
+                                <div class="setting_services_item" draggable="true">
+                                    <form class="frm_setting_services_item" action="">
+                                        <div class="setting_services_header">
+                                            <img src="images/svg/i_line.svg" alt="" class="img-fluid" width="25">
+                                            <h3>プルダウン</h3>
+                                        </div>
+                                        <div class="setting_services_title">
+                                            <h4>年代</h4>
+                                        </div>
+                                        <select>
+                                            <option value="10代未満">10代未満</option>
+                                            <option value="10代: 10~19">10代: 10~19</option>
+                                            <option value="20代:20~29">20代:20~29</option>
+                                            <option value="30代:30~39">30代:30~39</option>
+                                            <option value="40代:40~49">40代:40~49</option>
+                                            <option value="50代:50~59">50代:50~59</option>
+                                            <option value="60代:60~69">60代:60~69</option>
+                                            <option value="70代以上: over 70">70代以上: over 70</option>
+                                        </select>
+                                        <ul class="setting_services_footer">
+                                            <li><a href="#"><img src="images/svg/i_edit.svg" alt="" class="img-fluid" width="27"> </a></li>
+                                            <li><a href="#"><img src="images/svg/i_trash.svg" alt="" class="img-fluid" width="24"></a></li>
+                                            <li><a href="#"><img src="images/svg/i_storage.svg" alt="" class="img-fluid" width="30"></a></li>
+                                        </ul>
+                                    </form>
+                                </div>
+
+                                <div class="setting_services_item" draggable="true">
+                                    <form class="frm_setting_services_item" action="">
+                                        <div class="setting_services_header">
+                                            <img src="images/svg/i_line.svg" alt="" class="img-fluid" width="25">
+                                            <h3>プルダウン</h3>
+                                        </div>
+                                        <div class="setting_services_title">
+                                            <h4>接客成果</h4>
+                                        </div>
+                                        <select>
+                                            <option value="購入意向示す">購入意向示す</option>
+                                            <option value="...">...</option>
+                                        </select>
+                                        <ul class="setting_services_footer">
+                                            <li><a href="#"><img src="images/svg/i_edit.svg" alt="" class="img-fluid" width="27"> </a></li>
+                                            <li><a href="#"><img src="images/svg/i_trash.svg" alt="" class="img-fluid" width="24"></a></li>
+                                            <li><a href="#"><img src="images/svg/i_storage.svg" alt="" class="img-fluid" width="30"></a></li>
+                                        </ul>
+                                    </form>
+                                </div>
+
+                                <div class="w_newconditions">
+                                    <a href="#" class="btn_newconditions"><img src="images/svg/i_add2.svg" alt="" class="img-fluid" width="30"> 新規追加</a>
+                                </div>
+                            </div> -->
+
+                            <div class="memo-corp__content">
+                                <div>
+
+                                    <div class="memo-corp__section mt--30 mx--30" draggable="true">
+                                        <div class="memo-corp__section__header py--5 handle">
+                                            <div class="d--flex justify__content--center">
+                                                <i class="icon icon--drag_icon" style="visibility: visible;"></i>
+                                            </div>
+                                            <p class="memo-corp__section__header--type mr--15 mt--5">テキスト</p>
+                                        </div>
+                                        <div class="memo-corp__section__form--title d--flex my--20 ml--30">
+                                            <p>フリーメモ</p>
+                                            <label>お知らせ機能として利用する 
+                                                <input type="checkbox" class="ios-switch blue  bigswitch" checked>
+                                                <div><div></div></div>
+                                            </label>
+                                        </div>
+                                        <div class="mx--30">
+                                            <textarea type="text" class="memo-corp__section__form--input" rows="5" maxlength="10000" placeholder="ここにテキストを入力" readonly=""></textarea>
+                                        </div>
+                                        <div class="memo-corp__section__action my--20 mx--20" style="visibility: visible;">
+                                            <i class="icon btn icon--memo_pen"></i>
+                                            <i class="icon btn icon--memo_bin ml--30"></i>
+                                            <i class="icon icon_save ml--30 mr--10"></i>
+                                        </div>
+                                    </div>
+
+                                    <div class="memo-corp__section mt--30 mx--30" draggable="true">
+                                        <div class="memo-corp__section__header py--5 handle">
+                                            <div class="d--flex justify__content--center">
+                                                <i class="icon icon--drag_icon" style="visibility: visible;"></i>
+                                            </div>
+                                            <p class="memo-corp__section__header--type mr--15 mt--5">プルダウン</p>
+                                        </div>
+                                        <div class="memo-corp__section__form--title my--20 ml--30">
+                                            <p>年代</p>
+                                        </div>
+                                        <div class="mx--20 memo-corp__section__form--select">
+                                            <select>
+                                                <option value="10代未満: under 10">10代未満: under 10</option>
+                                                <option value="10代: 10~19">10代: 10~19</option>
+                                                <option value="20代:20~29">20代:20~29</option>
+                                                <option value="30代:30~39">30代:30~39</option>
+                                                <option value="40代:40~49">40代:40~49</option>
+                                                <option value="50代:50~59">50代:50~59</option>
+                                                <option value="60代:60~69">60代:60~69</option>
+                                                <option value="70代以上: over 70">70代以上: over 70</option>
+                                            </select>
+                                        </div>
+                                        <div class="memo-corp__section__action my--20 mx--20" style="visibility: visible;">
+                                            <i class="icon btn icon--memo_pen"></i>
+                                            <i class="icon btn icon--memo_bin ml--30"></i>
+                                            <i class="icon icon_save2 ml--30 mr--10"></i>
+                                        </div>
+                                    </div>
+
+                                    <div class="memo-corp__section mt--30 mx--30" draggable="true">
+                                        <div class="memo-corp__section__header py--5 handle">
+                                            <div class="d--flex justify__content--center">
+                                                <i class="icon icon--drag_icon" style="visibility: visible;"></i>
+                                            </div>
+                                            <p class="memo-corp__section__header--type mr--15 mt--5">プルダウン</p>
+                                        </div>
+                                        <div class="memo-corp__section__form--title my--20 ml--30">
+                                            <p>性別</p>
+                                        </div>
+                                        <div class="mx--20 memo-corp__section__form--select">
+                                            <select>
+                                                <option value="男性">男性</option>
+                                                <option value="女性">女性</option>
+                                                <option value="その他">その他</option>
+                                            </select>
+                                        </div>
+                                        <div class="memo-corp__section__action my--20 mx--20" style="visibility: visible;">
+                                            <i class="icon btn icon--memo_pen"></i>
+                                            <i class="icon btn icon--memo_bin ml--30"></i>
+                                            <i class="icon icon--memo_save_disable ml--30 mr--10"></i>
+                                        </div>
+                                    </div>
+
+                                    <div class="memo-corp__section mt--30 mx--30">
+                                        <div class="memo-corp__section__header py--5 handle">
+                                            <div class="d--flex justify__content--center">
+                                                <i class="icon icon--drag_icon" style="visibility: visible;"></i>
+                                            </div><p class="memo-corp__section__header--type mr--15 mt--5">テキスト</p></div>
+                                            <div class="memo-corp__section__form--title my--20 ml--30">
+                                                <p>フリーメモ</p>
+                                            </div>
+                                            <div class="mx--30">
+                                                <textarea type="text" class="memo-corp__section__form--input" rows="5" maxlength="10000" placeholder="ここにテキストを入力" readonly=""></textarea>
+                                            </div>
+
+                                            <div class="memo-corp__section__action my--20 mx--20" style="visibility: visible;">
+                                                <i class="icon btn icon--memo_pen"></i><i class="icon btn icon--memo_bin ml--30"></i>
+                                                <i class="icon icon--memo_save_disable ml--30 mr--10"></i></div>
+                                            </div>
+                                        </div>
+                                        <div class="memo-corp__btn-plus d--flex justify__content--center my--30">
+                                            <button class="btn--lg btn--action d--flex flex--row justify__content--center align__items--center ">
+                                            <i class="icon icon--memo-corp_plus"></i>
+                                            <p>新規追加</p></button>
+                                        </div>
+                                    </div>
+
+
+
+
                         </div>
                     </div>
                 </main>
