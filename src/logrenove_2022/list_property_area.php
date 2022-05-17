5c1a111b8b4b9eaf90eb66fb7b3c6b21f06c3ef8
nicker <nickey3000@gmail.com>
2022-05-17 11:24:18 +0700

    list page

---
+++ src/logrenove_2022/list_property_area.php
@@ -0,0 +1,249 @@
+<!doctype html>
+<html lang="en">
+    <?php include('head.php'); ?>
+    <body>
+
+        <?php require('header.php'); ?>
+
+        <main>
+            <section class="section_list_templates">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12 col-lg-12">
+                            <h2 class="title">恵比寿・目黒・白金エリア<span>の物件一覧</span></h2>
+                            <p class="description">紹介文が入ります紹介文が入ります紹介文が入ります紹介文が入ります紹介文が入ります紹介文が入ります紹介文が入ります紹介文が入ります紹介文が入ります紹介文が入ります紹介文が入ります紹介文が入ります。</p>
+                            <p class="d-flex justify-content-end"><a href="#" class="btn btn_condition"><img src="images/icons/i_sort.svg" alt="i_sort" width="15"> 条件を変更する</a></p>
+                            <ul class="list_content list_content_property">
+                                <li>
+                                    <a href="detail.php">
+                                        <div class="box_img">
+                                            <img class="img-fluid" src="images/products/img06.jpg" alt="img06">
+                                        </div>
+                                        <div class="box_body">
+                                            <h3>番町千鳥ヶ淵アビタシオン 201号室</h3>
+                                            <h4>5,400万円</h4>
+                                            <p>東京都港区西麻布2丁目</p>
+                                            <p><span>最寄り駅</span> / <span>徒歩13分</span> </p>
+                                            <p><span>2階</span> / <span>48.24㎡</span> / <span>１LDK</span> </p>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="detail.php">
+                                        <div class="box_img">
+                                            <img class="img-fluid" src="images/products/img06.jpg" alt="img06">
+                                        </div>
+                                        <div class="box_body">
+                                            <h3>番町千鳥ヶ淵アビタシオン 201号室</h3>
+                                            <h4>5,400万円</h4>
+                                            <p>東京都港区西麻布2丁目</p>
+                                            <p><span>最寄り駅</span> / <span>徒歩13分</span> </p>
+                                            <p><span>2階</span> / <span>48.24㎡</span> / <span>１LDK</span> </p>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="detail.php">
+                                        <div class="box_img">
+                                            <img class="img-fluid" src="images/products/img06.jpg" alt="img06">
+                                        </div>
+                                        <div class="box_body">
+                                            <h3>番町千鳥ヶ淵アビタシオン 201号室</h3>
+                                            <h4>5,400万円</h4>
+                                            <p>東京都港区西麻布2丁目</p>
+                                            <p><span>最寄り駅</span> / <span>徒歩13分</span> </p>
+                                            <p><span>2階</span> / <span>48.24㎡</span> / <span>１LDK</span> </p>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="detail.php">
+                                        <div class="box_img">
+                                            <img class="img-fluid" src="images/products/img06.jpg" alt="img06">
+                                        </div>
+                                        <div class="box_body">
+                                            <h3>番町千鳥ヶ淵アビタシオン 201号室</h3>
+                                            <h4>5,400万円</h4>
+                                            <p>東京都港区西麻布2丁目</p>
+                                            <p><span>最寄り駅</span> / <span>徒歩13分</span> </p>
+                                            <p><span>2階</span> / <span>48.24㎡</span> / <span>１LDK</span> </p>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="detail.php">
+                                        <div class="box_img">
+                                            <img class="img-fluid" src="images/products/img06.jpg" alt="img06">
+                                        </div>
+                                        <div class="box_body">
+                                            <h3>番町千鳥ヶ淵アビタシオン 201号室</h3>
+                                            <h4>5,400万円</h4>
+                                            <p>東京都港区西麻布2丁目</p>
+                                            <p><span>最寄り駅</span> / <span>徒歩13分</span> </p>
+                                            <p><span>2階</span> / <span>48.24㎡</span> / <span>１LDK</span> </p>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="detail.php">
+                                        <div class="box_img">
+                                            <img class="img-fluid" src="images/products/img06.jpg" alt="img06">
+                                        </div>
+                                        <div class="box_body">
+                                            <h3>番町千鳥ヶ淵アビタシオン 201号室</h3>
+                                            <h4>5,400万円</h4>
+                                            <p>東京都港区西麻布2丁目</p>
+                                            <p><span>最寄り駅</span> / <span>徒歩13分</span> </p>
+                                            <p><span>2階</span> / <span>48.24㎡</span> / <span>１LDK</span> </p>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="detail.php">
+                                        <div class="box_img">
+                                            <img class="img-fluid" src="images/products/img06.jpg" alt="img06">
+                                        </div>
+                                        <div class="box_body">
+                                            <h3>番町千鳥ヶ淵アビタシオン 201号室</h3>
+                                            <h4>5,400万円</h4>
+                                            <p>東京都港区西麻布2丁目</p>
+                                            <p><span>最寄り駅</span> / <span>徒歩13分</span> </p>
+                                            <p><span>2階</span> / <span>48.24㎡</span> / <span>１LDK</span> </p>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="detail.php">
+                                        <div class="box_img">
+                                            <img class="img-fluid" src="images/products/img06.jpg" alt="img06">
+                                        </div>
+                                        <div class="box_body">
+                                            <h3>番町千鳥ヶ淵アビタシオン 201号室</h3>
+                                            <h4>5,400万円</h4>
+                                            <p>東京都港区西麻布2丁目</p>
+                                            <p><span>最寄り駅</span> / <span>徒歩13分</span> </p>
+                                            <p><span>2階</span> / <span>48.24㎡</span> / <span>１LDK</span> </p>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="detail.php">
+                                        <div class="box_img">
+                                            <img class="img-fluid" src="images/products/img06.jpg" alt="img06">
+                                        </div>
+                                        <div class="box_body">
+                                            <h3>番町千鳥ヶ淵アビタシオン 201号室</h3>
+                                            <h4>5,400万円</h4>
+                                            <p>東京都港区西麻布2丁目</p>
+                                            <p><span>最寄り駅</span> / <span>徒歩13分</span> </p>
+                                            <p><span>2階</span> / <span>48.24㎡</span> / <span>１LDK</span> </p>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="detail.php">
+                                        <div class="box_img">
+                                            <img class="img-fluid" src="images/products/img06.jpg" alt="img06">
+                                        </div>
+                                        <div class="box_body">
+                                            <h3>番町千鳥ヶ淵アビタシオン 201号室</h3>
+                                            <h4>5,400万円</h4>
+                                            <p>東京都港区西麻布2丁目</p>
+                                            <p><span>最寄り駅</span> / <span>徒歩13分</span> </p>
+                                            <p><span>2階</span> / <span>48.24㎡</span> / <span>１LDK</span> </p>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="detail.php">
+                                        <div class="box_img">
+                                            <img class="img-fluid" src="images/products/img06.jpg" alt="img06">
+                                        </div>
+                                        <div class="box_body">
+                                            <h3>番町千鳥ヶ淵アビタシオン 201号室</h3>
+                                            <h4>5,400万円</h4>
+                                            <p>東京都港区西麻布2丁目</p>
+                                            <p><span>最寄り駅</span> / <span>徒歩13分</span> </p>
+                                            <p><span>2階</span> / <span>48.24㎡</span> / <span>１LDK</span> </p>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="detail.php">
+                                        <div class="box_img">
+                                            <img class="img-fluid" src="images/products/img06.jpg" alt="img06">
+                                        </div>
+                                        <div class="box_body">
+                                            <h3>番町千鳥ヶ淵アビタシオン 201号室</h3>
+                                            <h4>5,400万円</h4>
+                                            <p>東京都港区西麻布2丁目</p>
+                                            <p><span>最寄り駅</span> / <span>徒歩13分</span> </p>
+                                            <p><span>2階</span> / <span>48.24㎡</span> / <span>１LDK</span> </p>
+                                        </div>
+                                    </a>
+                                </li>
+                            </ul>
+                            <p class="text-center d-block d-lg-none mb-4">
+                                <a href="#"><img class="img-fluid" src="images/icons/i_dot.svg" alt="i_dot" width="6"></a>
+                            </p>
+                            <p class="d-flex justify-content-center justify-content-lg-end"><a href="#" class="btn btn_condition"><img src="images/icons/i_sort.svg" alt="i_sort" width="15"> 条件を変更する</a></p>
+
+                            <nav aria-label="Page navigation example">
+                                <ul class="pagination justify-content-center">
+                                    <li class="page-item disabled">
+                                        <a class="page-link"><img class="img-fluid" src="images/icons/i_left.svg" alt="i_left" width="8"></a>
+                                    </li>
+                                    <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">...</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">6</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">7</a></li>
+                                    <li class="page-item">
+                                        <a class="page-link" href="#"><img class="img-fluid" src="images/icons/i_right2.svg" alt="i_right" width="8"></a>
+                                    </li>
+                                </ul>
+                                </nav>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="section_searchconditions">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12 col-lg-12">
+                            <div class="box_conditions">
+                                <h4>人気のエリアから探す</h4>
+                                <p>表参道･青山　麻布･広尾　渋谷･恵比寿･中目黒　目黒･白金高輪　下北沢･三軒茶屋　東横線･目黒線　駒沢･二子玉川　代々木公園　井の頭線　神楽坂　品川・田町　銀座・築地　豊洲清澄・門前仲町　皇居西側　中央線　千駄ヶ谷･四ッ谷　西新宿　東新宿･早稲田　その他</p>
+                            </div>
+
+                            <div class="box_conditions">
+                                <h4>人気の駅から探す</h4>
+                                <p>表参道駅　乃木坂駅　目黒駅　中目黒駅　代官山駅　恵比寿駅　渋谷駅　三軒茶屋駅　広尾駅　麻布十番駅　六本木駅　品川駅　田町駅　五反田駅　大崎駅</p>
+                            </div>
+
+                            <div class="box_conditions">
+                                <h4>こだわりから探す</h4>
+                                <ul>
+                                    <li><a href="#">リノベ済物件</a></li>
+                                    <li><a href="#">カスタム可能物件</a></li>
+                                    <li><a href="#">ペット飼育可</a></li>
+                                    <li><a href="#">ウォークインクローゼット</a></li>
+                                    <li><a href="#">角部屋　眺望・夜景</a></li>
+                                    <li><a href="#">セキュリティ充実</a></li>
+                                </ul>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+        </main>
+        
+        <?php include('footer.php') ?>
+
+        <?php include('js-footer.php') ?>
+    </body>
+</html>
