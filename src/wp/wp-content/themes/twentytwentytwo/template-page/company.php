1dd423abc5363cdefecd525b5de36185c39004c8
nicker <nickey3000@gmail.com>
2022-06-01 09:50:45 +0700

    demo wp

---
+++ src/wp/wp-content/themes/twentytwentytwo/template-page/company.php
@@ -0,0 +1,48 @@
+<?php 
+/*
+    Template Name: Company1
+*/
+?>
+
+
+        <?php get_header(); ?>
+
+        <main> <!-- InstanceBeginEditable name="content" -->
+            <!--lower visual HTML Part-->
+            <div id="kv-lower" class="company">
+                <div class="container">
+                    <div class="ttl">
+                        <h2>企業情報<span>Company</span></h2>
+                    </div>
+                </div>
+            </div>
+            <!--lower visual HTML Part end-->
+            <!--category HTML Part-->
+            <div id="page-link">
+                <div class="narrow">
+                    <div class="inner">
+                        <div class="block"><a href="../company/profile/">
+                        <div class="img-inner"><img src="<?php bloginfo('template_directory');?>/assets/images/common/company_img_1.jpg" alt=""></div>
+                        <p>会社概要</p>
+                        </a></div>
+                        <div class="block"><a href="../company/philosophy/">
+                        <div class="img-inner"><img src="<?php bloginfo('template_directory');?>/assets/images/common/company_img_2.jpg" alt=""></div>
+                        <p>企業理念</p>
+                        </a></div>
+                        <div class="block"><a href="../company/group/">
+                        <div class="img-inner"><img src="<?php bloginfo('template_directory');?>/assets/images/common/company_img_3.jpg" alt=""></div>
+                        <p>グループ会社一覧</p>
+                        </a></div>
+                        <div class="block"><a href="../company/history/">
+                        <div class="img-inner"><img src="<?php bloginfo('template_directory');?>/assets/images/common/company_img_4.jpg" alt=""></div>
+                        <p>沿革</p>
+                        </a></div>
+                    </div>
+                </div>
+            </div>
+            <!--category HTML Part end-->
+            <!-- InstanceEndEditable --> 
+        </main>
+
+        <?php get_footer(); ?>
+    
