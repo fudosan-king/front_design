8c24429ed6ddbb907db96270f493149db1424308
nicker <nickey3000@gmail.com>
2022-06-02 10:56:20 +0700

    fixed site

---
+++ src/wp/wp-content/themes/chinotatemono/index.php
@@ -0,0 +1,130 @@
+<?php get_header(); ?>
+
+        <main> <!-- InstanceBeginEditable name="content" -->
+            <!--toppage visual part-->
+            <div class="kv">
+                <div class="kv__message"><img src="<?php bloginfo('template_directory');?>/assets/images/common/top_bg1_message.svg" alt="つむぎあい Weaving Together"></div>
+            </div>
+            <!--toppage visual part end-->
+            <section id="aboutus">
+                <div class="container scroll-fade-row">
+                <h2><span>About us</span>「より遠く、より速く移動する」時代から、<br>
+                    「より身近に感じる」時代へ</h2>
+                <hr>
+                <img src="<?php bloginfo('template_directory');?>/assets/images/common/top_bg_about.png" alt="つむぎあい" class="image-fix">
+                <p>私たち千野建物管理は、<br class="pc-only">マンション管理から賃貸管理・不動産仲介業まで、<br class="pc-only">「住」をテーマにさまざまなサービスを手掛けています。<br>
+                    価値観が移ろいやすいこの時代、<br class="pc-only">いち早く「住」とIT技術を融合させることで、<br class="pc-only">多くの方にご満足いただけるサービスを提供してきました。<br>
+                    これからも皆様との出会いを大切に重ねながら、<br class="pc-only">一本一本の糸を紡ぐように、<br class="pc-only">より良い住まいと生活環境を提供してまいります。</p>
+                </div>
+            </section>
+            <!--top page news part-->
+            <section id="top-news" class="scroll-fade-row">
+                <div class="container">
+                <div class="box">
+                    <div class="box__ttl">
+                    <div class="box__ttl--txt"><a href="news/">
+                        <h2>NEWS</h2>
+                        </a></div>
+                    <div class="box__ttl--next"><a href="news/">すべて見る</a></div>
+                    </div>
+                    <div class="box__content">
+                    <dl>
+                        <dt class="" datetime="2021-11-01">2021.11.01</dt>
+                        <dd><a href="">千野建物管理がイノベリオスと業務提携し マンション管理アプリを活用した事業で協業開始</a></dd>
+                        <dt class="" datetime="2021-10-04">2021.10.04</dt>
+                        <dd><a href="">千野建物管理が電子契約サービス 「電子印鑑GMOサイン」を導入決定</a></dd>
+                        <dt class="" datetime="2021-10-01">2021.10.01</dt>
+                        <dd><a href="news/omotesando.html">千野建物管理、表参道支店での営業開始のお知らせ</a></dd>
+                        <dt class="" datetime="2021-10-01">2021.10.01</dt>
+                        <dd><a href="news/omotesando.html">千野建物管理、表参道支店での営業開始のお知らせ</a></dd>
+                        <dt class="" datetime="2021-10-01">2021.10.01</dt>
+                        <dd><a href="news/omotesando.html">千野建物管理、表参道支店での営業開始のお知らせ</a></dd>
+                        <dt class="" datetime="2021-10-01">2021.10.01</dt>
+                        <dd><a href="news/omotesando.html">千野建物管理、表参道支店での営業開始のお知らせ</a></dd>
+                        <dt class="" datetime="2021-10-01">2021.10.01</dt>
+                        <dd><a href="news/omotesando.html">千野建物管理、表参道支店での営業開始のお知らせ</a></dd>
+                        <dt class="" datetime="2021-10-01">2021.10.01</dt>
+                        <dd><a href="news/omotesando.html">千野建物管理、表参道支店での営業開始のお知らせ</a></dd>
+                        <dt class="" datetime="2021-10-01">2021.10.01</dt>
+                        <dd><a href="news/omotesando.html">千野建物管理、表参道支店での営業開始のお知らせ</a></dd>
+                        <dt class="" datetime="2021-10-01">2021.10.01</dt>
+                        <dd><a href="news/omotesando.html">千野建物管理、表参道支店での営業開始のお知らせ</a></dd>
+                    </dl>
+                    </div>
+                </div>
+                </div>
+            </section>
+            <!--top page news part end-->
+            <section class="content scroll-fade-row">
+                <div class="inner business">
+                <div class="inner__img"> </div>
+                <div class="inner__txt">
+                    <div class="ttl-box"><a href="business/">
+                    <h2>事業紹介<span>Business</span></h2></a>
+                    </div>
+                    <ul class="column2">
+                    <li><a href="business/mansionmanagement/" class="btn-next bgleft"><span>分譲マンション管理事業</span></a></li>
+                    <li><a href="business/buildingmanagement/" class="btn-next bgleft"><span>ビル・施設管理事業</span></a></li>
+                    <li><a href="business/brokerage/" class="btn-next bgleft"><span>仲介事業</span></a></li>
+                    <li><a href="business/propertymanagement/" class="btn-next bgleft"><span>プロパティマネジメント事業</span></a></li>
+                    </ul>
+                </div>
+                </div>
+            </section>
+            <section class="content scroll-fade-row">
+                <div class="inner livemansion">
+                <div class="inner__img"> </div>
+                <div class="inner__txt">
+                    <div class="ttl-box"><a href="livemansion/">
+                    <h2>管理マンションにお住まいの方<span>Live in managed condominiums</span></h2></a>
+                    </div>
+                    <ul class="column">
+                    <li><a href="livemansion/service/" class="btn-next bgleft"><span>ご入居中のお客様へのサービス ／ 専有部サービスのご紹介</span></a></li>
+                    <li><a href="livemansion/investment/" class="btn-next bgleft"><span>マンションを売りたい・貸したい</span></a></li>
+                    </ul>
+                </div>
+                </div>
+            </section>
+            <section class="content scroll-fade-row">
+                <div class="inner findmansion">
+                <div class="inner__img"> </div>
+                <div class="inner__txt">
+                    <div class="ttl-box"><a href="findmansion/">
+                    <h2>マンション管理会社を探している<span>Find a condominium management company</span></h2></a>
+                    </div>
+                    <ul class="column">
+                    <li><a href="findmansion/mansionassociate/" class="btn-next bgleft"><span>マンション管理組合様・デベロッパー様</span></a></li>
+                    </ul>
+                </div>
+                </div>
+            </section>
+            <section class="content scroll-fade-row">
+                <div class="inner rentalowner">
+                <div class="inner__img"> </div>
+                <div class="inner__txt">
+                    <a href=""><div class="ttl-box"><a href="rentalowner/">
+                    <h2>賃貸物件のオーナー様<span>Rental property owner</span></h2></a>
+                    </div></a>
+                    <ul class="column">
+                    <li><a href="rentalowner/rethink/" class="btn-next bgleft"><span>管理会社を見直したい</span></a></li>
+                    <li><a href="rentalowner/effective/" class="btn-next bgleft"><span>不動産の有効活用について</span></a></li>
+                    <li><a href="rentalowner/freeassessment/" class="btn-next bgleft"><span>適正賃料の無料査定</span></a></li>
+                    </ul>
+                </div>
+                </div>
+            </section>
+            <section id="link">
+                <div class="container">
+                <ul>
+                    <li><a href="https://www.propolife.co.jp/recruit/" target="_blank"><img src="<?php bloginfo('template_directory');?>/assets/img/bn_link_1.png" alt="採用情報"></a></li>
+                    <li><a href="https://www.facebook.com/CHINOTATEMONOKANRI/" target="_blank"><img src="<?php bloginfo('template_directory');?>/assets/img/bn_link_2.png" alt="facebook"></a></li>
+                    <li><a href="https://www.instagram.com/chino_tatemonokanri/?hl=ja" target="_blank"><img src="<?php bloginfo('template_directory');?>/assets/img/bn_link_3.png" alt="instagram"></a></li>
+                    <li><a href="download/" target="_blank"><img src="<?php bloginfo('template_directory');?>/assets/img/bn_link_4.png" alt="書式DOWNLOAD"></a></li>
+                    <!--<li><a href="" target="_blank"><img src="assets/img/bn_link_5.png" alt="仲介見積り"></a></li>-->
+                </ul>
+                </div>
+            </section>
+            <!-- InstanceEndEditable -->
+        </main>
+        
+        <?php get_footer(); ?>
---
+++ src/wp/wp-content/themes/chinotatemono/template-page/common/header.php
@@ -0,0 +1,60 @@
+<header>
+  <div class="block">
+    <div class="block__ttl">
+      <h1><a href="<?php echo home_url('/'); ?>"><img src="<?php bloginfo('template_directory');?>/assets/img/logo_chinotatemono.svg" alt="千野建物管理株式会社"></a></h1>
+    </div>
+    <div class="block__navi"><a href="" class="btn-cv">お問い合わせ</a>
+      <nav class="">
+        <ul class="mega_menu">
+          <li><a href="<?php echo home_url('/'); ?>">TOP</a></li>
+          <li class="menu"><a href="<?php echo home_url('/company/'); ?>">企業情報</a>
+            <ul class="child_menu">
+              <li class="ttl"><b>企業情報</b><span>company</span></li>
+              <li class="link"><a href="<?php echo home_url('/company/profile/'); ?>"><img src="<?php bloginfo('template_directory');?>/assets/img/navi_img_1.jpg" alt=""><span>会社概要</span></a></li>
+              <li class="link"><a href="<?php echo home_url('/company/philosophy/'); ?>"><img src="<?php bloginfo('template_directory');?>/assets/img/navi_img_2.jpg" alt=""><span>企業理念</span></a></li>
+              <li class="link"><a href="<?php echo home_url('/company/group/'); ?>"><img src="<?php bloginfo('template_directory');?>/assets/img/navi_img_3.jpg" alt=""><span>グループ会社一覧</span></a></li>
+              <li class="link"><a href="<?php echo home_url('/company/history/'); ?>"><img src="<?php bloginfo('template_directory');?>/assets/img/navi_img_4.jpg" alt=""><span>沿革</span></a></li>
+            </ul>
+          </li>
+          <li><a href="news/">NEWS</a></li>
+          <li class="menu"> <a href="business/">事業紹介</a>
+            <ul class="child_menu">
+              <li class="ttl"><b>事業紹介</b><span>business</span></li>
+              <li class="link"><a href="<?php echo home_url('/business/mansionmanagement/'); ?>"><img src="<?php bloginfo('template_directory');?>/assets/img/navi_img_5.jpg" alt=""><span>分譲マンション管理事業</span></a></li>
+              <li class="link"><a href="<?php echo home_url('/business/buildingmanagement/'); ?>"><img src="<?php bloginfo('template_directory');?>/assets/img/navi_img_6.jpg" alt=""><span>ビル・施設管理事業</span></a></li>
+              <li class="link"><a href="<?php echo home_url('/business/brokerage/'); ?>"><img src="<?php bloginfo('template_directory');?>/assets/img/navi_img_7.jpg" alt=""><span>仲介事業</span></a></li>
+              <li class="link"><a href="<?php echo home_url('/business/propertymanagement/'); ?>"><img src="<?php bloginfo('template_directory');?>/assets/img/navi_img_8.jpg" alt=""><span>プロパティマネジメント事業</span></a></li>
+            </ul>
+          </li>
+          <li class="menu"><a href="<?php echo home_url('/livemansion/'); ?>">管理マンションにお住まいの方</a>
+            <ul class="child_menu">
+              <li class="ttl lg"><b>管理マンションにお住まいの方</b><span>Live in managed condominiums</span></li>
+              <li class="link lg"><a href="<?php echo home_url('/livemansion/service/'); ?>"><img src="<?php bloginfo('template_directory');?>/assets/img/navi_img_9.jpg" alt=""><span>ご入居中のお客様へのサービス/専有部サービスのご紹介</span></a></li>
+              <li class="link lg"><a href="<?php echo home_url('/livemansion/investment/'); ?>"><img src="<?php bloginfo('template_directory');?>/assets/img/navi_img_10.jpg" alt=""><span>マンションを売りたい・<br>
+                貸したい</span></a></li>
+            </ul>
+          </li>
+          <li class="menu"><a href="<?php echo home_url('/findmansion/'); ?>">マンション管理会社を探している</a>
+            <ul class="child_menu">
+              <li class="ttl lg"><b>マンション管理会社を探している</b><span>Find a condominium company</span></li>
+              <li class="link lg"><a href="<?php echo home_url('/findmansion/mansionassociate/'); ?>"><img src="<?php bloginfo('template_directory');?>/assets/img/navi_img_11.jpg" alt=""><span>マンション管理組合様・<br>
+                デベロッパー様</span></a></li>
+            </ul>
+          </li>
+          <li class="menu"><a href="<?php echo home_url('/rentalowner/'); ?>">賃貸物件のオーナー様</a>
+            <ul class="child_menu">
+              <li class="ttl lg"><b>賃貸物件のオーナー様</b><span>Rental property owner</span></li>
+              <li class="link"><a href="<?php echo home_url('/rentalowner/rethink/'); ?>"><img src="<?php bloginfo('template_directory');?>/assets/img/navi_img_12.jpg" alt=""><span>管理会社を見直したい</span></a></li>
+              <li class="link"><a href="<?php echo home_url('/rentalowner/effective/'); ?>"><img src="<?php bloginfo('template_directory');?>/assets/img/navi_img_13.jpg" alt=""><span>不動産の有効活用について</span></a></li>
+              <li class="link lg"><a href="<?php echo home_url('/rentalowner/freeassessment/'); ?>"><img src="<?php bloginfo('template_directory');?>/assets/img/navi_img_14.jpg" alt=""><span>賃貸の無料査定</span></a></li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+      <div id="nav-toggle">
+        <div><span></span><span></span><span></span></div>
+      </div>
+    </div>
+  </div>
+</header>
+<!--header.php #Header HTML part end-->
