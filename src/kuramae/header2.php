36f24f0c9b917c0c5a4d87ff11cf2557841c9120
nicker <nickey3000@gmail.com>
2022-07-04 11:58:56 +0700

    update kuramae03

--- src/kuramae/header2.php
+++ src/kuramae/header2.php
@@ -2,8 +2,8 @@
     <div class="navbar navbar-expand-lg bsnav bsnav-transparent bsnav-sticky bsnav-sticky-slide">
         <a class="navbar-brand" href="index2.php">
             <h1>
-                <img src="images/SVG/logo.svg" alt="" class="img-fluid logo_white" width="246">
-                <img src="images/SVG/logo_black.svg" alt="" class="img-fluid logo_black" width="246">
+                <img src="images/SVG/logo.svg" alt="プロスタイル蔵前" class="img-fluid logo_white" width="246">
+                <img src="images/SVG/logo_black.svg" alt="プロスタイル蔵前" class="img-fluid logo_black" width="246">
             </h1>
         </a>
         <button class="navbar-toggler toggler-spring menu">
