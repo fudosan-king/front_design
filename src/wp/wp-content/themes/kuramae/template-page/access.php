6966132692fcfac41ff6246e58d01e88199974f0
nicker <nickey3000@gmail.com>
2022-07-08 13:54:04 +0700

    update access page

--- src/wp/wp-content/themes/kuramae/template-page/access.php
+++ src/wp/wp-content/themes/kuramae/template-page/access.php
@@ -8,119 +8,136 @@
 <?php get_header(); ?>
 
 <main>
-    <section class="slide-main before-login">
-        <div class="slides">
-            <div class="slideContent">
-                <div class="slide slide-init js-slide" style="background-image: url(<?php bloginfo('template_directory');?>/assets/images/common/main/bg_top.jpg)"></div>
-                <div class="slide js-slide" style="background-image: url(<?php bloginfo('template_directory');?>/assets/images/common/main/KV01.jpg)"></div>
-            </div>
-        </div>
-
-        
-        <div class="scroll_down">
-            <a href="#section_reservation" data-scroll class="scroll_title">Scroll</a>
-        </div>
-        <div class="news news_afterlogin">
-            <div class="row">
-                <div class="col-12 col-lg-2 align-self-center">
-                    <h2>新着情報</h2>
-                </div>
-                <div class="col-12 col-lg-10 align-self-center">
-                    <div class="carousel carousel_news" data-flickity='{"pageDots": false, "contain": true, "autoPlay": 3500 }'>
-                        <div class="carousel-cell">
-                            <p><a href="#">物件エントリー者様限定サイトに、間取りを追加いたしました。</a></p>
-                        </div>
-                        <div class="carousel-cell">
-                            <p><a href="#">物件エントリー者様限定サイトに、間取りを追加いたしました。</a></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <section class="section_subbanner">
+        <img src="<?php bloginfo('template_directory');?>/assets/images/common/main/bg_access.jpg" alt="bg_location" class="img-fluid">
     </section>
 
-    <section id="section_reservation" class="section_reservation">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12 col-lg-12">
-                    <img src="<?php bloginfo('template_directory');?>/assets/images/common/reservation_text.svg" alt="" class="img-fluid" width="1245">
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <section class="section_reservation_brown">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12 col-lg-12">
-                    <p>ディライトテラス建物内新モデルルームオープン【完全予約制】</p>
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <section class="section_story access">
+    <section class="section_topaccess">
         <div class="container">
-            <div class="row no-gutters">
-                <div class="col-12 col-lg-6">
+            <div class="row">
+                <div class="col-12 col-lg-12">
                     <div class="story_content" data-aos="fade-down" data-aos-duration="3000">
                         <ul class="list_global">
                             <li><span>PROSTYLE</span></li>
                             <li><span>KURAMAE</span></li>
-                            <li class="active"><span>STORY</span></li>
+                            <li class="active"><span>ACCESS</span></li>
                         </ul>
-                        <h2 class="title">KURAMAE Luxury Arena</h2>
-                        <p class="subtitle">Living in the KURAMAE.</p>
-                        <p class="mb-0">江戸のころから栄え、人々を惹きつけてきた「蔵前」。<br>
-                        雄大な都市に位置しながらも、昔ながらの情緒が残るこの場所に<br>
-                        無垢材の心地よさに深みのある暮らしを送りながらも<br>
-                        アクティブな暮らしが愉しめるレジデンスが誕生します。</p>
-                        <a href="<?php echo home_url('/location/'); ?>" class="btn btn_viewlocaltion">ロケーションを見る</a>
-                    </div>
-                </div>
-                <div class="col-12 col-lg-6">
-                    <div class="living_img">
-                        <img src="<?php bloginfo('template_directory');?>/assets/images/common/map_kuramae.png" data-aos="fade-up" data-aos-duration="3000" alt="" class="img-fluid">
+                        <h2 class="title">徒歩15分以内に8駅5路線利用可能なアクセシビリティ</h2>
+                        <div class="box_img">
+                            <img src="<?php bloginfo('template_directory');?>/assets/images/common/train_acess.png" alt="train_acess" class="img-fluid">
+                        </div>
+                        <ul class="list_color">
+                            <li><span class="dark_orange"></span> 都営浅草線「蔵前」駅（A0出入口）徒歩5分</li>
+                            <li><span class="pink"></span> 都営大江戸線「蔵前」駅（A6出入口）徒歩7分</li>
+                            <li><span class="pink"></span> 都営浅草線「浅草橋」駅（A4出入口）徒歩11分</li>
+                            <li><span class="pink"></span> 都営浅草線「浅草」駅（A1出入口）徒歩14分</li>
+                            <li><span class="orange"></span> 東京メトロ銀座線「稲荷町」駅（4出入口）徒歩14分</li>
+                            <li><span class="orange"></span> 東京メトロ銀座線「田原町」駅（エレベーター出入口）徒歩10分</li>
+                            <li><span class="grey"></span> 都営大江戸線・つくばエクスプレス「新御徒町」駅（A4出入口）徒歩8分</li>
+                            <li><span class="yellow"></span> JR中央・総武線各駅停車「浅草橋」駅（東口）徒歩12分</li>
+                        </ul>
                     </div>
                 </div>
             </div>
         </div>
     </section>
 
-    <section class="section_location">
-        <h2 class="title">ACCESS</h2>
+    <section class="section_mainaccess">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-lg-12">
+                    <div class="box_trainline">
+                        <h2><img src="<?php bloginfo('template_directory');?>/assets/images/common/icon_a.png" alt="icon_a" class="img-fluid"> 都営浅草線「蔵前」駅利用</h2>
+                        <div class="box_img">
+                            <img src="<?php bloginfo('template_directory');?>/assets/images/common/asakusa_line.png" alt="asakusa_line" class="img-fluid">
+                        </div>
+                        <ul>
+                            <li>
+                                <div class="box_line">
+                                    <h3>｢浅草｣<span>駅</span></h3>
+                                    <h4>直通<span>1</span>分［直通1分］</h4>
+                                </div>
+                                <p>●都営浅草線利用［都営浅草線利利用］</p>
+                            </li>
+                            <li>
+                                <div class="box_line">
+                                    <h3>｢押上｣<span>駅</span></h3>
+                                    <h4>直通<span>5</span>分［直通5分］</h4>
+                                </div>
+                                <p>●都営浅草線利用［都営浅草線利利用］</p>
+                            </li>
+                            <li>
+                                <div class="box_line">
+                                    <h3>｢日本橋｣<span>駅</span></h3>
+                                    <h4>直通<span>7</span>分［直通7分］</h4>
+                                </div>
+                                <p>●都営浅草線利用［都営浅草線利利用］</p>
+                            </li>
+                            <li>
+                                <div class="box_line">
+                                    <h3>｢新橋｣<span>駅</span></h3>
+                                    <h4>直通<span>12</span>分［直通12分］</h4>
+                                </div>
+                                <p>●都営浅草線利用［都営浅草線利利用］</p>
+                            </li>
+                            <li>
+                                <div class="box_line">
+                                    <h3>｢新宿｣<span>駅</span></h3>
+                                    <h4>直通<span>15</span>分［直通15分］</h4>
+                                </div>
+                                <p>●都営浅草線利用「浅草橋」駅でJR中央・総武線各駅停車者に乗換え「御茶ノ水」駅でJR中央線快速に乗換え</p>
+                            </li>
+                            <li>
+                                <div class="box_line">
+                                    <h3>｢羽田空港第3ターミナル｣<span>駅</span></h3>
+                                    <h4>直通<span>27</span>分［直通32分］</h4>
+                                </div>
+                                <p>●都営浅草線利用「大門」駅で東京モノレール空港快速に乗換え</p>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="box_trainline">
+                        <h2><img src="<?php bloginfo('template_directory');?>/assets/images/common/icon_e.png" alt="icon_a" class="img-fluid"> 都営大江戸線「蔵前」駅利用</h2>
+                        <div class="box_img">
+                            <img src="<?php bloginfo('template_directory');?>/assets/images/common/oedo_line.png" alt="oedo_line" class="img-fluid">
+                        </div>
+                        <ul>
+                            <li>
+                                <div class="box_line">
+                                    <h3>｢飯田橋｣<span>駅</span></h3>
+                                    <h4>直通<span>9</span>分［直通9分］</h4>
+                                </div>
+                                <p>●都営大江戸線利用［都営大江戸線利利用］</p>
+                            </li>
+                            <li>
+                                <div class="box_line">
+                                    <h3>｢六本木｣<span>駅</span></h3>
+                                    <h4>直通<span>26</span>分［直通29分］</h4>
+                                </div>
+                                <p>●都営大江戸線利用［都営大江戸線利利用］</p>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <p class="note">※上記所要時間は、日中平常時および通勤時のもので時間帯により異なります。また、乗換、待ち時間は含まれません。</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="section_location pt80">
         <div class="gmap">
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4146.140179329061!2d139.78210049743035!3d35.704836103574614!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188eba5218f147%3A0x246d9895d75b24b4!2s111-0055%2C%20Japan!5e0!3m2!1sen!2s!4v1656311130898!5m2!1sen!2s" width="100%" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
         <div class="box_contact">
             <div class="box_contact_img">
-                <img src="<?php bloginfo('template_directory');?>/assets/images/common/img_contact.jpg" alt="" class="img-fluid">
+                <img src="<?php bloginfo('template_directory');?>/assets/images/common/img_contact.jpg" alt="img_contact" class="img-fluid">
             </div>
             <div class="box_contact_content">
                 <h3>プロスタイル蔵前</h3>
-                <p><img src="<?php bloginfo('template_directory');?>/assets/images/common/i_location.svg" alt="" class="img-fluid" width="20"> 〒000-0000　東京都台東区三筋一丁目（地番）　　営業時間 10:00~19:00（⽕・⽔曜⽇定休 祝⽇を除く）</p>
-                <p><img src="<?php bloginfo('template_directory');?>/assets/images/common/i_tel.svg" alt="" class="img-fluid" width="20"> TEL : <a href="tel:03-6897-8570">03-6897-8570</a> 　FAX : <a href="tel:03-6897-8571">03-6897-8571</a></p>
-            </div>
-        </div>
-    </section>
-
-    <section class="section_contact">
-        <div class="container">
-            <div class="row">
-                <div class="col-12 col-lg-12">
-                    <h2 class="title">CONTACT</h2>
-                    <p>資料請求・オンライン説明会</p>
-                    <div class="box_contact">
-                        <div class="box_contact_img">
-                            <img src="<?php bloginfo('template_directory');?>/assets/images/common/img_contact2.png" alt="" class="img-fluid">
-                        </div>
-                        <div class="box_contact_content">
-                            <h3>●●●●●●●パンフレット進呈</h3>
-                            <p>プロスタイル蔵前 の魅力がつまったパンフレットを無料で進呈いたします。以下のフォームからご請求ください。</p>
-                            <a href="<?php echo home_url('/inquiry/'); ?>" class="btn btn_inquiry btn--animated">お問い合わせ</a>
-                        </div>
-                    </div>
-                </div>
+                <p><img src="<?php bloginfo('template_directory');?>/assets/images/common/i_location.svg" alt="i_location" class="img-fluid" width="20"> 〒000-0000　東京都台東区三筋一丁目（地番）　　営業時間 10:00~19:00（⽕・⽔曜⽇定休 祝⽇を除く）</p>
+                <p><img src="<?php bloginfo('template_directory');?>/assets/images/common/i_tel.svg" alt="i_tel" class="img-fluid" width="20"> TEL : <a href="tel:03-6897-8570">03-6897-8570</a> 　FAX : <a href="tel:03-6897-8571">03-6897-8571</a></p>
             </div>
         </div>
     </section>
