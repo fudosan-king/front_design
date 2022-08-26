661cd2a0415bf8285ee296f8a813e7926a975b57
nicker <nickey3000@gmail.com>
2022-08-26 15:16:50 +0700

    update more page

--- src/kuramae/header3.php
+++ src/kuramae/header3.php
@@ -12,21 +12,21 @@
         </button>
         <div class="collapse navbar-collapse justify-content-end">
             <ul class="navbar-nav navbar-mobile mr-0">
-                <li class="nav-item js-menuAnimation"><a class="nav-link" href="#">物件コンセプト</a></li>
                 <li class="nav-item js-menuAnimation"><a class="nav-link" href="locations.php">ロケーション</a></li>
                 <li class="nav-item js-menuAnimation"><a class="nav-link" href="#">天然無垢のマンション</a></li>
                 <li class="nav-item js-menuAnimation dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" href="#">間取り </a>
+                    <a class="nav-link" href="plan.php">間取り </a>
+                    <button type="button" class="dropdown-toggle dropdown-toggle-split btn_down" data-bs-toggle="dropdown" aria-expanded="false">
+                        <span class="visually-hidden">Toggle Dropdown</span>
+                    </button>
                     <ul class="dropdown-menu fade-down">
-                        <li><a class="dropdown-item" href="#">プラン詳細</a></li>
+                        <li><a class="dropdown-item" href="plan_detail.php">プラン詳細</a></li>
                     </ul>
                 </li>
-                <li class="nav-item js-menuAnimation"><a class="nav-link" href="#">設備・仕様</a></li>
+                <li class="nav-item js-menuAnimation"><a class="nav-link" href="equipment.php">設備・仕様</a></li>
                 <li class="nav-item js-menuAnimation"><a class="nav-link" href="access.php">アクセス</a></li>
-                <li class="nav-item js-menuAnimation"><a class="nav-link" href="#">モデルルーム</a></li>
                 <li class="nav-item js-menuAnimation"><a class="nav-link" href="outline.php">物件概要</a></li>
                 <li class="nav-item js-menuAnimation"><a class="nav-link" href="map.php">現地案内図</a></li>
-                <!-- <li class="nav-item js-menuAnimation"><a class="nav-link" href="#">限定サイトログインページ</a></li> -->
                 <li class="nav-item js-menuAnimation"><a class="nav-link" href="inquiry.php">資料請求</a></li>
             </ul>
         </div>
