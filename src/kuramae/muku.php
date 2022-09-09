cbea612891f6c8ae7bab03656a968cb9e62df6d6
ninhdk <ninhdk98@propolife.co.jp>
2022-09-09 16:35:28 +0700

    add muku page

---
+++ src/kuramae/muku.php
@@ -0,0 +1,129 @@
+<!doctype html>
+<html class="no-js">
+
+<head>
+    <?php require 'head.php'; ?>
+</head>
+
+<body>
+    <div id="page">
+
+        <?php include 'header2.php'; ?>
+
+        <main>
+            <section class="section_muku">
+                <img src="images/main/bg_top.jpg" alt="bg_top" class="img-fluid w-100">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12 col-lg-9">
+                            <div class="muku_content" data-aos="fade-down" data-aos-duration="3000">
+                                <ul class="list_global">
+                                    <li><span>PROSTYLE</span></li>
+                                    <li><span>KURAMAE</span></li>
+                                    <li class="active"><span>ABOUT MUKU</span></li>
+                                </ul>
+                                <h2 class="title">天然無垢材とは</h2>
+                                <p class="subtitle">What is natural solid wood</p>
+                                <p class="mb-0">山や森で育った原木（丸太）から、使用する大きさに挽き割った建材です。<br>
+                                    古来より日本の建築文化に深く根付き、現存する歴史的な寺社仏閣もみな天然無垢材による木造建築です。<br>木は種類や育った場所、周囲の環境などによって一本一本に個性が生じるもので、一本の木でも部分によって性質が違います。これをしっかりと見極めて、適した場所に適した材料を使うことが、何百年も生き続ける住まいづくりの基本といえます。</p>
+                            </div>
+                        </div>
+                        <div class="col-12 col-lg-12">
+                            <ul class="list_img_content">
+                                <li>
+                                    <a href="images/activerelax01.jpg"><img src="images/activerelax01.jpg" alt="activemuku" class="img-fluid"></a>
+                                    <h3>耐久性</h3>
+                                    <p>長く使って欲しいからこそ、自然索材にこだわりたい。<br>
+                                    天然無垢材は、合板材と比較すると驚くほど耐久年数が長いのが特長です。合板材の場合、経年劣化により表面がはがれてきますが、天然無垢材は簡単なメンテナンスをするだけで長く使い続けることができます。</p>
+                                </li>
+                                <li>
+                                    <a href="images/activerelax02.jpg"><img src="images/activerelax02.jpg" alt="activemuku" class="img-fluid"></a>
+                                    <h3>安心・安全</h3>
+                                    <p>天然無垢材は、シックハウス症候群の原因とされているホルムアルデヒドなどの化学物質が、合板材を使用した場合と比べ大幅に少ないのが特長です。<br>
+                                    ただ高級というだけではなく、住む人の健康にも配慮した安全・安心の仕まいです。</p>
+                                </li>
+                                <li>
+                                    <a href="images/activerelax03.jpg"><img src="images/activerelax03.jpg" alt="activemuku" class="img-fluid"></a>
+                                    <h3>天然無垢材（オーク）</h3>
+                                    <p>本物の木だからこそ感じられる質感や、長く使い続けるうちに酪し出される経年変化の風合いは、なんともいえぬ味わいとなって部屋のデザイン性を高め、唯一無二の魅力となります。アンティーク家具のように変化していく楽しみをこれからの人生で感じて欲しい。それが私たちの願いです。</p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="section_artisanshipdesign">
+                <img src="images/bg_artisanshipdesign.jpg" alt="artisanshipdesign" class="img-fluid w-100">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12 col-lg-2"></div>
+                        <div class="col-12 col-lg-10">
+                            <div class="artisanshipdesign_content muku_content" data-aos="fade-down" data-aos-duration="3000">
+                                <ul class="list_global">
+                                    <li><span>PROSTYLE</span></li>
+                                    <li><span>KURAMAE</span></li>
+                                    <li class="active"><span>CHARM OF MUKU</span></li>
+                                </ul>
+                                <h2 class="title">住む人にぬくもりと安らぎを与える<br>“無垢材の魅力”</h2>
+                                <p class="subtitle">High quality relaxation in Kuramae</p>
+                                <p class="mb-0">弊社で採用している主な無垢材の一つ、「オーク（ナラ）」は代表的な樹木で、はるか昔から ウィスキーの樽や船舶の建材として使われています。重厚感があり傷がつきにくく耐久性に優れています。美しい木目も特徴的で、時が経つほどに味が出てくる素材です。合板フローリングと無垢材フローリングの住宅では体感で温かさの違いが実感できます。特に室温が22°Cを下回るとその差は非常に顕著です。この体感差には、熱浸透率（J/s0.5m2K）が関係しています。熱浸透率は建材の熱伝導率（熱の伝わりやすさ）と比熱（熱を貯められる容量）そして比重（体積当たりの重量）によって決まります。例えば無垢材フローリング材料であるオークの熱浸透率は500（J/s0.5m2K）と、鉄筋コンクリート2250（J/s0.5m2K）の約5分の1であり、冬季に床温度が低くなっていても、足裏から熱が奪われにくいため、心地よく感じます。</p>
+                            </div>
+                        </div>
+                        <div class="col-12 col-lg-12">
+                            <ul class="list_img_content">
+                                <li>
+                                    <a href="images/activerelax01.jpg"><img src="images/activerelax01.jpg" alt="activemuku" class="img-fluid"></a>
+                                    <h3>1.人に優しい素材</h3>
+                                    <p>山や森で育った原木をひき割って作った「無垢材」は、いわゆる自然素材です。アトピーや喘息、シックハウス症候群の原因になる「ホルムアルデヒド」を含む接着剤を使用していないため、安心・安全です。無垢材は住む人の健康を考えるうえで、非常に魅力的な建材です。</p>
+                                </li>
+                                <li>
+                                    <a href="images/activerelax02.jpg"><img src="images/activerelax02.jpg" alt="activemuku" class="img-fluid"></a>
+                                    <h3>2.オリジナリティ溢れる無垢材</h3>
+                                    <p>オークの天然無垢材の表面には虎斑（とらふ）と呼ばれる独特の縞模様があり、またそのひとつひとつが異なる表情を持ちます。生きた木から取られた無垢材は 一枚一枚がそれぞれに個性を持ち、世界中を探しても同じ物は一つとしてなく、オリジナリティ溢れる材料です。</p>
+                                </li>
+                                <li>
+                                    <a href="images/activerelax03.jpg"><img src="images/activerelax03.jpg" alt="activemuku" class="img-fluid"></a>
+                                    <h3>3.夏は涼しく、冬は暖かく</h3>
+                                    <p>木の断熱材はコンクリートの約12バイト言われるほどで、周囲の湿度に影響されにくいのが特徴です。<br>
+                                    夏は、木に触れてもベタつくことなく、とてもさわやか。冬は、裸足でもいられるほどほんのりとした温もりも感じることができます。木の住まいではエアコンに頼る度合いが低くなるので、体への負担が軽減されるとともに、省エネにも役立ち住む人の健康や暮らしを守ります。</p>
+                                </li>
+                                <li>
+                                    <a href="images/activerelax03.jpg"><img src="images/activerelax03.jpg" alt="activemuku" class="img-fluid"></a>
+                                    <h3>4.天然の徐加湿器</h3>
+                                    <p>天然の木は、伐採された後も呼吸を続けています。湿気が多いと吸収し、乾燥すると蓄えた湿度を吐き出す調湿効果があります。例えば奈良の正倉院は太い木材を横にして重ねていく校倉造り。1300年近くの時を経た今でも、中の宝物を大切に守り続けているのは、木のもつ優れた調湿作用によって結露やカビも防止でき、空気をきれいにし、快適に保つことが出来るためです。</p>
+                                </li>
+                                <li>
+                                    <a href="images/activerelax03.jpg"><img src="images/activerelax03.jpg" alt="activemuku" class="img-fluid"></a>
+                                    <h3>5.優れた耐久性</h3>
+                                    <p>天然の木は、伐採された後も呼吸を続けています。湿気が多いと吸収し、乾燥すると蓄えた湿度を吐き出す調湿効果があります。例えば奈良の正倉院は太い木材を横にして重ねていく校倉造り。1300年近くの時を経た今でも、中の宝物を大切に守り続けているのは、木のもつ優れた調湿作用によって結露やカビも防止でき、空気をきれいにし、快適に保つことが出来るためです。</p>
+                                </li>
+                                <li>
+                                    <a href="images/activerelax03.jpg"><img src="images/activerelax03.jpg" alt="activemuku" class="img-fluid"></a>
+                                    <h3>6.天然のアロマオイル</h3>
+                                    <p>森林浴をすると、リラックスして心地よくなるのは、樹木が発する芳香成分（フィトンチッド）の働きによるものです。フィトンチッドには、人間の自律神経を安定させる作用があると言われています。 天然無垢材はアロマテラピーのような香りで心も体もリフレッシュでき、ストレスを和らげます。</p>
+                                </li>
+                                <li>
+                                    <a href="images/activerelax03.jpg"><img src="images/activerelax03.jpg" alt="activemuku" class="img-fluid"></a>
+                                    <h3>7.吸音効果</h3>
+                                    <p>無垢材フローリングは高い吸音効果を持っています。木材は多孔質構造のため、高音から低音まで幅広く吸音するので、部屋の生活音、残響音をまろやかに緩和することができ、心地よい空間を作ります。</p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <?php include('section_tel.php'); ?>
+
+        </main>
+
+        <?php require 'footer.php'; ?>
+
+
+    </div><!-- End page -->
+
+    <?php require 'js-footer.php'; ?>
+</body>
+
+</html>
