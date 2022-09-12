07aacd2a9916061ef1ddd8567a39fd43eba37b8b
ninhdk <ninhdk98@propolife.co.jp>
2022-09-12 09:04:41 +0700

    import image

--- src/kuramae/muku.php
+++ src/kuramae/muku.php
@@ -12,7 +12,7 @@
 
         <main>
             <section class="section_muku">
-                <img src="images/main/bg_top.jpg" alt="bg_top" class="img-fluid w-100">
+                <img src="images/main/bg_muku.jpg" alt="bg_top" class="img-fluid w-100">
                 <div class="container">
                     <div class="row">
                         <div class="col-12 col-lg-9">
@@ -31,19 +31,19 @@
                         <div class="col-12 col-lg-12">
                             <ul class="list_img_content">
                                 <li>
-                                    <a href="images/activerelax01.jpg"><img src="images/activerelax01.jpg" alt="activemuku" class="img-fluid"></a>
+                                    <a href="images/muku01.jpg"><img src="images/muku01.jpg" alt="mukuimg" class="img-fluid"></a>
                                     <h3>耐久性</h3>
                                     <p>長く使って欲しいからこそ、自然索材にこだわりたい。<br>
                                     天然無垢材は、合板材と比較すると驚くほど耐久年数が長いのが特長です。合板材の場合、経年劣化により表面がはがれてきますが、天然無垢材は簡単なメンテナンスをするだけで長く使い続けることができます。</p>
                                 </li>
                                 <li>
-                                    <a href="images/activerelax02.jpg"><img src="images/activerelax02.jpg" alt="activemuku" class="img-fluid"></a>
+                                    <a href="images/muku02.jpg"><img src="images/muku02.jpg" alt="mukuimg" class="img-fluid"></a>
                                     <h3>安心・安全</h3>
                                     <p>天然無垢材は、シックハウス症候群の原因とされているホルムアルデヒドなどの化学物質が、合板材を使用した場合と比べ大幅に少ないのが特長です。<br>
                                     ただ高級というだけではなく、住む人の健康にも配慮した安全・安心の仕まいです。</p>
                                 </li>
                                 <li>
-                                    <a href="images/activerelax03.jpg"><img src="images/activerelax03.jpg" alt="activemuku" class="img-fluid"></a>
+                                    <a href="images/muku03.jpg"><img src="images/muku03.jpg" alt="mukuimg" class="img-fluid"></a>
                                     <h3>天然無垢材（オーク）</h3>
                                     <p>本物の木だからこそ感じられる質感や、長く使い続けるうちに酪し出される経年変化の風合いは、なんともいえぬ味わいとなって部屋のデザイン性を高め、唯一無二の魅力となります。アンティーク家具のように変化していく楽しみをこれからの人生で感じて欲しい。それが私たちの願いです。</p>
                                 </li>
@@ -54,7 +54,7 @@
             </section>
 
             <section class="section_artisanshipdesign">
-                <img src="images/bg_artisanshipdesign.jpg" alt="artisanshipdesign" class="img-fluid w-100">
+                <img src="images/charmofmuku.jpg" alt="charmofmuku" class="img-fluid w-100">
                 <div class="container">
                     <div class="row">
                         <div class="col-12 col-lg-2"></div>
@@ -73,38 +73,38 @@
                         <div class="col-12 col-lg-12">
                             <ul class="list_img_content">
                                 <li>
-                                    <a href="images/activerelax01.jpg"><img src="images/activerelax01.jpg" alt="activemuku" class="img-fluid"></a>
+                                    <a href="images/muku04.jpg"><img src="images/muku04.jpg" alt="mukuimg" class="img-fluid"></a>
                                     <h3>1.人に優しい素材</h3>
                                     <p>山や森で育った原木をひき割って作った「無垢材」は、いわゆる自然素材です。アトピーや喘息、シックハウス症候群の原因になる「ホルムアルデヒド」を含む接着剤を使用していないため、安心・安全です。無垢材は住む人の健康を考えるうえで、非常に魅力的な建材です。</p>
                                 </li>
                                 <li>
-                                    <a href="images/activerelax02.jpg"><img src="images/activerelax02.jpg" alt="activemuku" class="img-fluid"></a>
+                                    <a href="images/muku05.jpg"><img src="images/muku05.jpg" alt="mukuimg" class="img-fluid"></a>
                                     <h3>2.オリジナリティ溢れる無垢材</h3>
                                     <p>オークの天然無垢材の表面には虎斑（とらふ）と呼ばれる独特の縞模様があり、またそのひとつひとつが異なる表情を持ちます。生きた木から取られた無垢材は 一枚一枚がそれぞれに個性を持ち、世界中を探しても同じ物は一つとしてなく、オリジナリティ溢れる材料です。</p>
                                 </li>
                                 <li>
-                                    <a href="images/activerelax03.jpg"><img src="images/activerelax03.jpg" alt="activemuku" class="img-fluid"></a>
+                                    <a href="images/muku06.jpg"><img src="images/muku06.jpg" alt="mukuimg" class="img-fluid"></a>
                                     <h3>3.夏は涼しく、冬は暖かく</h3>
                                     <p>木の断熱材はコンクリートの約12バイト言われるほどで、周囲の湿度に影響されにくいのが特徴です。<br>
                                     夏は、木に触れてもベタつくことなく、とてもさわやか。冬は、裸足でもいられるほどほんのりとした温もりも感じることができます。木の住まいではエアコンに頼る度合いが低くなるので、体への負担が軽減されるとともに、省エネにも役立ち住む人の健康や暮らしを守ります。</p>
                                 </li>
                                 <li>
-                                    <a href="images/activerelax03.jpg"><img src="images/activerelax03.jpg" alt="activemuku" class="img-fluid"></a>
+                                    <a href="images/muku07.jpg"><img src="images/muku07.jpg" alt="mukuimg" class="img-fluid"></a>
                                     <h3>4.天然の徐加湿器</h3>
                                     <p>天然の木は、伐採された後も呼吸を続けています。湿気が多いと吸収し、乾燥すると蓄えた湿度を吐き出す調湿効果があります。例えば奈良の正倉院は太い木材を横にして重ねていく校倉造り。1300年近くの時を経た今でも、中の宝物を大切に守り続けているのは、木のもつ優れた調湿作用によって結露やカビも防止でき、空気をきれいにし、快適に保つことが出来るためです。</p>
                                 </li>
                                 <li>
-                                    <a href="images/activerelax03.jpg"><img src="images/activerelax03.jpg" alt="activemuku" class="img-fluid"></a>
+                                    <a href="images/muku08.jpg"><img src="images/muku08.jpg" alt="mukuimg" class="img-fluid"></a>
                                     <h3>5.優れた耐久性</h3>
-                                    <p>天然の木は、伐採された後も呼吸を続けています。湿気が多いと吸収し、乾燥すると蓄えた湿度を吐き出す調湿効果があります。例えば奈良の正倉院は太い木材を横にして重ねていく校倉造り。1300年近くの時を経た今でも、中の宝物を大切に守り続けているのは、木のもつ優れた調湿作用によって結露やカビも防止でき、空気をきれいにし、快適に保つことが出来るためです。</p>
+                                    <p>寺院や神社など日本の歴史的建造物の多くは木材でできています。日本の木造住宅が建築方法やメンテナンスによって、百年・千年単位で建ち続けることが可能だという事実は、多くの建造物が証明してくれています。例えば日本最古の木造建築物「法隆寺五重塔」など約1400年以上も立ち続けるほど、天然無垢材は耐久性のある材料です。</p>
                                 </li>
                                 <li>
-                                    <a href="images/activerelax03.jpg"><img src="images/activerelax03.jpg" alt="activemuku" class="img-fluid"></a>
+                                    <a href="images/muku09.jpg"><img src="images/muku09.jpg" alt="mukuimg" class="img-fluid"></a>
                                     <h3>6.天然のアロマオイル</h3>
                                     <p>森林浴をすると、リラックスして心地よくなるのは、樹木が発する芳香成分（フィトンチッド）の働きによるものです。フィトンチッドには、人間の自律神経を安定させる作用があると言われています。 天然無垢材はアロマテラピーのような香りで心も体もリフレッシュでき、ストレスを和らげます。</p>
                                 </li>
                                 <li>
-                                    <a href="images/activerelax03.jpg"><img src="images/activerelax03.jpg" alt="activemuku" class="img-fluid"></a>
+                                    <a href="images/muku10.jpg"><img src="images/muku10.jpg" alt="mukuimg" class="img-fluid"></a>
                                     <h3>7.吸音効果</h3>
                                     <p>無垢材フローリングは高い吸音効果を持っています。木材は多孔質構造のため、高音から低音まで幅広く吸音するので、部屋の生活音、残響音をまろやかに緩和することができ、心地よい空間を作ります。</p>
                                 </li>
