604afadb05661bd986b1fb44af22d07e890dc34b
nicker <nickey3000@gmail.com>
2022-10-17 16:30:28 +0700

    HTML

---
+++ src/prostyleryokan_2022/footer.php
@@ -0,0 +1,5 @@
+<footer>
+	<a href="#"><img src="assets/images/logo_footer.svg" alt="ryokan" class="img-fluid" width="250"></a>
+    <p>Copyright © <a href="index.php">PROSTYLE RYOKAN</a> All Rights Reserved.</p>
+</footer>
+
---
+++ src/prostyleryokan_2022/header.php
@@ -0,0 +1,7 @@
+<header>
+    <h1><a href="index.php"><img class="img-fluid" src="assets/images/SAUNA_logo.svg" alt="SAUNA logo" width="250"></a></h1>
+    <ul class="lang">
+        <li class="active"><a href="#">JP</a></li>
+        <li><a href="#">EN</a></li>
+    </ul>
+</header>
