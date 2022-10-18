13e16c40f5c532e45664408b5b280ca85d841496
nicker <nickey3000@gmail.com>
2022-10-18 10:04:37 +0700

    ...

---
+++ src/wp_ prostyleryokan_2022/wp-content/themes/prostyleryokan/footer.php
@@ -0,0 +1,11 @@
+<footer>
+	<a href="#"><img src="<?php bloginfo('template_directory');?>/assets/images/logo_footer.svg" alt="ryokan" class="img-fluid" width="250"></a>
+    <p>Copyright © <a href="index.php">PROSTYLE RYOKAN</a> All Rights Reserved.</p>
+</footer>
+
+<?php 
+    require 'template-page/js-footer.php';
+?>
+
+</body>
+</html>
---
+++ src/wp_ prostyleryokan_2022/wp-content/themes/prostyleryokan/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<!--header.php #Header HTML part-->
+<html lang="ja">
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KFVKSR"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta charset="UTF-8">
+        <title>プロスタイル旅館 - コーポレートサイト</title>
+        <meta name="description" content="神奈川県横浜市中区にあるプロスタイル旅館は「旅先の我が家をつくる」をコンセプトとしてホテル運営やコンサルティング業務などを行う会社です">
+        <meta name="keywords" content="プロスタイル蔵前,蔵前,新築分譲マンション,居住用,ラグジュアリー,大江戸線,浅草線,都営地下鉄,都心生活">
+        <meta property="og:site_name" content="【公式】プロスタイル蔵前｜新築分譲マンション">
+        <meta property="og:url" content="">
+
+        <link rel="apple-touch-icon" sizes="152x152" href="<?php bloginfo('template_directory');?>/assets/images/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="<?php bloginfo('template_directory');?>/assets/images/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="<?php bloginfo('template_directory');?>/assets/images/favicon-16x16.png">
+        <link rel="manifest" href="<?php bloginfo('template_directory');?>/assets/images/site.webmanifest">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="theme-color" content="#ffffff">
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.0/animate.min.css" type="text/css">
+        <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/scss/flickity.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.css">
+        <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/scss/styles.css" type="text/css">
+        <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/scss/be_styles.css" type="text/css">
+    </head>
+  
+  <body>
