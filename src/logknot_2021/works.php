47503a1adfc6e0f988e8ab37e6d4fe93a60a2f15
nicker <nickey3000@gmail.com>
2022-11-02 14:32:41 +0700

    ...

--- src/logknot_2021/works.php
+++ src/logknot_2021/works.php
@@ -127,6 +127,46 @@
                                             </div>
                                         </a>
 
+                                        <a href="#" class="card">
+                                            <div class="content">
+                                            <span class="title">Web Design</span>
+                                            <span class="category">Mobile / Design / Rebranding</span>
+                                            </div>
+                                            <div class="image">
+                                            <img src="https://images.unsplash.com/photo-1509395176047-4a66953fd231?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80" alt="" />
+                                            </div>
+                                        </a>
+
+                                        <a href="#" class="card">
+                                            <div class="content">
+                                            <span class="title">UX Design</span>
+                                            <span class="category">UX / UI / Research </span>
+                                            </div>
+                                            <div class="image">
+                                            <img src="https://images.unsplash.com/photo-1545235617-9465d2a55698?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80" alt="" />
+                                            </div>
+                                        </a>
+
+                                        <a href="#" class="card">
+                                            <div class="content">
+                                            <span class="title">Concepts</span>
+                                            <span class="category">Design / Creative</span>
+                                            </div>
+                                            <div class="image">
+                                            <img src="https://images.unsplash.com/photo-1553531888-a5892402adce?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80" alt="" />
+                                            </div>
+                                        </a>
+
+                                        <a href="#" class="card">
+                                            <div class="content">
+                                            <span class="title">Creative & Cool</span>
+                                            <span class="category">Design / Food</span>
+                                            </div>
+                                            <div class="image">
+                                            <img src="https://images.unsplash.com/photo-1484980859177-5ac1249fda6f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1684&q=80" alt="" />
+                                            </div>
+                                        </a>
+
                                         <a href="#" class="btn btnmore">
                                             <span>もっと見る</span>
                                             <img src="https://www.logknot.co.jp/wp-content/themes/logknot2021/assets/images/svg/i_right.svg" alt="" class="img-fluid" width="30">
