778377538459220f13bc017bd565624746eec552
nicker <nickey3000@gmail.com>
2022-11-04 13:53:02 +0700

    ...

--- src/logknot_2021/works.php
+++ src/logknot_2021/works.php
@@ -24,7 +24,7 @@
                                 
                                 <div class="box_works_content">
                                     <h2 class="title mb-5">360度パース画像</h2>
-                                    <a href="https://t4.ftcdn.net/jpg/05/38/82/79/240_F_538827931_lB2eE3EcP7z0ZzOxWV0gm24l36wdWWst.jpg" class="card"  data-fancybox="gallery" data-caption="<b>戸建住宅</b> <br /> 外観パース">
+                                    <a href="https://t4.ftcdn.net/jpg/05/38/82/79/240_F_538827931_lB2eE3EcP7z0ZzOxWV0gm24l36wdWWst.jpg" class="card"  data-fancybox="360degree" data-caption="<b>戸建住宅</b> <br /> 外観パース">
                                         <div class="content">
                                             <span class="title">戸建住宅</span>
                                             <span class="category">外観パース</span>
@@ -33,7 +33,7 @@
                                             <img src="https://t4.ftcdn.net/jpg/05/38/82/79/240_F_538827931_lB2eE3EcP7z0ZzOxWV0gm24l36wdWWst.jpg" alt="01">
                                         </div>
                                     </a>
-                                    <a href="assets/images/gallerys/02.jpg" class="card" data-fancybox="gallery" data-caption="<b>サービス付き高齢者住宅</b> <br /> 外観パース">
+                                    <a href="assets/images/gallerys/02.jpg" class="card" data-fancybox="360degree" data-caption="<b>サービス付き高齢者住宅</b> <br /> 外観パース">
                                         <div class="content">
                                             <span class="title">サービス付き高齢者住宅</span>
                                             <span class="category">外観パース</span>
@@ -47,7 +47,7 @@
 
                                 <div class="box_works_content">
                                     <h2 class="title mb-5">パース図面</h2>
-                                        <a href="assets/images/gallerys/03.jpg" class="card" data-fancybox="gallery" data-caption="<b>マンション外構</b> <br /> 外観パース">
+                                        <a href="assets/images/gallerys/03.jpg" class="card" data-fancybox="perspective_drawing" data-caption="<b>マンション外構</b> <br /> 外観パース">
                                             <div class="content">
                                                 <span class="title">マンション外構</span>
                                                 <span class="category">外観パース</span>
@@ -57,7 +57,7 @@
                                             </div>
                                         </a>
 
-                                        <a href="assets/images/gallerys/04.jpg" class="card" data-fancybox="gallery" data-caption="<b>飲食店</b> <br /> 外観パース">
+                                        <a href="assets/images/gallerys/04.jpg" class="card" data-fancybox="perspective_drawing" data-caption="<b>飲食店</b> <br /> 外観パース">
                                             <div class="content">
                                                 <span class="title">飲食店</span>
                                                 <span class="category">外観パース</span>
@@ -67,7 +67,7 @@
                                             </div>
                                         </a>
 
-                                        <a href="assets/images/gallerys/05.jpg" class="card" data-fancybox="gallery" data-caption="<b>パチンコ店夜景</b> <br /> 外観パース">
+                                        <a href="assets/images/gallerys/05.jpg" class="card" data-fancybox="perspective_drawing" data-caption="<b>パチンコ店夜景</b> <br /> 外観パース">
                                             <div class="content">
                                                 <span class="title">パチンコ店夜景</span>
                                                 <span class="category">外観パース</span>
@@ -77,7 +77,7 @@
                                             </div>
                                         </a>
 
-                                        <a href="assets/images/gallerys/06.jpg" class="card" data-fancybox="gallery" data-caption="<b>デイサービス</b> <br /> 内観パース">
+                                        <a href="assets/images/gallerys/06.jpg" class="card" data-fancybox="perspective_drawing" data-caption="<b>デイサービス</b> <br /> 内観パース">
                                             <div class="content">
                                                 <span class="title">デイサービス</span>
                                                 <span class="category">内観パース</span>
@@ -87,7 +87,7 @@
                                             </div>
                                         </a>
 
-                                        <a href="assets/images/gallerys/07.jpg" class="card" data-fancybox="gallery" data-caption="<b>エレベータホール</b> <br /> 内観パース">
+                                        <a href="assets/images/gallerys/07.jpg" class="card" data-fancybox="perspective_drawing" data-caption="<b>エレベータホール</b> <br /> 内観パース">
                                             <div class="content">
                                                 <span class="title">エレベータホール</span>
                                                 <span class="category">内観パース</span>
@@ -97,7 +97,7 @@
                                             </div>
                                         </a>
 
-                                        <a href="assets/images/gallerys/08.jpg" class="card" data-fancybox="gallery" data-caption="<b>住宅</b> <br /> 内観パース">
+                                        <a href="assets/images/gallerys/08.jpg" class="card" data-fancybox="perspective_drawing" data-caption="<b>住宅</b> <br /> 内観パース">
                                             <div class="content">
                                                 <span class="title">住宅</span>
                                                 <span class="category">内観パース</span>
@@ -107,7 +107,7 @@
                                             </div>
                                         </a>
 
-                                        <a href="assets/images/gallerys/09.jpg" class="card" data-fancybox="gallery" data-caption="<b>古民家改修</b> <br /> 外観パース">
+                                        <a href="assets/images/gallerys/09.jpg" class="card" data-fancybox="perspective_drawing" data-caption="<b>古民家改修</b> <br /> 外観パース">
                                             <div class="content">
                                                 <span class="title">古民家改修</span>
                                                 <span class="category">外観パース</span>
@@ -117,7 +117,7 @@
                                             </div>
                                         </a>
 
-                                        <a href="assets/images/gallerys/10.jpg" class="card" data-fancybox="gallery" data-caption="<b>建売住宅２棟</b> <br /> 外観パース">
+                                        <a href="assets/images/gallerys/10.jpg" class="card" data-fancybox="perspective_drawing" data-caption="<b>建売住宅２棟</b> <br /> 外観パース">
                                             <div class="content">
                                                 <span class="title">建売住宅２棟</span>
                                                 <span class="category">外観パース</span>
@@ -127,7 +127,7 @@
                                             </div>
                                         </a>
 
-                                        <a href="assets/images/gallerys/11.jpg" class="card" data-fancybox="gallery" data-caption="<b>戸建て住宅</b> <br /> 外観パース">
+                                        <a href="assets/images/gallerys/11.jpg" class="card" data-fancybox="perspective_drawing" data-caption="<b>戸建て住宅</b> <br /> 外観パース">
                                             <div class="content">
                                                 <span class="title">戸建て住宅</span>
                                                 <span class="category">外観パース</span>
