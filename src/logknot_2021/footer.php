819baca4d6c4d50523f88b3cecf1f95e6a0c59f3
nicker <nickey3000@gmail.com>
2023-04-05 08:27:22 +0700

    update footer

--- src/logknot_2021/footer.php
+++ src/logknot_2021/footer.php
@@ -12,18 +12,45 @@
                     </ul>
                 </div>
 
-                <div class="col-12 col-lg-12">
-                    <div class="footer_info text-center">
-                        <!-- <p class="mb-3"><a href="<?//=site_url('wp-content/uploads/LogKnot㈱_第1期_計算書類.pdf')?>" target="_tbank" rel="noopener noreferrer">令和3年3月期</a> <img src="<?//=IMAGE_PATH?>/pdf.png" alt="" class="pdf" height="20" width="auto"></p> -->
-
-                        <a href="https://www.logknot.co.jp"><img src="https://www.logknot.co.jp/wp-content/themes/logknot2021/assets/images/svg/logo.svg" alt="" class="img-fluid" width="200"></a>
-                        <p class="mt-2">LogKnot株式会社　<br>
-                        東京都港区北青山3-6-23</p>
-                        <ul class="list_social justify-content-center">
-                            <li><a target="_blank" href="https://www.facebook.com/logknot"><img src="https://www.logknot.co.jp/wp-content/themes/logknot2021/assets/images/svg/i_fb.svg" alt="" class="img-fluid" width="12"></a></li>
-                            <!-- <li><a href=""><img src="images/svg/i_twitter.svg" alt="" class="img-fluid" width="24"></a></li>
-                            <li><a href=""><img src="images/svg/i_instagram.svg" alt="" class="img-fluid" width="22"></a></li> -->
-                        </ul>
+                <div class="col-12 col-lg-8 m-auto">
+                    <div class="row align-items-center">
+                        <div class="col-12 col-lg-4">
+                            <div class="footer_info text-center">
+                                <!-- <p class="mb-3"><a href="<?//=site_url('wp-content/uploads/LogKnot㈱_第1期_計算書類.pdf')?>" target="_tbank" rel="noopener noreferrer">令和3年3月期</a> <img src="<?//=IMAGE_PATH?>/pdf.png" alt="" class="pdf" height="20" width="auto"></p> -->
+                                <a href="https://www.logknot.co.jp"><img src="https://www.logknot.co.jp/wp-content/themes/logknot2021/assets/images/svg/logo.svg" alt="" class="img-fluid" width="200"></a>
+                                <p class="mt-2">LogKnot株式会社　<br>
+                                東京都港区北青山3-6-23</p>
+                                <ul class="list_social justify-content-center">
+                                    <li><a target="_blank" href="https://www.facebook.com/logknot"><img src="https://www.logknot.co.jp/wp-content/themes/logknot2021/assets/images/svg/i_fb.svg" alt="" class="img-fluid" width="12"></a></li>
+                                    <!-- <li><a href=""><img src="images/svg/i_twitter.svg" alt="" class="img-fluid" width="24"></a></li>
+                                    <li><a href=""><img src="images/svg/i_instagram.svg" alt="" class="img-fluid" width="22"></a></li> -->
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-6 col-lg-4">
+                            <div class="box_list_company">
+                                <h5>グループ企業</h5>
+                                <ul class="list_company">
+                                    <li><a taget="_blank" href="https://www.propolife.co.jp/">株式会社プロポライフグループ</a></li>
+                                    <li><a taget="_blank" href="https://www.logsuite.co.jp/">株式会社LogSuite</a></li>
+                                    <li><a taget="_blank" href="https://www.prostyle-residence.com/">株式会社Prostyle</a></li>
+                                    <li><a taget="_blank" href="https://www.logasset.co.jp/">株式会社LogAsset</a></li>
+                                    <li><a taget="_blank" href="https://www.logarchitects.co.jp/">株式会社LogArchitects</a></li>
+                                    <li><a taget="_blank" href="https://www.logknot.co.jp/">LogKnot株式会社</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-6 col-lg-4">
+                            <div class="box_list_company">
+                                <ul class="list_company">
+                                    <li><a taget="_blank" href="https://www.chinokanri.co.jp/">千野建物管理株式会社</a></li>
+                                    <li><a taget="_blank" href="https://www.prostyleryokan.com/">株式会社Prostyle旅館</a></li>
+                                    <li><a taget="_blank" href="http://nikuan-kotakino.com/">株式会社小滝野</a></li>
+                                    <li><a taget="_blank" href="https://www.oki-ig.com/">株式会社沖縄イゲトー</a></li>
+                                    <li><a taget="_blank" href="https://www.propolifevietnam.com/">PROPOLIFE VIETNAM</a></li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 
