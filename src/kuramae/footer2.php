9bad680b9693ad8fb14f1dd5944a9afe87a0d26e
ninhdk98 <ninhdk98@propolife.co.jp>
2023-08-02 12:57:51 +0700

    new layout top page

---
+++ src/kuramae/footer2.php
@@ -0,0 +1,40 @@
+<footer class="footer2">
+	<div class="container">
+		<div class="row">
+    		<div class="col-12 col-lg-12">
+                <a href="index2.php"><img src="images/SVG/logo.svg" alt="" class="img-fluid mb-3" width="200"></a>
+                <ul>
+                    <li><a href="whatsnews.php">新着情報</a></li>
+                    <li><a href="about.php">PROSTYLEについて</a></li>
+                    <li><a target="_blank" href="https://www.propolife.co.jp/terms/">利用規約</a></li>
+                    <li><a target="_blank" href="https://www.propolife.co.jp/antisocial/">反社会的勢力排除に関する基本方針</a></li>
+                    <li><a target="_blank" href="https://www.prostyle-residence.com/privacy-policy/">プライバシーポリシー</a></li>
+                    <li><a target="_blank" href="https://www.propolife.co.jp/socialpolicy/">ソーシャルメディアポリシー</a></li>
+                </ul>
+                
+                <ul class="mb-0">
+                    <li><a target="_blank" href="https://www.prostyle-residence.com/">【売主】 <img src="images/logo_small.svg" alt="" width="116"> </a></li>
+                    <!-- <li><a target="_blank" href="https://www.logsuite.co.jp/">【販売】<img src="images/SVG/logsuite_white.svg" alt="" width="80"></a></li> -->
+                </ul>
+            </div>
+		</div>
+	</div>
+    <div class="footer_bottom">
+        <address>
+            <p class="mb-0">Copyright © <a target="_blank" href="https://www.prostyle-residence.com/">PROSTYLE INC.</a> All Rights Reserved.</p>
+        </address>
+    </div>
+</footer>
+
+<section class="section_request fixed-bottom">
+    <a class="btn d-none d-lg-inline-block" id="button_order" data_button_order="資料請求" href="https://www.prostyle-residence.com/kuramae/inquiry/"><span>WEB資料請求はこちら</span></a>
+    <a class="btn btn_reservation d-none d-lg-inline-block" id="btn_reservation" data_button_reservation="来場予約" href="https://www.prostyle-residence.com/kuramae/inquiry/"><span>ご来場予約はこちら</span></a>
+
+    <a class="btn d-inline-block d-lg-none" data_button_order="資料請求" href="https://www.prostyle-residence.com/kuramae/inquiry/"><span>資料請求</span></a>
+    <a class="btn btn_reservation d-inline-block d-lg-none" id="btn_reservation" data_button_reservation="来場予約" href="https://www.prostyle-residence.com/kuramae/inquiry/"><span>来場予約</span></a>
+</section>
+
+<div class="bsnav-mobile d-sm-block">
+    <div class="bsnav-mobile-overlay"></div>
+    <div class="navbar"></div>
+</div>
---
+++ src/kuramae/index3.php
@@ -0,0 +1,225 @@
+<!doctype html>
+<html class="no-js">
+
+<head>
+    <?php require 'head.php'; ?>
+</head>
+
+<body>
+    <div id="page">
+
+        <?php include 'header4.php'; ?>
+
+        <main>
+            <section class="slide-main before-login slide-main2">
+
+                <div class="top_banner">
+                    <div class="container">
+                        <div class="bgTopBanner_top_left"></div>
+                        <div class="bgTopBanner_top_right"></div>
+                        <div class="bgTopBanner_bottom_right"></div>
+                        <div class="bgTopBanner_bottom_left"></div>
+                        <div class="row">
+                            <h2>おかげさまで「プロスタイル蔵前」は<br class="d-md-block d-none"/>完売いたしました。</h2>
+                            <h2>ただいまキャンセル待ちを受け付けております。</h2>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="slides">
+                    <div class="slideContent">
+                        <div class="slide slide-init js-slide"></div>
+                    </div>
+                </div>
+
+
+                <div class="scroll_down">
+                    <a href="#section_reservation" data-scroll class="scroll_title">Scroll</a>
+                </div>
+            </section>
+
+            <div class="news news_afterlogin news_afterlogin2">
+                <div class="row">
+                    <div class="col-12 col-lg-2 align-self-center">
+                        <h2>新着情報</h2>
+                    </div>
+                    <div class="col-12 col-lg-10 align-self-center">
+                        <div class="slick_news">
+                            <div><a href="https://staging.prostyle-residence.com/kuramae/news/">年末年始休業のお知らせ</a></div>
+                            <div><a href="https://staging.prostyle-residence.com/kuramae/news/">「PROSTYLE札幌
+                                    宮の森・東京説明会」は好評のうちに終了しました</a></div>
+                            <div><a href="https://staging.prostyle-residence.com/kuramae/news/">8月28日(土)、29日(日)「PROSTYLE札幌
+                                    宮の森・東京説明会」を開催します</a></div>
+                            <div><a href="https://staging.prostyle-residence.com/kuramae/news/">【プロスタイル札幌
+                                    宮の森】家具・インテリアオプションのご案内</a></div>
+                            <div><a href="https://staging.prostyle-residence.com/kuramae/news/">『プロスタイル札幌 宮の森』工事進捗状況</a>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <section id="section_reservation" class="section_reservation">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-12 col-lg-12">
+                            <ul class="list_maininfo">
+                                <li>
+                                    <span><span class="small_text">都営浅草線</span>「蔵前」</span>
+                                    <span class="medium_text">駅徒歩</span>
+                                    <span class="big_number">５</span>
+                                    <span class="medium_text">分</span>
+                                </li>
+                                <li>全戸無垢材フローリング</li>
+                                <li>
+                                    <span class="small_number">8</span>駅
+                                    <span class="small_number">5</span>
+                                    <span class="route"><span class="note">（※１）</span>路線利用可能なアクセシビリティ</span>
+                                </li>
+                            </ul>
+                            <!-- <img src="images/reservation_text.svg" alt="" class="img-fluid d-none d-lg-block" width="1245">
+                            <ul class="d-block d-lg-none">
+                                <li>
+                                    <img src="images/reservation_text_01.svg" alt="" class="img-fluid">
+                                </li>
+                                <li><img src="images/reservation_text_02.svg" alt="" class="img-fluid"></li>
+                                <li><img src="images/reservation_text_03.svg" alt="" class="img-fluid"></li>
+                            </ul> -->
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="section_reservation_brown section_reservation_brown2">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-12 col-lg-12">
+                            <button class="cancel">キャンセル待ちはこちらから
+                                <span class="i-rowRight-black"><img src="./images/SVG/chevron-right_black.svg" alt=""></span>
+                            </button>
+                            <p class="sub_desc">※現在は、全戸完売につき購入申込みをいただくことはできません。また、キャンセル住戸が発生しない場合もございます。</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="section_sub_banner section_sub_banner2">
+                <div class="container">
+                    <div class="bg_top_left"></div>
+                    <div class="bg_top_right"></div>
+                    <div class="bg_bottom_right"></div>
+                    <div class="bg_bottom_left"></div>
+                    <div class="row">
+                        <div class="col-12 col-lg-12">
+                            <p class="title">当物件は｢予備認定マンション｣に認定されました。</p>
+                            <div class="description">
+                                <div class="description_left">
+                                    <div>
+                                        <p>予備認定</p>
+                                        <p>マンションとは</p>
+                                    </div>
+                                </div>
+                                <div class="description_right">
+                                    <p>国土交通大臣が定めた「マンションの管理の適正化の推進を図るための基本的な方針」に基づいて、<br
+                                            class="d-none d-sm-block">令和４年4月より、新築マンションの管理計画案を認定する仕組み（予備認定）です。</p>
+                                </div>
+                            </div>
+                            <div class="interest_rate">
+                                <div class="interest_rate_left">予備認定を受けた新築マンションは一定期間【フラット35】の金利引下げが適用されます</div>
+                                <div class="interest_rate_right">
+                                    <div>
+                                        <div>
+                                            <span>【フラット35】の金利から</span>
+                                            <span>当初<h1>５</h1>年間</span>
+                                            <span>年 <h1>0.25%</h1> 引下げ</span>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <span>さらに</span>
+                                        <div>
+                                            <span>【フラット35】S (金利Bプラン)と併用した場合</span>
+                                            <span>当初<h1>10</h1>年間</span>
+                                            <span>年 <h1>0.25%</h1> 引下げ</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="container sub_description">
+                    <div class="row sub_description">
+                        <div class="col-12 col-lg-12">
+                            <ul>
+                                <li>
+                                    <p>【フラット35】維持保全型として金利引下げを適用するためには、【フラット35】維持保全型の基準に適合することを証する適合証明書の取得が必要になります。</p>
+                                    <p>詳しくは住宅金融支援機構のフラット35サイト（<a href="https://www.flat35.com/"
+                                            target="_blank">https://www.flat35.com/</a>）をご確認ください。</p>
+                                </li>
+                                <li>
+                                    <p>【フラット35】維持保全型には予算金額があり、予算金額に達する見込みとなった場合は、受付終了となります。</p>
+                                </li>
+                                <li>
+                                    <p>【フラット35】のご利用には諸条件がございます、詳細は担当者までお尋ねください。</p>
+                                </li>
+                                <li>
+                                    <p>制度の詳細は、フラット35サイト（<a href="https://www.flat35.com/"
+                                            target="_blank">https://www.flat35.com/</a>）及び公益財団法人マンション管理センター（<a
+                                            href="https://www.mankan.or.jp/11_managementplan/precert.html"
+                                            target="_blank">https://www.mankan.or.jp/11_managementplan/precert.html</a>）の
+                                    </p>
+                                    <p>ホームページ等で必ずご確認ください。</p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="section_designer section_designer2">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12 col-lg-4">
+                            <div class="box_left" data-aos="fade-up" data-aos-duration="3000">
+                                <div class="box_designer_img">
+                                    <img src="images/designer.jpg" alt="artisanshipdesign" class="img-fluid"
+                                        width="560">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-lg-8">
+                            <div class="designer_content" data-aos="fade-down" data-aos-duration="3000">
+                                <ul class="list_global">
+                                    <li><span>PROSTYLE</span></li>
+                                    <li><span>KURAMAE</span></li>
+                                    <li class="active"><span>DESIGNER</span></li>
+                                </ul>
+
+                                <div class="box_profile">
+                                    <p><small>PROFILE</small></p>
+                                    <p>株式会社シー・アンド・スタイル</p>
+                                    <h1>古賀 正章<span>Masaaki Koga</span></h1>
+                                    <p class="lh-base">設計事務所にて主に集合住宅の設計・監理業務を経験し2005年より現職。分譲マン<br>
+                                        ションのデザイン監修や、住宅リノベーションなどに携わる。</p>
+                                    <a href="#" class="btn btn-link">株式会社シー・アンド・スタイル</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <?php include('section_tel.php'); ?>
+
+        </main>
+
+        <?php require 'footer2.php'; ?>
+
+
+    </div><!-- End page -->
+
+    <?php require 'js-footer.php'; ?>
+</body>
+
+</html>
