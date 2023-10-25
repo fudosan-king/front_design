0df6ff9981acee31b41108616e2f402fff1ede0a
nicker pham <nickey3000@gmail.com>
2023-10-25 15:37:22 +0700

    update cslink

--- src/cslink/index2.php
+++ src/cslink/index2.php
@@ -21,9 +21,46 @@
             <div class="container">
                 <div class="row">
                     <div class="col-12">
-                        <div class="main_content">
+                        <div class="main_content template2">
+
+                            <div class="main_left">
+                                <aside class="box_avatar">
+                                    <a class="avatar" href="#"><img src="assets/images/avatar.png" alt="user"></a>
+                                    <h2 class="about_me_heading">山田 一郎</h2>
+                                    <p>Ichiro Yamada</p>
+                                </aside>
+
+                                <a href="#" class="btn btn_inquiry btn_inquiry2">お問い合わせ</a>
+                                <a href="#" class="btn btn_showdetail">詳細を見る <img class="bounce" src="assets/images/chevrons-down.svg" alt="chevrons down"></a>
+
+                                <div id="box_detail" class="box_detail">
+                                    <aside class="box_info">
+                                        <p class="box_logo">
+                                            <a class="logo" href="#"><img src="assets/images/vision_logo.png" alt="vision_logo"></a>
+                                            <a target="_blank" href="https://www.vision-net.co.jp/" class="linkweb">株式会社ビジョン</a>
+                                        </p>
+
+                                        <p><span>部署</span> Webプロデュース事業部</p>
+                                        <p><span>役職</span> Web Director</p>
+                                        <ul class="info">
+                                            <li><a href="tel:0368978580"><img src="assets/images/phone.svg" alt="phone"> 03-6897-8580</a></li>
+                                            <li><a href="tel:08046225219"><img src="assets/images/smartphone.svg" alt="smartphone"> +81 080‐4622‐5219</a></li>
+                                            <li><a href="mailto:saito-masayuki@propolife.co.jp"><img src="assets/images/mail.svg" alt="mail"> saito-masayuki@propolife.co.jp</a></li>
+                                        </ul>
+                                        <a href="#" class="btn btn_inquiry">お問い合わせ</a>
+                                        <p class="about_me_desc">自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります。</p>
+                                    </aside>
+
+                                    <aside class="box_social">
+                                        <a href="#" class="btn_social"><img src="assets/images/X.svg" alt="twitter logo"> twitter</a>
+                                        <a href="#" class="btn_social"><img src="assets/images/instagram.svg" alt="instagram logo"> Instaglam</a>
+                                    </aside>
+                                    <a href="#" class="btn btn_close">閉じる <img class="bounce" src="assets/images/chevrons-up.svg" alt="chevrons up"></a>
+
+                                </div>
 
 
+                            </div>
 
                             <div class="main_right template2">
                                 <article class="standard_article" data-aos="fade-up">
@@ -151,45 +188,6 @@
                                     </ul>
 
                                 </article>
-
-                            </div>
-
-                            <div class="main_left">
-                                <aside class="box_avatar">
-                                    <a class="avatar" href="#"><img src="assets/images/avatar.png" alt="user"></a>
-                                    <h2 class="about_me_heading">山田 一郎</h2>
-                                    <p>Ichiro Yamada</p>
-                                </aside>
-
-                                <a href="#" class="btn btn_inquiry btn_inquiry2">お問い合わせ</a>
-                                <a href="#" class="btn btn_showdetail">詳細を見る <img class="bounce" src="assets/images/chevrons-down.svg" alt="chevrons down"></a>
-
-                                <div id="box_detail" class="box_detail">
-                                    <aside class="box_info">
-                                        <p class="box_logo">
-                                            <a class="logo" href="#"><img src="assets/images/vision_logo.png" alt="vision_logo"></a>
-                                            <a target="_blank" href="https://www.vision-net.co.jp/" class="linkweb">株式会社ビジョン</a>
-                                        </p>
-
-                                        <p><span>部署</span> Webプロデュース事業部</p>
-                                        <p><span>役職</span> Web Director</p>
-                                        <ul class="info">
-                                            <li><a href="tel:0368978580"><img src="assets/images/phone.svg" alt="phone"> 03-6897-8580</a></li>
-                                            <li><a href="tel:08046225219"><img src="assets/images/smartphone.svg" alt="smartphone"> +81 080‐4622‐5219</a></li>
-                                            <li><a href="mailto:saito-masayuki@propolife.co.jp"><img src="assets/images/mail.svg" alt="mail"> saito-masayuki@propolife.co.jp</a></li>
-                                        </ul>
-                                        <a href="#" class="btn btn_inquiry">お問い合わせ</a>
-                                        <p class="about_me_desc">自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります。</p>
-                                    </aside>
-
-                                    <aside class="box_social">
-                                        <a href="#" class="btn_social"><img src="assets/images/X.svg" alt="twitter logo"> twitter</a>
-                                        <a href="#" class="btn_social"><img src="assets/images/instagram.svg" alt="instagram logo"> Instaglam</a>
-                                    </aside>
-                                    <a href="#" class="btn btn_close">閉じる <img class="bounce" src="assets/images/chevrons-up.svg" alt="chevrons up"></a>
-
-                                </div>
-
 
                             </div>
 
