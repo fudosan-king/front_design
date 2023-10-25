f6dbe59bd9295cfe90bddb03e9e77eed8214e8ac
nicker pham <nickey3000@gmail.com>
2023-10-25 15:19:38 +0700

    cslink

---
+++ src/cslink/index2.php
@@ -0,0 +1,211 @@
+<!doctype html>
+<html class="no-js" lang="jp">
+
+<head>
+    <?php require 'head.php'; ?>
+</head>
+
+<body>
+    <div id="page">
+        <header class="text-start">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h1>ブログタイトルが入ります</h1>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+        <div class="main">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="main_content">
+
+
+
+                            <div class="main_right template2">
+                                <article class="standard_article" data-aos="fade-up">
+                                    <h2>タイトルが入りますタイトルが入りますタイトルが入りますタイトルが入りますタイトルが入りますタイトルが入ります...</h2>
+                                    <p class="entry_date">2023.10.6</p>
+                                    <div class="standard_img">
+                                        <ul>
+                                            <li>
+                                                <p>資料名説明資料名説明</p>
+                                                <a class="btn" href="#"><img src="assets/images/download.svg" alt="download" width="14"> Downlord</a>
+                                            </li>
+                                            <li>
+                                                <p>資料名説明資料名説明資料名説明資料名<br>
+                                                    説明資料名説明資料名説明</p>
+                                                <a class="btn" href="#"><img src="assets/images/download.svg" alt="download" width="14"> Downlord</a>
+                                            </li>
+                                            <li>
+                                                <p>資料名説明資料名説明</p>
+                                                <a class="btn" href="#"><img src="assets/images/download.svg" alt="download" width="14"> Downlord</a>
+                                            </li>
+                                        </ul>
+                                        <div class="standard_post_img">
+                                            <a href="assets/images/img01.jpg" data-fancybox data-caption="img01"><img class="img-fluid" src="assets/images/img01.jpg" alt="img01"></a>
+                                        </div>
+                                    </div>
+                                    <div class="standard_content">
+                                        <p>商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト。</p>
+                                    </div>
+                                    <ul class="standard_list_video">
+                                        <li>
+                                            <div class="box_video">
+                                                <a href="assets/images/video.jpg" data-fancybox="gallery_video">
+                                                    <img class="img-fluid" src=" assets/images/video.jpg" alt="video">
+                                                </a>
+                                            </div>
+                                            <p>資料テキストが入ります資料テキストが入ります資料テキストが入ります。</p>
+                                        </li>
+                                        <li>
+                                            <div class="box_video">
+                                                <a href="#" data-fancybox="gallery_video"></a>
+                                            </div>
+                                            <p>資料テキストが入ります資料テキストが入ります資料テキストが入ります。</p>
+                                        </li>
+                                        <li>
+                                            <div class="box_video">
+                                                <a href="#" data-fancybox="gallery_video"></a>
+                                            </div>
+                                            <p>資料テキストが入ります資料テキストが入ります資料テキストが入ります。</p>
+                                        </li>
+                                        <li>
+                                            <div class="box_video">
+                                                <a href="#" data-fancybox="gallery_video"></a>
+                                            </div>
+                                            <p>資料テキストが入ります資料テキストが入ります資料テキストが入ります。</p>
+                                        </li>
+                                        <li>
+                                            <div class="box_video">
+                                                <a href="#" data-fancybox="gallery_video"></a>
+                                            </div>
+                                            <p>資料テキストが入ります資料テキストが入ります資料テキストが入ります。</p>
+                                        </li>
+                                    </ul>
+
+                                </article>
+
+                                <article class="standard_article" data-aos="fade-up">
+                                    <h2>タイトルが入りますタイトルが入りますタイトルが入りますタイトルが入りますタイトルが入りますタイトルが入ります...</h2>
+                                    <p class="entry_date">2023.10.6</p>
+                                    <div class="standard_img">
+                                        <ul>
+                                            <li>
+                                                <p>資料名説明資料名説明</p>
+                                                <a class="btn" href="#"><img src="assets/images/download.svg" alt="download" width="14"> Downlord</a>
+                                            </li>
+                                            <li>
+                                                <p>資料名説明資料名説明資料名説明資料名<br>
+                                                    説明資料名説明資料名説明</p>
+                                                <a class="btn" href="#"><img src="assets/images/download.svg" alt="download" width="14"> Downlord</a>
+                                            </li>
+                                            <li>
+                                                <p>資料名説明資料名説明</p>
+                                                <a class="btn" href="#"><img src="assets/images/download.svg" alt="download" width="14"> Downlord</a>
+                                            </li>
+                                        </ul>
+                                        <div class="standard_post_img">
+                                            <a href="assets/images/img01.jpg" data-fancybox data-caption="img01"><img class="img-fluid" src="assets/images/img01.jpg" alt="img01"></a>
+                                        </div>
+                                    </div>
+                                    <div class="standard_content">
+                                        <p>商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト。</p>
+                                    </div>
+                                    <ul class="standard_list_video">
+                                        <li>
+                                            <div class="box_video">
+                                                <a href=" assets/images/video.jpg" data-fancybox="gallery_video">
+                                                    <img class="img-fluid" src="assets/images/video.jpg" alt="video">
+                                                </a>
+                                            </div>
+                                            <p>資料テキストが入ります資料テキストが入ります資料テキストが入ります。</p>
+                                        </li>
+                                        <li>
+                                            <div class="box_video">
+                                                <a href="#" data-fancybox="gallery_video"></a>
+                                            </div>
+                                            <p>資料テキストが入ります資料テキストが入ります資料テキストが入ります。</p>
+                                        </li>
+                                        <li>
+                                            <div class="box_video">
+                                                <a href="#" data-fancybox="gallery_video"></a>
+                                            </div>
+                                            <p>資料テキストが入ります資料テキストが入ります資料テキストが入ります。</p>
+                                        </li>
+                                        <li>
+                                            <div class="box_video">
+                                                <a href="#" data-fancybox="gallery_video"></a>
+                                            </div>
+                                            <p>資料テキストが入ります資料テキストが入ります資料テキストが入ります。</p>
+                                        </li>
+                                        <li>
+                                            <div class="box_video">
+                                                <a href="#" data-fancybox="gallery_video"></a>
+                                            </div>
+                                            <p>資料テキストが入ります資料テキストが入ります資料テキストが入ります。</p>
+                                        </li>
+                                    </ul>
+
+                                </article>
+
+                            </div>
+
+                            <div class="main_left">
+                                <aside class="box_avatar">
+                                    <a class="avatar" href="#"><img src="assets/images/avatar.png" alt="user"></a>
+                                    <h2 class="about_me_heading">山田 一郎</h2>
+                                    <p>Ichiro Yamada</p>
+                                </aside>
+
+                                <a href="#" class="btn btn_inquiry btn_inquiry2">お問い合わせ</a>
+                                <a href="#" class="btn btn_showdetail">詳細を見る <img class="bounce" src="assets/images/chevrons-down.svg" alt="chevrons down"></a>
+
+                                <div id="box_detail" class="box_detail">
+                                    <aside class="box_info">
+                                        <p class="box_logo">
+                                            <a class="logo" href="#"><img src="assets/images/vision_logo.png" alt="vision_logo"></a>
+                                            <a target="_blank" href="https://www.vision-net.co.jp/" class="linkweb">株式会社ビジョン</a>
+                                        </p>
+
+                                        <p><span>部署</span> Webプロデュース事業部</p>
+                                        <p><span>役職</span> Web Director</p>
+                                        <ul class="info">
+                                            <li><a href="tel:0368978580"><img src="assets/images/phone.svg" alt="phone"> 03-6897-8580</a></li>
+                                            <li><a href="tel:08046225219"><img src="assets/images/smartphone.svg" alt="smartphone"> +81 080‐4622‐5219</a></li>
+                                            <li><a href="mailto:saito-masayuki@propolife.co.jp"><img src="assets/images/mail.svg" alt="mail"> saito-masayuki@propolife.co.jp</a></li>
+                                        </ul>
+                                        <a href="#" class="btn btn_inquiry">お問い合わせ</a>
+                                        <p class="about_me_desc">自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります。</p>
+                                    </aside>
+
+                                    <aside class="box_social">
+                                        <a href="#" class="btn_social"><img src="assets/images/X.svg" alt="twitter logo"> twitter</a>
+                                        <a href="#" class="btn_social"><img src="assets/images/instagram.svg" alt="instagram logo"> Instaglam</a>
+                                    </aside>
+                                    <a href="#" class="btn btn_close">閉じる <img class="bounce" src="assets/images/chevrons-up.svg" alt="chevrons up"></a>
+
+                                </div>
+
+
+                            </div>
+
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+    <?php require 'footer.php'; ?>
+    <?php require 'js-footer.php'; ?>
+</body>
+
+</html>
