7948ee4820f1c02232f88fd2625a502d742f3e34
nicker pham <nickey3000@gmail.com>
2023-10-26 15:36:41 +0700

    fix bug fe

--- src/cslink/index2.php
+++ src/cslink/index2.php
@@ -43,9 +43,9 @@
                                         <p><span>部署</span> Webプロデュース事業部</p>
                                         <p><span>役職</span> Web Director</p>
                                         <ul class="info">
-                                            <li><a href="tel:0368978580"><img src="assets/images/phone.svg" alt="phone"> 03-6897-8580</a></li>
-                                            <li><a href="tel:08046225219"><img src="assets/images/smartphone.svg" alt="smartphone"> +81 080‐4622‐5219</a></li>
-                                            <li><a href="mailto:saito-masayuki@propolife.co.jp"><img src="assets/images/mail.svg" alt="mail"> saito-masayuki@propolife.co.jp</a></li>
+                                            <li><img src="assets/images/phone.svg" alt="phone"> <a href="tel:0368978580">03-6897-8580</a></li>
+                                            <li><img src="assets/images/smartphone.svg" alt="smartphone"> <a href="tel:08046225219">+81 080‐4622‐5219</a></li>
+                                            <li><img src="assets/images/mail.svg" alt="mail"> <a href="mailto:saito-masayuki@propolife.co.jp">saito-masayuki@propolife.co.jp</a></li>
                                         </ul>
                                         <a href="#" class="btn btn_inquiry">お問い合わせ</a>
                                         <p class="about_me_desc">自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります自己紹介テキストが入ります。</p>
@@ -66,29 +66,34 @@
                                 <article class="standard_article" data-aos="fade-up">
                                     <h2>タイトルが入りますタイトルが入りますタイトルが入りますタイトルが入りますタイトルが入りますタイトルが入ります...</h2>
                                     <p class="entry_date">2023.10.6</p>
-                                    <div class="standard_img">
-                                        <ul>
-                                            <li>
-                                                <p>資料名説明資料名説明</p>
-                                                <a class="btn" href="#"><img src="assets/images/download.svg" alt="download" width="14"> Downlord</a>
-                                            </li>
-                                            <li>
-                                                <p>資料名説明資料名説明資料名説明資料名<br>
-                                                    説明資料名説明資料名説明</p>
-                                                <a class="btn" href="#"><img src="assets/images/download.svg" alt="download" width="14"> Downlord</a>
-                                            </li>
-                                            <li>
-                                                <p>資料名説明資料名説明</p>
-                                                <a class="btn" href="#"><img src="assets/images/download.svg" alt="download" width="14"> Downlord</a>
-                                            </li>
-                                        </ul>
-                                        <div class="standard_post_img">
-                                            <a href="assets/images/img01.jpg" data-fancybox data-caption="img01"><img class="img-fluid" src="assets/images/img01.jpg" alt="img01"></a>
+                                    <div class="standard_post_img">
+                                        <a href="assets/images/img01.jpg" data-fancybox data-caption="img01"><img class="img-fluid" src="assets/images/img01.jpg" alt="img01"></a>
+                                    </div>
+
+                                    <div class="box_reverse">
+                                        <div class="standard_content">
+                                            <p>商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト。</p>
                                         </div>
+
+                                        <div class="standard_download">
+                                            <ul>
+                                                <li>
+                                                    <p>リンク用テキストが入りますリンク用テキストが入りますリンク用テキストが入ります。</p>
+                                                    <a class="btn" href="#"><img src="assets/images/download.svg" alt="Download" width="14"> Download</a>
+                                                </li>
+                                                <li>
+                                                    <p>リンク用テキストが入りますリンク用テキストが入りますリンク用テキストが入ります。</p>
+                                                    <a class="btn" href="#"><img src="assets/images/download.svg" alt="ownload" width="14"> Download</a>
+                                                </li>
+                                                <li>
+                                                    <p>リンク用テキストが入りますリンク用テキストが入りますリンク用テキストが入ります。</p>
+                                                    <a class="btn" href="#"><img src="assets/images/Download.svg" alt="Download" width="14"> Download</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+
                                     </div>
-                                    <div class="standard_content">
-                                        <p>商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト。</p>
-                                    </div>
+
                                     <ul class="standard_list_video">
                                         <li>
                                             <div class="box_video">
@@ -129,34 +134,37 @@
                                 <article class="standard_article" data-aos="fade-up">
                                     <h2>タイトルが入りますタイトルが入りますタイトルが入りますタイトルが入りますタイトルが入りますタイトルが入ります...</h2>
                                     <p class="entry_date">2023.10.6</p>
-                                    <div class="standard_img">
-                                        <ul>
-                                            <li>
-                                                <p>資料名説明資料名説明</p>
-                                                <a class="btn" href="#"><img src="assets/images/download.svg" alt="download" width="14"> Downlord</a>
-                                            </li>
-                                            <li>
-                                                <p>資料名説明資料名説明資料名説明資料名<br>
-                                                    説明資料名説明資料名説明</p>
-                                                <a class="btn" href="#"><img src="assets/images/download.svg" alt="download" width="14"> Downlord</a>
-                                            </li>
-                                            <li>
-                                                <p>資料名説明資料名説明</p>
-                                                <a class="btn" href="#"><img src="assets/images/download.svg" alt="download" width="14"> Downlord</a>
-                                            </li>
-                                        </ul>
-                                        <div class="standard_post_img">
-                                            <a href="assets/images/img01.jpg" data-fancybox data-caption="img01"><img class="img-fluid" src="assets/images/img01.jpg" alt="img01"></a>
+                                    <div class="standard_post_img">
+                                        <a href="assets/images/img01.jpg" data-fancybox data-caption="img01"><img class="img-fluid" src="assets/images/img01.jpg" alt="img01"></a>
+                                    </div>
+
+                                    <div class="box_reverse">
+                                        <div class="standard_content">
+                                            <p>商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト。</p>
+                                        </div>
+                                        <div class="standard_download">
+                                            <ul>
+                                                <li>
+                                                    <p>リンク用テキストが入りますリンク用テキストが入りますリンク用テキストが入ります。</p>
+                                                    <a class="btn" href="#"><img src="assets/images/download.svg" alt="Download" width="14"> Download</a>
+                                                </li>
+                                                <li>
+                                                    <p>リンク用テキストが入りますリンク用テキストが入りますリンク用テキストが入ります。</p>
+                                                    <a class="btn" href="#"><img src="assets/images/download.svg" alt="ownload" width="14"> Download</a>
+                                                </li>
+                                                <li>
+                                                    <p>リンク用テキストが入りますリンク用テキストが入りますリンク用テキストが入ります。</p>
+                                                    <a class="btn" href="#"><img src="assets/images/Download.svg" alt="Download" width="14"> Download</a>
+                                                </li>
+                                            </ul>
                                         </div>
                                     </div>
-                                    <div class="standard_content">
-                                        <p>商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト商品説明テキスト。</p>
-                                    </div>
+
                                     <ul class="standard_list_video">
                                         <li>
                                             <div class="box_video">
-                                                <a href=" assets/images/video.jpg" data-fancybox="gallery_video">
-                                                    <img class="img-fluid" src="assets/images/video.jpg" alt="video">
+                                                <a href="assets/images/video.jpg" data-fancybox="gallery_video">
+                                                    <img class="img-fluid" src=" assets/images/video.jpg" alt="video">
                                                 </a>
                                             </div>
                                             <p>資料テキストが入ります資料テキストが入ります資料テキストが入ります。</p>
