c1bcdcf262d5b08b796b682be383f5095df121d7
nicker pham <nickey3000@gmail.com>
2023-12-11 11:10:51 +0700

    fdlauncher page

--- src/fdlauncher/header.php
+++ src/fdlauncher/header.php
@@ -1,5 +1,5 @@
 <header>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
             <h1><a href="index.php"><img src="assets/images/logo.svg" alt="logo"></a></h1>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -8,19 +8,19 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">圧倒的価格</a>
+                        <a class="nav-link active" aria-current="page" href="#section_price" data-scroll>圧倒的価格</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">ホームページ制作</a>
+                        <a class="nav-link" href="#section_plan" data-scroll>ホームページ制作</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">導入フロー</a>
+                        <a class="nav-link" href="#section_flow" data-scroll>導入フロー</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">FAQ</a>
+                        <a class="nav-link" href="#section_faq" data-scroll>FAQ</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#"><img src="assets/images/i_edit.svg" alt="i_edit" width="18"> 資料請求・お問合せ</a>
+                        <a class="nav-link" href="#"><i></i> 資料請求・お問合せ</a>
                     </li>
                 </ul>
             </div>
