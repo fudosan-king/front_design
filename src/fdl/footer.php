304812d88d39ad7a1571b8d447206ef15399657d
nicker pham <nickey3000@gmail.com>
2023-12-21 10:42:06 +0700

    fdl

---
+++ src/fdl/footer.php
@@ -0,0 +1,32 @@
+<footer>
+    <ul class="list_top">
+        <li><a href="#">売買物件</a></li>
+        <li><a href="#">static_pages1のName</a></li>
+        <li><a href="#">無料査定</a></li>
+        <li><a href="#">アクセス</a></li>
+        <li><a href="#">ブログ</a></li>
+        <li><a href="#">会社概要</a></li>
+        <li><a href="#">スタッフ</a></li>
+    </ul>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-lg-3 text-center text-lg-end">
+                <ul>
+                    <li>LogKnot株式会社</li>
+                    <li>〒1070061</li>
+                    <li>Kita-Aoyama</li>
+                </ul>
+            </div>
+            <div class="col-12 col-lg-3 text-center">
+                <a class="logo_footer" href="#"><img class="img-fluid" src="assets/images/logknot.svg" alt="logknot" width="100"></a>
+            </div>
+            <div class="col-12 col-lg-3 text-center text-lg-start">
+                <ul>
+                    <li>営業時間　09:30〜18:00</li>
+                    <li>定休日　土・日・祝</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <p>Copyright © <a href="index.php">LogKnot株式会社</a> INC. ALL rights reserved.</p>
+</footer>
---
+++ src/fdl/index.php
@@ -0,0 +1,257 @@
+<!doctype html>
+<html class="no-js" lang="jp">
+
+<head>
+    <?php require 'head.php'; ?>
+</head>
+
+<body>
+    <div id="page">
+        <?php include 'header.php'; ?>
+
+        <section class="section_banner">
+            <div class="carousel" data-flickity='{ "prevNextButtons": false, "pageDots": false, "autoPlay": true, "pauseAutoPlayOnHover": false, "wrapAround": true }'>
+                <div class="carousel-cell"><img src="assets/images/uploads/banner01.jpg" alt="banner01"></div>
+                <div class="carousel-cell"><img src="assets/images/uploads/banner02.jpg" alt="banner02"></div>
+                <div class="carousel-cell"><img src="assets/images/uploads/banner03.jpg" alt="banner03"></div>
+            </div>
+        </section>
+
+        <section class="section_products">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <p class="descriptions">ここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入ります</p>
+
+                        <div class="tab-wrap">
+
+                            <input type="radio" id="tab1" name="tabGroup1" class="tab" checked>
+                            <label for="tab1">おすすめ（新築一戸建）</label>
+
+                            <input type="radio" id="tab2" name="tabGroup1" class="tab">
+                            <label for="tab2">おすすめ（中古一戸建）</label>
+
+                            <input type="radio" id="tab3" name="tabGroup1" class="tab">
+                            <label for="tab3">おすすめ（マンション）</label>
+
+                            <input type="radio" id="tab4" name="tabGroup1" class="tab">
+                            <label for="tab4">おすすめ（土地）</label>
+
+
+                            <div class="tab__content">
+                                <div class="top_panel">
+                                    <p>ここにテキストが入りますここにテキストが入ります</p>
+                                    <a href="#" class="btn">ここにテキストが入</a>
+                                </div>
+                                <ul class="list_pro">
+                                    <li>
+                                        <div class="top_pro">
+                                            <div class="pro_img">
+                                                <a href="#"><img class="img-fluid" src="https://t4.ftcdn.net/jpg/06/13/17/13/240_F_613171394_Uwg21ImBariUnGVVAvKHBdNxGCyJ6nZA.jpg" alt="demo images"></a>
+                                            </div>
+                                            <span>NEW</span>
+                                        </div>
+                                        <div class="pro_content">
+                                            <h2><span>多摩市和２</span> <span>新築一戸建</span></h2>
+                                            <p>ここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入ります</p>
+                                            <a href="#" class="btn btn_detail">ここにテキ</a>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="top_pro">
+                                            <div class="pro_img">
+                                                <a href="#"><img class="img-fluid" src="https://t3.ftcdn.net/jpg/06/28/85/80/240_F_628858060_VQZbeMHDbFQI3HMBQ0WpeZBUYTTuJpK5.jpg" alt="demo images"></a>
+                                            </div>
+                                            <span>NEW</span>
+                                        </div>
+                                        <div class="pro_content">
+                                            <h2><span>多摩市和２</span> <span>新築一戸建</span></h2>
+                                            <p>ここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入ります</p>
+                                            <a href="#" class="btn btn_detail">ここにテキ</a>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="top_pro">
+                                            <div class="pro_img">
+                                                <a href="#"><img class="img-fluid" src="https://t4.ftcdn.net/jpg/06/18/71/57/240_F_618715797_mWet5Cy5Ao97wcQf9M2Cqizve1DtYKx5.jpg" alt="demo images"></a>
+                                            </div>
+                                            <span>NEW</span>
+                                        </div>
+                                        <div class="pro_content">
+                                            <h2><span>多摩市和２</span> <span>新築一戸建</span></h2>
+                                            <p>ここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入ります</p>
+                                            <a href="#" class="btn btn_detail">ここにテキ</a>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="top_pro">
+                                            <div class="pro_img">
+                                                <a href="#"><img class="img-fluid" src="https://t4.ftcdn.net/jpg/06/27/13/83/240_F_627138363_fsgQv1F32b1mlrmNEEMe5Aj5p7ZkLcNA.jpg" alt="demo images"></a>
+                                            </div>
+                                            <span>NEW</span>
+                                        </div>
+                                        <div class="pro_content">
+                                            <h2><span>多摩市和２</span> <span>新築一戸建</span></h2>
+                                            <p>ここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入ります</p>
+                                            <a href="#" class="btn btn_detail">ここにテキ</a>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="top_pro">
+                                            <div class="pro_img">
+                                                <a href="#"><img class="img-fluid" src="https://t3.ftcdn.net/jpg/06/35/19/70/240_F_635197098_CnUouHxBW9FxGGSMchaCU5JZHE6z59Pa.jpg" alt="demo images"></a>
+                                            </div>
+                                            <span>NEW</span>
+                                        </div>
+                                        <div class="pro_content">
+                                            <h2><span>多摩市和２</span> <span>新築一戸建</span></h2>
+                                            <p>ここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入ります</p>
+                                            <a href="#" class="btn btn_detail">ここにテキ</a>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="top_pro">
+                                            <div class="pro_img">
+                                                <a href="#"><img class="img-fluid" src="https://t3.ftcdn.net/jpg/06/29/70/64/240_F_629706489_17eBWiOd0kDwztR2hUekQke0dDqYWlCC.jpg" alt="demo images"></a>
+                                            </div>
+                                            <span>NEW</span>
+                                        </div>
+                                        <div class="pro_content">
+                                            <h2><span>多摩市和２</span> <span>新築一戸建</span></h2>
+                                            <p>ここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入ります</p>
+                                            <a href="#" class="btn btn_detail">ここにテキ</a>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="top_pro">
+                                            <div class="pro_img">
+                                                <a href="#"><img class="img-fluid" src="https://t3.ftcdn.net/jpg/06/45/00/72/240_F_645007225_SZo7K3mP25H5JZHKfGaITriC2Ln4DzVL.jpg" alt="demo images"></a>
+                                            </div>
+                                            <span>NEW</span>
+                                        </div>
+                                        <div class="pro_content">
+                                            <h2><span>多摩市和２</span> <span>新築一戸建</span></h2>
+                                            <p>ここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入ります</p>
+                                            <a href="#" class="btn btn_detail">ここにテキ</a>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="top_pro">
+                                            <div class="pro_img">
+                                                <a href="#"><img class="img-fluid" src="https://t4.ftcdn.net/jpg/03/01/36/39/240_F_301363949_v1vptE9oCAFkFxN9LIKqWCTXfGQ6sfCe.jpg" alt="demo images"></a>
+                                            </div>
+                                            <span>NEW</span>
+                                        </div>
+                                        <div class="pro_content">
+                                            <h2><span>多摩市和２</span> <span>新築一戸建</span></h2>
+                                            <p>ここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入ります</p>
+                                            <a href="#" class="btn btn_detail">ここにテキ</a>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+
+                            <div class="tab__content">
+                                <h3>おすすめ（中古一戸建）</h3>
+                                <p>ここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入ります</p>
+                                <p>ここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入ります</p>
+                            </div>
+
+                            <div class="tab__content">
+                                <h3>おすすめ（マンション）</h3>
+                                <p>ここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入ります</p>
+                                <p>ここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入ります</p>
+                                <p>ここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入ります</p>
+                                <p>ここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入ります</p>
+                            </div>
+
+                            <div class="tab__content">
+                                <h3>おすすめ（土地）</h3>
+                                <p>ここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入ります</p>
+                                <p>ここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入ります</p>
+                                <p>ここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入ります</p>
+                                <p>ここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入ります</p>
+                                <p>ここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入ります</p>
+                                <p>ここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入りますここにテキストが入ります</p>
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="section_news">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="top_news">
+                            <h2>NEWS</h2>
+                            <p><a href="#">新着情報</a></p>
+                        </div>
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    <h3>2015年6月28日</h3>
+                                    <p>年末年始営業時間のお知らせ</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <h3>2015年6月28日</h3>
+                                    <p>年末年始営業時間のお知らせ</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <h3>2015年6月28日</h3>
+                                    <p>年末年始営業時間のお知らせ</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <h3>2015年6月28日</h3>
+                                    <p>年末年始営業時間のお知らせ</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <h3>2015年6月28日</h3>
+                                    <p>年末年始営業時間のお知らせ</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <h3>2015年6月28日</h3>
+                                    <p>年末年始営業時間のお知らせ</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="section_contacts">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <ul>
+                            <li><a href="#"><img class="img-fluid" src="assets/images/i_email.svg" alt="i_email" width="30"></a></li>
+                            <li><a href="#"><img class="img-fluid" src="assets/images/i_map.svg" alt="i_map" width="32"></a></li>
+                            <li><a href="#"><img class="img-fluid" src="assets/images/i_home.svg" alt="i_home" width="33"></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+    </div>
+
+    <?php require 'footer.php'; ?>
+    <?php require 'js-footer.php'; ?>
+</body>
+
+</html>
