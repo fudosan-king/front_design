e7b527ca07a86c00c6b75a1a08e59531eaf2df5b
nicker pham <nickey3000@gmail.com>
2023-12-27 09:40:58 +0700

    ....

--- src/vision/js/functions.js
+++ src/vision/js/functions.js
@@ -71,6 +71,23 @@ jQuery(document).ready(function ($) {
     language: 'ja',
   });
 
+  $('.attachment input[type="file"]')
+    .on('change', (event) => {
+      let el = $(event.target).closest('.attachment').find('.btn-file');
+
+      el
+        .find('.btn-file__actions__item')
+        .css({
+          'padding': '135px'
+        });
+
+      el
+        .find('.btn-file__preview')
+        .css({
+          'background-image': 'url(' + window.URL.createObjectURL(event.target.files[0]) + ')'
+        });
+    });
+
 
 }(jQuery));
 
