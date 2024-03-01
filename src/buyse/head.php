c85fadd44f7f2b7abfa1983f561fc158619c66b1
nicker pham <nickey3000@gmail.com>
2024-03-01 13:23:31 +0700

    buyse

---
+++ src/buyse/head.php
@@ -0,0 +1,28 @@
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta charset="UTF-8">
+<title>FD Launcher</title>
+<meta content="" name="description">
+<meta content="" name="keywords">
+<meta property="og:site_name" content="">
+<meta property="og:url" content="">
+<meta property="og:description" content="">
+
+<link rel="apple-touch-icon" sizes="152x152" href="favicon_package_v0.16/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="favicon_package_v0.16/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="favicon_package_v0.16/favicon-16x16.png">
+<link rel="manifest" href="favicon_package_v0.16/site.webmanifest">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#ffffff">
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP&display=swap" rel="stylesheet">
+<link href="https://fonts.cdnfonts.com/css/sf-pro-display" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.0/animate.min.css" type="text/css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.css">
+<link rel="stylesheet" href="assets/scss/styles.css" type="text/css">
+<link rel="stylesheet" href="assets/css/be_styles.css" type="text/css">
---
+++ src/buyse/index.php
@@ -0,0 +1,249 @@
+<!doctype html>
+<html class="no-js" lang="jp">
+
+<head>
+    <?php require 'head.php'; ?>
+</head>
+
+<body>
+    <div id="page">
+        <?php include 'header.php'; ?>
+
+        <section class="section_banner">
+            <div class="container h-100">
+                <div class="row h-100">
+                    <div class="col-12 col-lg-8 align-self-center">
+                        <div class="banner_content">
+                            <ul data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2000">
+                                <li><a href="#"><span>港区</span></a></li>
+                                <li><a href="#"><span>渋谷区</span></a></li>
+                                <li><a href="#"><span>千代田区</span></a></li>
+                                <li><a href="#"><span>目黒区</span></a></li>
+                                <li><a href="#"><span>新宿区</span></a></li>
+                                <li><a href="#"><span>文京区</span></a></li>
+                            </ul>
+                            <h2 data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="1000">東京都心の<br>
+                                マンション高額買取に<br>
+                                自信があります</h2>
+                        </div>
+                    </div>
+                    <div class="col-12 col-lg-4 align-self-end align-self-lg-center">
+                        <div class="box_polygons" data-aos="zoom-in-right" data-aos-duration="4000">
+                            <img src="assets/images/i_polygons.svg" alt="i_polygons" class="img-fluid">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="trackrecord">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <p><small>※買取サービスを実施するグループ企業の不動産分譲会社の実績です</small></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="w_salesconsultation" data-aos="fade-left" data-aos-anchor="#example-anchor" data-aos-offset="500" data-aos-duration="2000">
+                <a href="#" class="box_salesconsultation">
+                    <span>広い物件も<br>強化買取中</span>
+                    <span>無料査定・売却相談 <img class="img-fluid" src="assets/images/i_arrowright.svg" alt="i_arrowright" width="10"></span>
+                </a>
+                <a class="btn_close" href="#"><img class="img-fluid" src="assets/images/i_close.svg" alt="i_arrowright" width="14"></a>
+            </div>
+        </section>
+
+        <div class="w_consultation d-block d-lg-none">
+            <a href="#" class="btn btn_consultation"><img src="assets/images/i_email.svg" alt="icon email" width="12"> 査定依頼・ご相談</a>
+        </div>
+
+        <div id="section_reasons" class="section_reasons">
+            <h2 data-aos="fade-down" data-aos-easing="linear" data-aos-duration="3500">早く高く買い取れる<br>3つの理由</h2>
+            <section>
+                <div class="container-fluid p-0">
+                    <div class="row g-0">
+                        <div class="col-12">
+                            <div class="w_contentreasons">
+                                <div class="box_img" data-aos="fade-down">
+                                    <img class="img-fluid" src="assets/images/box01.jpg" alt="box01">
+                                </div>
+                                <div class="box_content" data-aos="fade-up">
+                                    <h3>
+                                        <span class="number"><img class="img-fluid" src="assets/images/01.svg" alt="01"></span>
+                                        <span>東京都心を中心に<br>1700件超の豊富な買取実績</span>
+                                    </h3>
+                                    <p>1700件を超える買取実績と都心のマンション専門ならではの経験と独自のノウハウを活かして高額な買取金額をご提案。<br>
+                                        ビンテージマンションにも強く、築年数問わず買い取ります。</span></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section>
+                <div class="container-fluid p-0">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="w_contentreasons">
+                                <div class="box_content" data-aos="fade-up">
+                                    <h3>
+                                        <span class="number"><img class="img-fluid" src="assets/images/02.svg" alt="02"></span>
+                                        <span>直接買取なので<br><i>仲介手数料が不要</i></span>
+                                    </h3>
+                                    <h4>直接買取の場合は、仲介手数料が不要です。</h4>
+                                    <p>豊富な買取実績データにより、ご提供いただいた物件情報を元にスピーディで的確な査定が可能です。最短当日に査定金額をご提示いたします。</p>
+                                </div>
+                                <div class="box_img" data-aos="fade-down">
+                                    <img class="img-fluid" src="assets/images/box02.jpg" alt="box02">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section>
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-12 p-0">
+                            <div class="w_contentreasons">
+                                <div class="box_img" data-aos="fade-down">
+                                    <img class="img-fluid" src="assets/images/box03.jpg" alt="box03">
+                                </div>
+                                <div class="box_content" data-aos="fade-up">
+                                    <h3>
+                                        <span class="number"><img class="img-fluid" src="assets/images/03.svg" alt="03"></span>
+                                        <span>最短当日査定可能</span>
+                                    </h3>
+                                    <p>豊富な買取実績データにより、ご提供いただいた物件情報を元にスピーディで的確な査定が可能です。最短当日に査定金額をご提示いたします。</p>
+                                    <p><small>※価格確定は現地調査後になります</small></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+        </div>
+
+        <section class="section_contact d-none d-lg-block">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <p>マンション買取のことならバイスまでお気軽にご相談ください</p>
+                        <a target="_blank" href="assessment.php"><img class="img-fluid" src="assets/images/sales_consultation.svg" alt="sales consultation"></a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="section_advantagespurchasing" class="section_advantagespurchasing">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h2 class="title" data-aos="fade-up">買取のメリット</h2>
+                        <ul>
+                            <li data-aos="zoom-in-left" data-aos-duration="2000">
+                                <h3><img class="img-fluid" src="assets/images/i_sell.svg" alt="icon sales"> 周囲に知られずに売却</h3>
+                                <p>宣伝や広告活動を行いませんので、周囲に知られずに売却することが可能です。</p>
+                            </li>
+                            <li data-aos="zoom-in-right" data-aos-duration="2000">
+                                <h3><img class="img-fluid" src="assets/images/i_transactions.svg" alt="icon transactions" width="73"> スピーディな取引</h3>
+                                <p>マンション買取は、直接買い手が不動産会社からマンションを購入する仕組みです。そのため、仲介業者を介する必要がなく、取引が速やかに進みます。</p>
+                            </li>
+                            <li data-aos="zoom-in-left" data-aos-duration="2000">
+                                <h3><img class="img-fluid" src="assets/images/i_unnecessaryitems.svg" alt="unnecessary items"> 不用品はそのままでOK</h3>
+                                <p>家具・家電や生活用品などの不用品も現状のままでご売却可能です。クリーニング・リフォームは必要ありませんのでご安心ください。</p>
+                            </li>
+                            <li data-aos="zoom-in-right" data-aos-duration="2000">
+                                <div class="title_sm">
+                                    <h3><img class="img-fluid" src="assets/images/i_contract.svg" alt="icon contract"> 契約不適合責任の免責</h3>
+                                    <h4>(売却後に負う修繕責任がありません)</h4>
+                                </div>
+                                <p>給排水管の亀裂など多少の瑕疵がある物件でも、問題なく買取ます。</p>
+                            </li>
+
+                            <li data-aos="zoom-in-left" data-aos-duration="2000">
+                                <h3><img class="img-fluid" src="assets/images/i_viewings.svg" alt="icon viewings"> 内見回数は1回のみ</h3>
+                                <p>不動産会社が迅速に査定し、買取の意思を示すため、煩わしい内見回数は1回のみ。手間がかからずに効率的な取引ができます。 </p>
+                            </li>
+                            <li data-aos="zoom-in-right" data-aos-duration="2000">
+                                <h3><img class="img-fluid" src="assets/images/i_price.svg" alt="icon price" width="60"> 早い査定価格の確定</h3>
+                                <p>当日に価格をだします。不動産会社が直接買取るため、査定価格が早い段階で確定。買い手との価格交渉が少ないため、安心して取引できます。</p>
+                            </li>
+                            <li data-aos="zoom-in-left" data-aos-duration="2000">
+                                <h3><img class="img-fluid" src="assets/images/i_procedure.svg" alt="icon procedure" width="60"> 手続きが簡単</h3>
+                                <p>仲介手数料や広告費用が不要なので、手続きがシンプル。<br>売主は手間をかけずにスムーズな売却が可能です。</p>
+                            </li>
+                            <li data-aos="zoom-in-right" data-aos-duration="2000">
+                                <h3><img class="img-fluid" src="assets/images/i_deliverytime.svg" alt="icon deliverytime"> 引き渡し時期も選べる</h3>
+                                <p>新居の入居時期など、売主様の予定を可能な限り優先します。</p>
+                            </li>
+                            <li data-aos="zoom-in-left" data-aos-duration="2000">
+                                <h3><img class="img-fluid" src="assets/images/i_riskreduction.svg" alt="icon riskreduction"> リスク低減</h3>
+                                <p>買い手が確定しているため、取引が成立しないリスクが低減。安定感があります。</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="section_condominiumpurchase" class="section_condominiumpurchase">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h2 class="title" data-aos="fade-up">マンション買取の流れ</h2>
+                        <ul class="steps" data-aos="fade-up" data-aos-duration="3000">
+                            <li>
+                                <a href="#" title="">
+                                    <h3>Step.1</h3>
+                                    <p><img class="img-fluid" src="assets/images/i_procedure.svg" alt="icon procedure" width="50"></p>
+                                    <h4>査定•買取金額確認</h4>
+                                    <p>訪問査定・現地調査後に<br>買取金額の提示</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" title="">
+                                    <h3>Step.2</h3>
+                                    <p><img class="img-fluid" src="assets/images/i_transactions.svg" alt="icon transactions" width="60"></p>
+                                    <h4>売買契約締結</h4>
+                                    <p>買取金額に合意したら契約</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" title="">
+                                    <h3>Step.3</h3>
+                                    <p><img class="img-fluid" src="assets/images/i_price.svg" alt="icon price" width="58"></p>
+                                    <h4>引き渡し•決済</h4>
+                                    <p>代金の受領、<br>マンションの引き渡し</p>
+                                </a>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="section_contact">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <p>マンション買取のことなら <span class="d-block d-lg-inline">バイスまでお気軽にご相談ください</span></p>
+                        <a target="_blank" href="assessment.php"><img class="img-fluid" src="assets/images/sales_consultation.svg" alt="sales consultation"></a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+    </div>
+
+    <?php require 'footer.php'; ?>
+    <?php require 'js-footer.php'; ?>
+</body>
+
+</html>
