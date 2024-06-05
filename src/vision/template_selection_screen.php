52da9989edda80744877b442426e54d081e7425a
nicker pham <nickey3000@gmail.com>
2024-06-05 08:28:13 +0700

    update input type for menu item

--- src/vision/template_selection_screen.php
+++ src/vision/template_selection_screen.php
@@ -68,14 +68,21 @@
                         </form>
                     </li>
                     <li>
-                        <a class="dropdown-item" href="#">
+                        <input type="file" name="file-input" id="file-input" class="file-input__input">
+
+
+
+                        <label class="file-input__label" for="file-input">
+                            <!-- <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="upload" class="svg-inline--fa fa-upload fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                <path fill="currentColor" d="M296 384h-80c-13.3 0-24-10.7-24-24V192h-87.7c-17.8 0-26.7-21.5-14.1-34.1L242.3 5.7c7.5-7.5 19.8-7.5 27.3 0l152.2 152.2c12.6 12.6 3.7 34.1-14.1 34.1H320v168c0 13.3-10.7 24-24 24zm216-8v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h136v8c0 30.9 25.1 56 56 56h80c30.9 0 56-25.1 56-56v-8h136c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z"></path>
+                            </svg> -->
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
                                 <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
                                 <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
                             </svg>&nbsp;
-                            アカウント画像変更
-                        </a>
-                    </li>
+                            <span>アカウント画像変更</span>
+                        </label </li>
+
                     <li>
                         <a class="btn d-none d-sm-block signout dropdown-item" data-bs-toggle="modal" data-bs-target="#signout" style="display: flex;align-items: center;">
                             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="black" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
