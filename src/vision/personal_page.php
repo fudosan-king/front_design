18a584aa421ea34e4a573f86f2a6d21b0d0deffb
nicker pham <nickey3000@gmail.com>
2024-07-08 13:47:25 +0700

    ...

--- src/vision/personal_page.php
+++ src/vision/personal_page.php
@@ -6,7 +6,7 @@
 </head>
 
 <body id="body-pd">
-    <header class="header header_personal" id="header">
+    <!-- <header class="header header_personal" id="header">
         <h1>個人ページ</h1>
         <div class="header_right">
             <p class="logo_company">ロゴ・社名が入ります</p>
@@ -25,6 +25,100 @@
                 </ul>
             </div>
         </div>
+    </header> -->
+
+    <header class="header" id="header">
+        <h1>
+            <a href="http://18.179.82.28/watashiga2/admin/dashboard" class="logo"><img src="assets/images/logo_white.svg" style="vertical-align:baseline" width="200px" alt="header_logo"></a>
+        </h1>
+        <div class="header_right">
+            <a href="http://18.179.82.28/watashiga2/admin/dashboard" class="logo"><img src="assets/images/50yLR36v5uZ9WiRUmgJgPZIrNOPhi32tMeuwxrFp.png" width="123" alt="header_logo"></a>
+
+
+            <div class="header_avatar" style="position: relative; margin-right: 0;">
+                <div class="avatar_info avatar_name">
+                    <p>VISION11</p>
+                </div>
+                <a class="dropdown-toggle btn_dropdown dropdown-companies" data-bs-toggle="dropdown" id="dropdownMenuButtonCompnay" aria-expanded="false">
+                    <div class="avatar_info">
+                        <p>LOGKNOT LOGKNOT</p>
+                    </div>
+                </a>
+
+                <ul class="dropdown-menu">
+                    <li>
+                        <form>
+                            <input type="hidden" name="_token" value="TJDNtbapbesAjKbMpS1NDzZZcaJgXwhpNtlwbi6R"> <a class="dropdown-item" href="http://18.179.82.28/watashiga2/admin/companies/1/selected">VISION</a>
+                            <a class="dropdown-item" href="http://18.179.82.28/watashiga2/admin/companies/2/selected">LOGKNOT</a>
+                            <a class="dropdown-item" href="http://18.179.82.28/watashiga2/admin/companies/4/selected">ACD</a>
+                            <a class="dropdown-item" href="http://18.179.82.28/watashiga2/admin/companies/5/selected">PHONG TEST COMPANY</a>
+                            <a class="dropdown-item" href="http://18.179.82.28/watashiga2/admin/companies/6/selected">ASD</a>
+                        </form>
+                    </li>
+                </ul>
+
+
+
+            </div>
+
+
+            <div class="header_avatar header_avatar_right">
+
+
+                <a class="dropdown-toggle btn_dropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                    <span class="avatar_img">
+                        <img src="assets/images/qq1DPIKMiWn0e156lsxV74RkOFV3Xqb6IUU8bita.jpg" alt="avatar">
+                    </span>
+
+                    <div class="avatar_info avatar_name">
+                        <span>管理者</span>
+                        <p>administrator</p>
+                    </div>
+                </a>
+
+                <ul class="dropdown-menu">
+                    <li>
+                        <form>
+                            <input type="hidden" name="_token" value="TJDNtbapbesAjKbMpS1NDzZZcaJgXwhpNtlwbi6R">
+                            <a class="dropdown-item" data-toggle="modal" data-target="#changepassword">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gear" viewBox="0 0 16 16">
+                                    <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z"></path>
+                                    <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z"></path>
+                                </svg>&nbsp;
+                                パスワード変更</a>
+                        </form>
+                    </li>
+                    <li>
+                        <input type="file" name="file-input" id="file-input" class="file-input__input">
+
+
+
+                        <label class="file-input__label" for="file-input">
+                            <!-- <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="upload" class="svg-inline--fa fa-upload fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                <path fill="currentColor" d="M296 384h-80c-13.3 0-24-10.7-24-24V192h-87.7c-17.8 0-26.7-21.5-14.1-34.1L242.3 5.7c7.5-7.5 19.8-7.5 27.3 0l152.2 152.2c12.6 12.6 3.7 34.1-14.1 34.1H320v168c0 13.3-10.7 24-24 24zm216-8v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h136v8c0 30.9 25.1 56 56 56h80c30.9 0 56-25.1 56-56v-8h136c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z"></path>
+                            </svg> -->
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                                <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
+                            </svg>&nbsp;
+                            <span>アカウント画像変更</span>
+                        </label>
+                    </li>
+
+                    <li>
+                        <a class="btn d-none d-sm-block signout dropdown-item" data-bs-toggle="modal" data-bs-target="#signout" style="display: flex;align-items: center;">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="black" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"></path>
+                                <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"></path>
+                            </svg>&nbsp;サインアウト
+                        </a>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+
+
     </header>
 
     <div class="l-navbar" id="nav-bar">
