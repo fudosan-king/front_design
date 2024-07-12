0e7504d1c29aaeb3553c237858bf28300bdabfdd
nicker pham <nickey3000@gmail.com>
2024-07-12 12:59:53 +0700

    ...

--- src/realestates/header.php
+++ src/realestates/header.php
@@ -4,7 +4,7 @@
             <div class="col-12">
                 <h1>
                     <a href="index.php">
-                        <img class="img-fluid" src="assets/images/logo.png" alt="高額不動産買取専門 東京マンションオールディーズ" width="320">
+                        <img class="img-fluid" src="assets/images/logo.svg" alt="高額不動産買取専門 東京マンションオールディーズ" width="244">
                     </a>
                 </h1>
             </div>
