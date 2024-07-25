3bba8464441c481d770e9b7fdb2719ac3d6922c0
nicker pham <nickey3000@gmail.com>
2024-07-25 16:46:06 +0700

    ...

--- src/chinoblog/menu_sub.php
+++ src/chinoblog/menu_sub.php
@@ -1,211 +1,55 @@
-<!-- <div class="menu-list" style="display: block;">
+<div class="menu_list">
     <div class="container-fluid">
-        <ul class="navbar navbar_text d-flex d-lg-none">
-            <li class="nav-item">
-                <a class="nav-link" href="#">販売予定・販売中物件</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">分譲実績</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">中古マンション</a>
-            </li>
-        </ul>
-        <ul class="navbar social-list d-flex d-lg-none">
-            <li class="nav-item">
-                <a class="nav-link" href="https://www.facebook.com/prostyleresidence/">
-                    <img src="assets/images/i_fb.svg" alt="facebook" title="facebook">
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="https://mobile.twitter.com/PROSTYLE_OA">
-                    <img src="assets/images/i_x.svg" alt="X" title="X">
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="https://www.instagram.com/prostyle.official">
-                    <img src="assets/images/i_ins.svg" alt="instagram" title="instagram">
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="https://www.youtube.com/channel/UC1UquQQDwtU6VfKq3pwM4Hg">
-                    <img src="assets/images/i_youtube.svg" alt="youtube" title="youtube">
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="https://lin.ee/8dqmWKo">
-                    <img src="common/icon-line-black.svg" alt="line" title="line" width="17">
-                </a>
-            </li>
-            <li>
-                <a class="nav-link" href="https://www.prostyle-residence.com/contact/" target="">
-                    <span class="btn-blog i-list_btn">お問い合わせ</span>
-                </a>
-            </li>
-
-        </ul>
-    </div>
-</div> -->
-
-<div class="menu-list">
-    <div class="container-fluid">
-        <a class="navbar-brand d-block d-lg-none" href="https://www.prostyle-residence.com/mokusiru/">
-            <img class="img-fuild" alt="MOKUSIRU BLOG logo" src="common/logo_yoko.png" title="">
-        </a>
-
-        <nav class="navbar navbar-expand-lg d-block d-sm-none"><span id="" class="uag-toc__heading-anchor"></span><span id="" class="uag-toc__heading-anchor"></span><span id="" class="uag-toc__heading-anchor"></span><span id="" class="uag-toc__heading-anchor"></span><span id="" class="uag-toc__heading-anchor"></span><span id="" class="uag-toc__heading-anchor"></span><span id="" class="uag-toc__heading-anchor"></span><span id="" class="uag-toc__heading-anchor"></span><span id="" class="uag-toc__heading-anchor"></span><span id="" class="uag-toc__heading-anchor"></span><span id="" class="uag-toc__heading-anchor"></span>
-            <h3 class="d-block d-lg-none">＃ジャンル</h3>
-            <ul class="navbar-nav ">
-                <li class="nav-item menu-parent_item">
-                    <a href="https://www.prostyle-residence.com/mokusiru/category/%e6%96%b0%e7%af%89%e5%8f%96%e6%89%b1%e3%81%84%e7%89%a9%e4%bb%b6/" class="nav-link js-collapse">
-                        <span>＃新築マンション</span>
-                    </a>
-                </li>
-                <li class="nav-item menu-parent_item">
-                    <a href="https://www.prostyle-residence.com/mokusiru/category/life-style/" class="nav-link js-collapse">
-                        <span>＃ライフスタイル</span>
-                    </a>
-                </li>
-                <li class="nav-item menu-parent_item">
-                    <a href="https://www.prostyle-residence.com/mokusiru/category/city/" class="nav-link js-collapse">
-                        <span>＃街</span>
-                    </a>
-                </li>
-                <li class="nav-item menu-parent_item">
-                    <a href="https://www.prostyle-residence.com/mokusiru/category/facility-construction/" class="nav-link js-collapse">
-                        <span>＃設備・構造</span>
-                    </a>
-                </li>
-                <li class="nav-item menu-parent_item">
-                    <a href="https://www.prostyle-residence.com/mokusiru/category/money/" class="nav-link js-collapse">
-                        <span>＃お金の知識</span>
-                    </a>
-                </li>
-                <li class="nav-item menu-parent_item">
-                    <a href="https://www.prostyle-residence.com/mokusiru/category/hobby/" class="nav-link js-collapse">
-                        <span>＃趣味</span>
-                    </a>
-                </li>
-                <li class="nav-item menu-parent_item">
-                    <a href="https://www.prostyle-residence.com/mokusiru/category/solid-wood/" class="nav-link js-collapse">
-                        <span>＃無垢材</span>
-                    </a>
-                </li>
-                <li class="nav-item menu-parent_item">
-                    <a href="https://www.prostyle-residence.com/mokusiru/category/gourmet/" class="nav-link js-collapse">
-                        <span>＃グルメ</span>
-                    </a>
-                </li>
-            </ul><span id="" class="uag-toc__heading-anchor"></span><span id="" class="uag-toc__heading-anchor"></span><span id="" class="uag-toc__heading-anchor"></span><span id="" class="uag-toc__heading-anchor"></span><span id="" class="uag-toc__heading-anchor"></span><span id="" class="uag-toc__heading-anchor"></span><span id="" class="uag-toc__heading-anchor"></span><span id="" class="uag-toc__heading-anchor"></span><span id="" class="uag-toc__heading-anchor"></span><span id="" class="uag-toc__heading-anchor"></span><span id="" class="uag-toc__heading-anchor"></span>
-            <h3 class="d-block d-lg-none">現在公開中◇物件公式サイト</h3>
-            <ul class="navbar-nav ">
-                <li class="nav-item menu-parent_item">
-                    <a href="https://www.prostyle-residence.com/kuramae/" class="nav-link js-collapse">
-                        <span>＃プロスタイル蔵前</span>
-                    </a>
-                </li>
-                <li class="nav-item menu-parent_item">
-                    <a href="https://www.prostyle-residence.com/sapporo/miyanomori/" class="nav-link js-collapse">
-                        <span>＃プロスタイル札幌 宮の森</span>
-                    </a>
-                </li>
-            </ul><span id="contact" class="uag-toc__heading-anchor"></span><span id="contact" class="uag-toc__heading-anchor"></span><span id="contact" class="uag-toc__heading-anchor"></span><span id="contact" class="uag-toc__heading-anchor"></span><span id="contact" class="uag-toc__heading-anchor"></span><span id="contact" class="uag-toc__heading-anchor"></span><span id="contact" class="uag-toc__heading-anchor"></span><span id="contact" class="uag-toc__heading-anchor"></span><span id="contact" class="uag-toc__heading-anchor"></span><span id="contact" class="uag-toc__heading-anchor"></span><span id="contact" class="uag-toc__heading-anchor"></span>
-            <h3 class="d-block d-lg-none">Contact</h3>
-        </nav>
-
-        <nav class="navbar navbar-expand-lg d-none d-sm-block">
-            <ul class="navbar-nav flex-wrap">
-                <li class="nav-item menu-parent_item">
-                    <a href="https://www.prostyle-residence.com/mokusiru/category/city/" class="nav-link js-collapse">
-                        <span>#街</span>
-                    </a>
-                </li>
-                <li class="nav-item menu-parent_item">
-                    <a href="https://www.prostyle-residence.com/mokusiru/category/life-style/" class="nav-link js-collapse">
-                        <span>#ライフスタイル</span>
-                    </a>
-                </li>
-                <li class="nav-item menu-parent_item">
-                    <a href="https://www.prostyle-residence.com/mokusiru/category/facility-construction/" class="nav-link js-collapse">
-                        <span>#設備・構造</span>
-                    </a>
-                </li>
-                <li class="nav-item menu-parent_item">
-                    <a href="https://www.prostyle-residence.com/mokusiru/category/%e4%b8%8d%e5%8b%95%e7%94%a3%e7%9f%a5%e8%ad%98/" class="nav-link js-collapse">
-                        <span>#不動産知識</span>
-                    </a>
-                </li>
-                <li class="nav-item menu-parent_item">
-                    <a href="https://www.prostyle-residence.com/mokusiru/category/money/" class="nav-link js-collapse">
-                        <span>#お金の知識</span>
-                    </a>
-                </li>
-                <li class="nav-item menu-parent_item">
-                    <a href="https://www.prostyle-residence.com/mokusiru/category/hobby/" class="nav-link js-collapse">
-                        <span>#趣味</span>
-                    </a>
-                </li>
-                <li class="nav-item menu-parent_item">
-                    <a href="https://www.prostyle-residence.com/mokusiru/category/%e6%96%b0%e7%af%89%e5%8f%96%e6%89%b1%e3%81%84%e7%89%a9%e4%bb%b6/" class="nav-link js-collapse">
-                        <span>#新築マンション</span>
-                    </a>
-                </li>
-                <li class="nav-item menu-parent_item">
-                    <a href="https://www.prostyle-residence.com/mokusiru/category/%e6%96%b0%e7%af%89%e5%8f%96%e6%89%b1%e3%81%84%e7%89%a9%e4%bb%b6/%e3%83%97%e3%83%ad%e3%82%b9%e3%82%bf%e3%82%a4%e3%83%ab%e8%94%b5%e5%89%8d/" class="nav-link js-collapse">
-                        <span>#プロスタイル蔵前</span>
-                    </a>
-                </li>
-                <li class="nav-item menu-parent_item">
-                    <a href="https://www.prostyle-residence.com/mokusiru/category/gourmet/" class="nav-link js-collapse">
-                        <span>#グルメ</span>
-                    </a>
-                </li>
-                <li class="nav-item menu-parent_item">
-                    <a href="https://www.prostyle-residence.com/mokusiru/category/solid-wood/" class="nav-link js-collapse">
-                        <span>#無垢材</span>
-                    </a>
-                </li>
-            </ul>
-        </nav>
-        <div class="text-center d-lg-block d-sm-none icon-position-sm">
-            <div class="d-block icon-chevron-down icon-chevron">
-                <img class="" src="common/icon-down.svg" alt="" title="">
-            </div>
-            <div class="d-none icon-chevron-up icon-chevron">
-                <img class="" src="common/icon-up.svg" alt="" title="">
+        <div class="row">
+            <div class="col-12">
+                <div id="nav_toggle" class="menu_toggle">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-chevron-compact-down" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M1.553 6.776a.5.5 0 0 1 .67-.223L8 9.44l5.776-2.888a.5.5 0 1 1 .448.894l-6 3a.5.5 0 0 1-.448 0l-6-3a.5.5 0 0 1-.223-.67" />
+                    </svg>
+                </div>
+                <ul class="list_tags">
+                    <li>
+                        <a href="https://www.prostyle-residence.com/mokusiru/category/%e6%96%b0%e7%af%89%e5%8f%96%e6%89%b1%e3%81%84%e7%89%a9%e4%bb%b6/">
+                            <span>＃新築マンション</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.prostyle-residence.com/mokusiru/category/life-style/">
+                            <span>＃ライフスタイル</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.prostyle-residence.com/mokusiru/category/city/">
+                            <span>＃街</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.prostyle-residence.com/mokusiru/category/facility-construction/">
+                            <span>＃設備・構造</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.prostyle-residence.com/mokusiru/category/money/">
+                            <span>＃お金の知識</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.prostyle-residence.com/mokusiru/category/hobby/">
+                            <span>＃趣味</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.prostyle-residence.com/mokusiru/category/solid-wood/">
+                            <span>＃無垢材</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.prostyle-residence.com/mokusiru/category/gourmet/">
+                            <span>＃グルメ</span>
+                        </a>
+                    </li>
+                </ul>
             </div>
         </div>
-        <ul class="navbar social-list d-flex d-lg-none">
-            <li class="nav-item active">
-                <a class="nav-link" href="https://www.facebook.com/prostyleresidence/">
-                    <img src="common/icon-facebook-black.svg" alt="facebook" title="facebook">
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="https://mobile.twitter.com/PROSTYLE_OA">
-                    <img src="common/icon-twitterX_circle.svg" alt="twitter" title="twitter">
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="https://www.instagram.com/prostyle.official">
-                    <img src="common/icon-instagram-black.svg" alt="instagram" title="instagram">
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="https://www.youtube.com/channel/UC1UquQQDwtU6VfKq3pwM4Hg">
-                    <img src="common/icon-youtube-black.svg" alt="youtube" title="youtube">
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="https://lin.ee/8dqmWKo">
-                    <img src="common/icon-line-black.svg" alt="line" title="line">
-                </a>
-            </li>
-            <li>
-                <a class="nav-link" href="https://www.prostyle-residence.com/contact/" target="">
-                    <span class="btn-blog i-list_btn">お問い合わせ</span>
-                </a>
-            </li>
-        </ul>
     </div>
 </div>
