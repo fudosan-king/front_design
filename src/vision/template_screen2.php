b6ea536637836e9d5fc2c49f09cdc32b12040c30
nicker pham <nickey3000@gmail.com>
2024-07-25 13:19:27 +0700

    ...

--- src/vision/template_screen2.php
+++ src/vision/template_screen2.php
@@ -199,7 +199,7 @@
                                                 <a href="#" class="btn btn_opensample">サンプルを表示</a>
                                                 <a href="#" class="btn btn_nowopen"></a>
                                             </div>
-                                            <div class="box_button_bottom custom">
+                                            <div class="box_button_bottom">
                                                 <a href="#" class="btn btn_delete">
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
                                                         <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
@@ -229,7 +229,7 @@
                                                 <a href="#" class="btn btn_opensample">サンプルを表示</a>
                                                 <a href="#" class="btn btn_nowopen"></a>
                                             </div>
-                                            <div class="box_button_bottom custom">
+                                            <div class="box_button_bottom">
                                                 <a href="#" class="btn btn_delete">
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
                                                         <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
@@ -259,7 +259,7 @@
                                                 <a href="#" class="btn btn_opensample">サンプルを表示</a>
                                                 <a href="#" class="btn btn_nowopen"></a>
                                             </div>
-                                            <div class="box_button_bottom custom">
+                                            <div class="box_button_bottom">
                                                 <a href="#" class="btn btn_delete">
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
                                                         <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
