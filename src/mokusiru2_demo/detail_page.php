c851b49df5c927ddb6aed14745a167bce4f5ee7c
nicker pham <nickey3000@gmail.com>
2024-08-06 11:17:36 +0700

    update new

---
+++ src/mokusiru2_demo/detail_page.php
@@ -0,0 +1,244 @@
+<!DOCTYPE html>
+<!-- saved from url=(0044)https://www.prostyle-residence.com/mokusiru/ -->
+<html class="no-js" lang="ja">
+
+<head>
+    <?php include 'head.php'; ?>
+</head>
+
+<body class="home blog">
+    <?php include 'header.php'; ?>
+
+    <?php include 'menu_sub.php'; ?>
+
+    <div class="sub_page">
+        <div class="container">
+            <div class="row row-lists">
+                <div class="col-md-9">
+                    <div class="main-content">
+
+                        <section class="section_unit_detail">
+                            <h2>プロスタイル南町田</h2>
+                            <div class="box_unit_detail_img">
+                                <div class="big_img">
+                                    <a href="assets/images/products/big_img.png" data-fancybox="gallery"><img src="assets/images/products/big_img.png" alt="big_img"></a>
+                                </div>
+                                <ul class="list_fourimg">
+                                    <li>
+                                        <a href="assets/images/products/img01.png" data-fancybox="gallery"><img src="assets/images/products/img01.png" alt="img01"></a>
+                                    </li>
+                                    <li>
+                                        <a href="assets/images/products/img02.png" data-fancybox="gallery"><img src="assets/images/products/img02.png" alt="img02"></a>
+                                    </li>
+                                    <li>
+                                        <a href="assets/images/products/img03.png" data-fancybox="gallery"><img src="assets/images/products/img03.png" alt="img03"></a>
+                                    </li>
+                                    <li>
+                                        <a href="assets/images/products/img04.png" data-fancybox="gallery"><img src="assets/images/products/img04.png" alt="img04"></a>
+                                    </li>
+                                </ul>
+                            </div>
+
+                            <div class="box_keypoints">
+                                <span>本物件のポイント</span>
+                                <p>■天然無垢材を多用した高級デザイン <br>
+                                    ■高い収納率と機能的なプラン <br>
+                                    ■10.2帖のマスターベットルーム <br>
+                                    ■トイレ2ヶ所、ツインボール洗面人気の同時使いプラン <br>
+                                    ■南向きで陽当り良好<br>
+                                    ■2018年大規模修繕工事実施済 弊社売主となりますので是非お問合せ下さい。</p>
+                            </div>
+
+                            <div class="box_unit_detail_info">
+                                <div class="box_objectsummary">
+                                    <h2>物件概要</h2>
+                                    <div class="table-responsive">
+                                        <table class="table">
+                                            <tr>
+                                                <th>所在地</th>
+                                                <td>東京都品川区大井4</td>
+                                            </tr>
+                                            <tr>
+                                                <th>交通</th>
+                                                <td>半蔵門線　青山一丁目駅　徒歩5分<br>
+                                                    JR山の手線　原宿駅　徒歩12分<br>
+                                                    JR内房線 五井駅 バス5分 徒歩10分（バス：吹上通り角停留所）</td>
+                                            </tr>
+                                            <tr>
+                                                <th>総戸数</th>
+                                                <td>30戸</td>
+                                            </tr>
+                                            <tr>
+                                                <th>建物構造</th>
+                                                <td>ＲＣ</td>
+                                            </tr>
+                                            <tr>
+                                                <th>階数</th>
+                                                <td>地上7階</td>
+                                            </tr>
+                                            <tr>
+                                                <th>築年月</th>
+                                                <td>2017年3月</td>
+                                            </tr>
+                                            <tr>
+                                                <th>施主</th>
+                                                <td>プロポライフ</td>
+                                            </tr>
+                                            <tr>
+                                                <th>施工</th>
+                                                <td>西野建設</td>
+                                            </tr>
+                                        </table>
+                                    </div>
+                                    <h3>マンションの情報は、現況（販売中物件）と異なる場合がございます。</h3>
+                                    <p>
+                                        <small>※分譲当時のパンフレットの内容を記載しているため、以降に更新された情報が異なる場合がございます。<br>
+                                            ※不動産の表示に関する公正競争規約の改正（2022年９月１日）により、交通利便情報が販売中のものと異なる場合がございます。
+                                        </small>
+                                    </p>
+                                </div>
+
+                                <div class="box_features brown">
+                                    <h3>プロスタイル南町田の特徴</h3>
+                                    <p>プロスタイル南町田は、シャープな外観の2017年3月に竣工した7階建てのマンションです。東、南、西側が生活道路に面した3方向接道の開放的な敷地です。南側にエントランスへのアプローチが設けられ、北側に駐車場への入庫路が設置された歩車分離のレイアウトで安全面にも配慮しています。スロープの設けられたエントランスには、ダウンライトの灯るお洒落なポーチが優美な空間を演出しています。またTVモニター付きオートロックや防犯カメラを設置してセキュリティ対策をしています。さらに生活を便利にサポートする宅配ボックスもあり、留守時でも荷物の受け取りがスムーズです。プロスタイル南町田の総住戸数は30戸、分譲時の間取りは2LDK～4LDK、面積帯は58.73㎡～78.70㎡を中心としています。全戸西向きにレイアウトされ、1階住戸にはテラスの先に戸建て感覚の暮らしが楽しめる専用庭があります。各住戸のバルコニーは1.8mの奥行を確保し、バルコニーに面した居室は柱が室内に出ないアウトフレーム工法で、すっきりとしています。また玄関はダブルディンプルキーを採用し、住まう方の安心安全を守ります。プロスタイル南町田は、東急田園都市線南町田駅から徒歩6分です。</p>
+                                </div>
+
+                                <article class="article_estate">
+                                    <h3 class="article_subtitle2">プロスタイル南町田 売出し中物件</h3>
+                                    <ul class="list_estate_item">
+                                        <li>
+                                            <div class="list_estate_item_img">
+                                                <a href="https://staging.prostyle-residence.com/mokusiru/wp-content/uploads/2023/03/%E9%96%93%E5%8F%96%E3%82%8A.png" data-fancybox="gallery" data-caption="estate01"><img src="https://staging.prostyle-residence.com/mokusiru/wp-content/uploads/2023/03/%E9%96%93%E5%8F%96%E3%82%8A.png" alt="estate01"></a>
+                                            </div>
+                                            <div class="list_estate_item_content">
+                                                <h4>プロスタイル南町田 202号室</h4>
+                                                <p class="price">価格 <span class="big_price">5,280</span><i>万円</i></p>
+                                                <p><span>間取り</span>: <span>3LDK</span> </p>
+                                                <p><span>占有面積</span>: <span>79.36 m2</span> </p>
+                                                <p><span>所在階</span>: <span>１階／地上12階</span> </p>
+                                            </div>
+                                            <div class="list_estate_item_footer">
+                                                <a href="achievement_document_request.php" class="btn btn_contactus">お問合せ</a>
+                                                <a class="btn btn_viewdetail" href="unit_detailpage.php">詳細を見る</a>
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div class="list_estate_item_img">
+                                                <a href="https://staging.prostyle-residence.com/mokusiru/wp-content/uploads/2023/03/%E9%96%93%E5%8F%96%E3%82%8A.png" data-fancybox="gallery" data-caption="estate01"><img src="https://staging.prostyle-residence.com/mokusiru/wp-content/uploads/2023/11/A-2.jpg" alt="estate01"></a>
+                                            </div>
+                                            <div class="list_estate_item_content">
+                                                <h4>プロスタイル南町田 202号室</h4>
+                                                <p class="price">価格 <span class="big_price">5,280</span><i>万円</i></p>
+                                                <p><span>間取り</span>: <span>3LDK</span> </p>
+                                                <p><span>占有面積</span>: <span>79.36 m2</span> </p>
+                                                <p><span>所在階</span>: <span>１階／地上12階</span> </p>
+                                            </div>
+                                            <div class="list_estate_item_footer">
+                                                <a href="achievement_document_request.php" class="btn btn_contactus">お問合せ</a>
+                                                <a class="btn btn_viewdetail" href="unit_detailpage.php">詳細を見る</a>
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div class="list_estate_item_img">
+                                                <a href="https://staging.prostyle-residence.com/mokusiru/wp-content/uploads/2023/11/A-2.jpg" data-fancybox="gallery" data-caption="estate01"><img src="https://staging.prostyle-residence.com/mokusiru/wp-content/uploads/2023/11/estate02.jpg" alt="estate01"></a>
+                                            </div>
+                                            <div class="list_estate_item_content">
+                                                <h4>プロスタイル南町田 202号室</h4>
+                                                <p class="price">価格 <span class="big_price">5,280</span><i>万円</i></p>
+                                                <p><span>間取り</span>: <span>3LDK</span> </p>
+                                                <p><span>占有面積</span>: <span>79.36 m2</span> </p>
+                                                <p><span>所在階</span>: <span>１階／地上12階</span> </p>
+                                            </div>
+                                            <div class="list_estate_item_footer">
+                                                <a href="achievement_document_request.php" class="btn btn_contactus">お問合せ</a>
+                                                <a class="btn btn_viewdetail" href="unit_detailpage.php">詳細を見る</a>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </article>
+
+                                <div class="box_purchasing">
+                                    <h4>ご購入をお考えの方</h4>
+                                    <a href="request_inform_estate_onsale.php" class="btn"><img src="assets/images/i_home.svg" alt="home"> 売り出されたら教えて欲しい</a>
+                                </div>
+                                <div class="box_consideringselling">
+                                    <h4>ご売却をお考えの方へ</h4>
+                                    <p>プロスタイルマンションの販売をご検討いただける方には、多くの特典をご用意しています。</p>
+                                    <ul>
+                                        <li>特典内容が入ります特典内容が入ります特典内容が入ります特典内容が入ります。</li>
+                                        <li>特典内容が入ります特典内容が入ります特典内容が入ります特典内容が入ります。</li>
+                                        <li>特典内容が入ります特典内容が入ります特典内容が入ります特典内容が入ります。</li>
+                                    </ul>
+                                    <a href="free_audit_request.php" class="btn"><img src="assets/images/i_search.svg" alt="search"> 無料売却査定</a>
+                                </div>
+
+                                <hr class="dot">
+
+                                <ul class="box_followprostyle">
+                                    <li>
+                                        <a href="https://www.facebook.com/prostyleresidence/">
+                                            <img src="assets/images/i_fb_brown.svg" alt="facebook" title="facebook">
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="https://mobile.twitter.com/PROSTYLE_OA">
+                                            <img src="assets/images/i_x_brown.svg" alt="X" title="X">
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="https://www.instagram.com/prostyle.official">
+                                            <img src="assets/images/i_ins_brown.svg" alt="instagram" title="instagram">
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="https://www.youtube.com/channel/UC1UquQQDwtU6VfKq3pwM4Hg">
+                                            <img src="assets/images/i_youtube_brown.svg" alt="youtube" title="youtube">
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="https://lin.ee/8dqmWKo">
+                                            <img src="assets/images/i_line_brown.svg" alt="line" title="line">
+                                        </a>
+                                    </li>
+                                </ul>
+
+                                <a href="#" class="btn btn_mokusiru"><img src="assets/images/logo_mokusiru_white.svg" alt="" width="300"></a>
+
+                            </div>
+
+                        </section>
+
+
+                    </div>
+                </div>
+
+                <div class="col-md-3">
+                    <?php include 'sidebar_right.php'; ?>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div id="modal_video" class="modal fade modal_brands" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+        <div class="modal-dialog modal-dialog-centered modal-lg">
+            <div class="modal-content">
+                <span id="movie" class="uag-toc__heading-anchor"></span><span id="movie" class="uag-toc__heading-anchor"></span><span id="movie" class="uag-toc__heading-anchor"></span><span id="movie" class="uag-toc__heading-anchor"></span><span id="movie" class="uag-toc__heading-anchor"></span><span id="movie" class="uag-toc__heading-anchor"></span><span id="movie" class="uag-toc__heading-anchor"></span><span id="movie" class="uag-toc__heading-anchor"></span><span id="movie" class="uag-toc__heading-anchor"></span><span id="movie" class="uag-toc__heading-anchor"></span><span id="movie" class="uag-toc__heading-anchor"></span>
+                <h2>MOVIE</h2>
+                <a href="https://www.prostyle-residence.com/mokusiru/#" class="btn btnClose" data-dismiss="modal" aria-label="Close"><img src="common/i-close-white.png" alt=""></a>
+                <div class="embed-responsive embed-responsive-16by9 z-depth-1-half">
+                    <iframe id="video" class="embed-responsive-item" src="common/saved_resource.html" allowfullscreen=""></iframe>
+                </div>
+                <a href="https://www.prostyle-residence.com/mokusiru/#" class="btn btnClose_footer" data-dismiss="modal" aria-label="Close"><img src="common/i-close.png" alt=""></a>
+            </div>
+        </div>
+    </div>
+
+    <?php include 'footer.php'; ?>
+
+
+    <!-- Scripts -->
+    <?php include 'js-footer.php'; ?>
+</body>
+
+</html>
---
+++ src/mokusiru2_demo/footer.php
@@ -0,0 +1,100 @@
+<div id="modal_video" class="modal fade modal_brands" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+    <div class="modal-dialog modal-dialog-centered modal-lg">
+        <div class="modal-content">
+            <span id="movie" class="uag-toc__heading-anchor"></span><span id="movie" class="uag-toc__heading-anchor"></span><span id="movie" class="uag-toc__heading-anchor"></span><span id="movie" class="uag-toc__heading-anchor"></span><span id="movie" class="uag-toc__heading-anchor"></span><span id="movie" class="uag-toc__heading-anchor"></span><span id="movie" class="uag-toc__heading-anchor"></span><span id="movie" class="uag-toc__heading-anchor"></span><span id="movie" class="uag-toc__heading-anchor"></span><span id="movie" class="uag-toc__heading-anchor"></span><span id="movie" class="uag-toc__heading-anchor"></span>
+            <h2>MOVIE</h2>
+            <a href="https://www.prostyle-residence.com/mokusiru/#" class="btn btnClose" data-dismiss="modal" aria-label="Close"><img src="common/i-close-white.png" alt=""></a>
+            <div class="embed-responsive embed-responsive-16by9 z-depth-1-half">
+                <iframe id="video" class="embed-responsive-item" src="common/saved_resource.html" allowfullscreen=""></iframe>
+            </div>
+            <a href="https://www.prostyle-residence.com/mokusiru/#" class="btn btnClose_footer" data-dismiss="modal" aria-label="Close"><img src="common/i-close.png" alt=""></a>
+        </div>
+    </div>
+</div>
+
+<footer>
+    <div class="footer">
+        <div class="container">
+            <div class="footer_logo">
+                <a href="https://www.prostyle-residence.com/">
+                    <img src="common/logo-prostyle.png" alt="Prostyle" title="Prostyle">
+                </a>
+            </div>
+            <span class="divider"></span>
+
+            <div class="footer_btn">
+                <a class="btn btn-footer" href="https://www.prostyle-residence.com/contact/" target="">
+                    <span class="btn-blog i-list_btn">お問い合わせ</span>
+                </a>
+            </div>
+
+            <ul class="navbar social-list">
+                <li class="nav-item">
+                    <a class="nav-link" href="https://www.facebook.com/prostyleresidence/">
+                        <img src="assets/images/i_fb.svg" alt="facebook" title="facebook" width="10">
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://mobile.twitter.com/PROSTYLE_OA">
+                        <img src="assets/images/i_x.svg" alt="X" title="X" width="25">
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://www.instagram.com/prostyle.official">
+                        <img src="assets/images/i_ins.svg" alt="instagram" title="instagram" width="20">
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://www.youtube.com/channel/UC1UquQQDwtU6VfKq3pwM4Hg">
+                        <img src="assets/images/i_youtube.svg" alt="youtube" title="youtube" width="20">
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <img src="assets/images/tiktok.svg" alt="tiktok" title="tiktok" width="20">
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="header-top bg-brown">
+            <nav class="navbar navbar-expand-lg">
+                <p class="copy-right">
+                    Copyright © PROSTYLE INC. All Rights Reserved.
+                </p>
+                <p>
+                </p>
+                <div class="collapse navbar-collapse nav_menu-i-list">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="https://www.prostyle-residence.com/">
+                                PROSTYLEについて
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="https://www.prostyle-residence.com/terms/">
+                                利用規約
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="https://www.prostyle-residence.com/anti-social/">
+                                反社会的勢力排除に関する基本方針
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="https://www.prostyle-residence.com/privacy-policy/">
+                                プライバシーポリシー
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="https://www.prostyle-residence.com/social-media-policy/">
+                                ソーシャルメディアポリシー
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+
+    </div>
+</footer>
